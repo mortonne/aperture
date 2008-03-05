b4f90ddca42ee7dfcad4e3f5a6df0dd642e29595
morton <morton@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2008-03-05 06:02:13 +0000

    *** empty log message ***

--- events/addEvents.m
+++ events/addEvents.m
@@ -1,12 +1,19 @@
-function eeg = addEvents(eeg, eventsFile)
-%eeg = addEvents(eeg, eventsFile)
+function exp = addEvents(exp, eventsFile, resDir, evname)
+%exp = addEvents(exp, eventsFile)
 
-for s=1:length(eeg.subj)
+for s=1:length(exp.subj)
+  ev.name = evname;
+  ev.file = fullfile(resDir, 'events', [evnamme '_' exp.subj(s).id '.mat']);
+  
   events = [];
-  for n=1:length(eeg.subj(s).sess)
-    sess_events = load(fullfile(eeg.subj(s).sess(n).dir, eventsFile));
+  for n=1:length(exp.subj(s).sess)
+    sess_events = load(fullfile(exp.subj(s).sess(n).dir, eventsFile));
     events = concat(events(:), sess_events(:))';
   end
   
+  save(ev.file, 'events');
   
+  load(exp.file);
+  exp = setobj(exp, 'ev', ev);
+  save(exp.file);
 end
