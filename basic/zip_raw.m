862514937e7dfc2a6c1aa09b7c73cef5301d4832
morton <morton@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2008-05-08 23:39:07 +0000

    *** empty log message ***

---
+++ basic/zip_raw.m
@@ -0,0 +1,22 @@
+function zip_raw(exp, cmd)
+	%
+	%ZIP_RAW   Run bzip2 or bunzip2 on all rawfiles in an experiment.
+	%
+	
+	for s=1:length(exp.subj)
+		for n=1:length(exp.subj(s).sess)
+			eegdir = fullfile(exp.subj(s).sess(n).dir, 'eeg');
+			if strcmp(cmd, 'unzip')
+				d = dir(fullfile(eegdir, '*raw.bz2'));
+				if isempty(d)
+					fprintf('%s empty.\n', eegdir)
+					continue
+				end
+				filename = d.name;
+				filename = strrep(filename, ' ', '\ ');
+				%system(['ls ' fullfile(eegdir, filename)]);
+				system(['bunzip2 ' fullfile(eegdir, filename)]);
+			end
+		end
+	end
+	
