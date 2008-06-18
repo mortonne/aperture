9c0c849bb6f7363fad6cf5cee9229b6be73261db
morton <morton@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2008-06-18 12:55:39 +0000

    *** empty log message ***

---
+++ utils/struct2propval.m
@@ -0,0 +1,12 @@
+function propval = struct2propval(s)
+%
+%STRUCT2PROPVAL   Convert a structure into a cell array of property-value pairs.
+%   PROPVAL = STRUCT2PROPVAL(S)
+%
+
+fnames = fieldnames(s);
+vals = struct2cell(s);
+for i=1:length(fnames)
+	propval{2*i-1} = fnames{i};
+	propval{2*i} = vals{i};
+end
