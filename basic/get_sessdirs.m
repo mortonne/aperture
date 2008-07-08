55ffeab4396b5741da95a46b8580b4ee3beec4d4
morton <morton@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2008-07-08 22:37:43 +0000

    *** empty log message ***

--- basic/get_sessdirs.m
+++ basic/get_sessdirs.m
@@ -1,11 +1,24 @@
-function subj = get_sessdirs(dataroot,subjstr)
+function subj = get_sessdirs(dataroot,subjstr,file2check)
 %
 %GET_SESSDIRS   Generate a subj struct for a given experiment directory.
 %   SUBJ = GET_SESSDIRS(DATAROOT,SUBJSTR) looks for directories in
-%   DATAROOT that match SUBJSTR (which may contain wildcards), and
-%   creates a vector of "subj" structs, each contain a "sess" struct
-%   for each session directory that contains a session.log file.
+%   DATAROOT that match SUBJSTR (which may contain wildcards; default is
+%   'subj*'), and creates a vector of "subj" structs, each containing a 
+%   "sess" struct for each session directory that contains a session.log
+%   file.
 %
+%   SUBJ = GET_SESSDIRS(DATAROOT,SUBJSTR,FILE2CHECK) uses FILE2CHECK as
+%   the criterion for whether a session should be included.  Set FILE2CHECK
+%   to '' to return all session directories, regardless of whether they
+%   contain data.
+%
+
+if ~exist('file2check','var')
+  file2check = 'session.log';
+end
+if ~exist('subjstr','var')
+  subjstr = 'subj*';
+end
 
 % get all directories matching subjstr
 d = dir(fullfile(dataroot, subjstr));
@@ -25,8 +38,8 @@ for s=1:length(subjects)
   for n=1:length(sessions)
     % get the session path
     sessdir = fullfile(subjdir, sessions{n});
-        
-    if exist(fullfile(sessdir, 'session.log'), 'file')
+    
+    if isempty(file2check) || exist(fullfile(sessdir, file2check), 'file')
       % if there is a logfile, add a sess struct
       subj(s).sess(n).number = str2num(sessions{n}(end));
       subj(s).sess(n).dir = sessdir;
