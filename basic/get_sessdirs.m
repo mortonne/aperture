28f34625635acdcf2499cf113df08c1081d41514
morton <morton@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2008-07-10 18:39:43 +0000

    *** empty log message ***

--- basic/get_sessdirs.m
+++ basic/get_sessdirs.m
@@ -8,13 +8,23 @@ function subj = get_sessdirs(dataroot,subjstr,file2check)
 %   file.
 %
 %   SUBJ = GET_SESSDIRS(DATAROOT,SUBJSTR,FILE2CHECK) uses FILE2CHECK as
-%   the criterion for whether a session should be included.  Set FILE2CHECK
-%   to '' to return all session directories, regardless of whether they
-%   contain data.
+%   the criterion for whether a session should be included.  FILE2CHECK
+%   is a path to a file relative to each session directory.  It may contain 
+%   wildcards, and may either be a string or a cell array of strings.  
+%   Set FILE2CHECK to '' to return all session directories, regardless of 
+%   whether they contain data.
+%
+%   Example
+%     subj = get_sessdirs(dataroot,'LTP*',{'session.log', 'eeg/*.raw'})
+%     gets all subjects in dataroot whose id starts with LTP, and all of
+%     their sessions that have both a session log file and an eeg file.
 %
 
 if ~exist('file2check','var')
-  file2check = 'session.log';
+  file2check = {'session.log'};
+end
+if ~iscell(file2check)
+  file2check = {file2check};
 end
 if ~exist('subjstr','var')
   subjstr = 'subj*';
@@ -39,8 +49,13 @@ for s=1:length(subjects)
     % get the session path
     sessdir = fullfile(subj(s).dir, sessions{n});
     
-    if isempty(file2check) || exist(fullfile(sessdir, file2check), 'file')
-      % if there is a logfile, add a sess struct
+    for i=1:length(file2check)
+      % see if the file2check exists for this session
+      fileExists(i) = ~isempty(dir(fullfile(sessdir, file2check{i})));
+    end
+    
+    if isempty(file2check) || all(fileExists)
+      % if the file(s) exist, add a sess struct
       subj(s).sess(n).number = str2num(sessions{n}(end));
       subj(s).sess(n).dir = sessdir;
     end
