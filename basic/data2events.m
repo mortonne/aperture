3dd4e632b6c8725fae54e0fccd254b596e46ea0c
morton <morton@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2008-07-10 18:17:28 +0000

    *** empty log message ***

---
+++ basic/data2events.m
@@ -0,0 +1,21 @@
+function events = data2events(data)
+%
+%DATA2EVENTS
+%
+
+fnames = fieldnames(data);
+for i=1:length(fnames)
+  
+  % transpose and vectorize to get back in events order
+  field = data.(fnames{i})';
+  vec = field(:);
+  
+  % fill in the events struct
+  for j=1:length(vec)
+    if iscell(vec)
+      events(j).(fnames{i}) = vec{j};
+      else
+      events(j).(fnames{i}) = vec(j);
+    end
+  end
+end
--- basic/exp2data.m
+++ basic/exp2data.m
@@ -1,10 +1,13 @@
-function data = exp2data(exp,dataFcnHandle, evname)
+function [data,exp] = exp2data(exp,dataFcnHandle,evname,datafile)
 
 if ~exist('dataFcnHandle','var')
   dataFcnHandle = @extract_data;
 end
 if ~exist('evname','var')
   evname = 'events';
+end
+if ~exist('datafile','var')
+  datafile = fullfile(exp.resDir, 'data.mat');
 end
 
 % create a data struct with all subjects
@@ -17,3 +20,9 @@ for s=1:length(exp.subj)
   
   data = concatData(data, subj_data);
 end
+
+% save the full data struct
+exp.datafile = datafile;
+exp = update_exp(exp);
+
+save(exp.datafile, 'data');
