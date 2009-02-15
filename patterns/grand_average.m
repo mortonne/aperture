dc893ee655e62d94008afca690cebe7a15368071
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2009-02-15 23:43:45 +0000

    updated the implementation and signature of grand_average.

--- patterns/grand_average.m
+++ patterns/grand_average.m
@@ -1,46 +1,49 @@
-function exp = grand_average(exp, patname, resDir)
-%GRAND AVERAGE   Calculate an average across patterns from multiple subjects.
-%   EXP = GRAND_AVERAGE(EXP,PATNAME,RESDIR)
+function pat = grand_average(subj, pat_name, res_dir)
+%GRAND_AVERAGE   Calculate an average across patterns from multiple subjects.
 %
-%   This script assumes that the dimensions of all subjects' patterns
-%   are identical.
+%  pat = grand_average(subj, pat_name, res_dir)
 %
+%  INPUTS:
+%      subj:  vector structure holding information about subjects. Each must
+%             contain a pat object named pat_name.
+%
+%  pat_name:  name of the pattern to concatenate across subjects.
+%
+%   res_dir:  directory in which to save the new pattern. Default is the same
+%             directory the first subject's pattern is saved in.
+%
+%  OUTPUTS:
+%       pat:  
 
-if ~exist('resDir', 'var')
-	resDir = fullfile(exp.resDir, 'eeg', patname);
+if ~exist('pat_name','var')
+  error('You must specify the name of the patterns you want to concatenate.')
+  elseif ~exist('subj','var')
+  error('You must pass a subj structure.')
+  elseif ~isstruct(subj)
+  error('subj must be a structure.')
 end
 
-subjpat = getobj(exp.subj(1), 'pat', patname);
+% get info from the first subject
+subj_pat = getobj(subj(1), 'pat', pat_name);
+if ~exist('res_dir','var')
+  % use the same directory as the subject patterns
+  res_dir = fullfile(fileparts(subj_pat.file));
+end
 
 % initialize the new pattern
-patfile = fullfile(resDir, 'patterns', [patname '_ga.mat']);
-pat = init_pat(patname, patfile);
-pat.dim = subjpat.dim;
+pat_file = fullfile(res_dir, [pat_name '_ga.mat']);
+pat = init_pat(pat_name, pat_file, 'multiple_subjects', subj_pat.params, subj_pat.dim);
 
 % get filenames for all subject patterns
-for s=1:length(exp.subj)
-	subjpat = getobj(exp.subj(s), 'pat', patname);
-	subjfiles{s} = subjpat.file;
+for s=1:length(subj)
+	subj_pat = getobj(subj(s), 'pat', pat_name);
+	subj_files{s} = subj_pat.file;
 end
 
-% check input files, lock output if desired
-if prepFiles(subjfiles, pat.file)~=0
-	error('Problem with one of the input files.')
-end
-
-% initialize pattern to hold all subjects
-pattern = NaN(pat.dim.ev.len, length(pat.dim.chan), length(pat.dim.time), length(pat.dim.freq), length(exp.subj));
-
-% get all subjects' means
-for s=1:length(subjfiles)
-	subj_pattern = loadPat(subjfiles{s});
-	pattern(:,:,:,:,s) = subj_pattern;
-end
+% load and concatenate all subject patterns
+pattern = getvarallsubj(subj, {'pat', pat_name}, 'pattern', 5);
 
 % average across subjects
 pattern = mean(pattern,5);
 save(pat.file, 'pattern');
 closeFile(pat.file);
-
-% add the new grand average pat object to the exp struct
-exp = update_exp(exp, 'pat', pat);
