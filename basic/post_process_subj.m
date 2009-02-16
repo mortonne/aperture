461c51c7e9222a35ed31207fba7f2f0927b097a3
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2009-02-16 21:43:56 +0000

    Changed post-processing wrapper, removed post_process_exp2.m

---
+++ basic/post_process_subj.m
@@ -0,0 +1,16 @@
+function post_process_subj(subj,varargin)
+%POST_PROCESS_SUBJ   Update the post-processing for one subject.
+%
+%  post_process_subj(subj, varargin)
+%
+%  INPUTS:
+%      subj:  standard subj structure representing one subject.
+%
+%  varargin:  additional inputs (after subj.id and sess.dir)
+%             to prep_egi_data2.
+
+% do any necessary post-processing, save events file for each subj
+for sess=subj.sess
+  % split, sync, rereference, detect blink artifacts
+  prep_egi_data2(subj.id,sess.dir,varargin{:});
+end
