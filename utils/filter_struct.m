78d3ccb115b3a270742e2039f1ce77874517919f
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2009-02-16 22:32:36 +0000

    Beginning audit of utils folder.

---
+++ utils/filter_struct.m
@@ -0,0 +1,42 @@
+function include = filter_struct(s, filt_fcn, filt_inputs)
+%FILTER_STRUCT   Filter a structure using a custom function.
+%
+%  include = filter_struct(s, filt_fcn, filt_inputs)
+%
+%  INPUTS:
+%            s:  a vector structure.
+%
+%     filt_fcn:  a handle to a function that takes one element of s as
+%                its first argument and returns true or false, with true 
+%                indicating that the element should be included in the 
+%                filtered structure.
+%
+%  filt_inputs:  cell array of additional inputs to filt_fcn.
+%
+%  OUTPUTS:
+%      include:  a logical array the same size as s.
+
+if ~exist('filt_inputs','var')
+  filt_inputs = {};
+  elseif ~iscell(filt_inputs)
+  error('filt_inputs must be a cell array.')
+end
+if ~exist('filt_fcn','var')
+  error('You must specify a filter function.')
+  elseif ~exist('s','var')
+  error('You must pass a vector structure to filter.')
+  elseif ~isstruct(s)
+  error('s must be a structure.')
+  elseif sum(size(s)>1)>1
+  error('s must be one-dimensional.')
+end
+
+% initilize logical vector
+include = false(size(s));
+
+for i=1:length(s)
+  % see if we want this element of the structure
+  if filt_fcn(s(i), filt_inputs{:})
+    include(i) = true;
+  end
+end
--- utils/rmobj.m
+++ utils/rmobj.m
@@ -1,13 +1,41 @@
-function exp = rmobj(exp, varargin)
-%RMOBJ   Remove an object from the exp struct.
-%   EXP = RMOBJ(EXP,VARARGIN)
+function s = rmobj(s, vars)
+%RMOBJ   Remove an object from an object hierarchy.
 %
-%   Before the object is removed, a backup is made
-%   in exp.resDir/exp_bk with a timestamped filename.
+%  s = rmobj(s, vars)
 %
+%  INPUTS:
+%        s:  structure containing an object that is to be removed.
+%
+%     vars:  cell array of object type, object name pairs that climbs
+%            the hierarchy of s. The last pair indicates the object
+%            to be removed.
+%
+%  OUTPUTS:
+%        s:  the modified structure, with the specified object deleted.
 
-% delete the object
-exp = recursive_rmfield(exp, varargin);
+if mod(length(vars),2)~=0
+  error('The length of vars must be a multiple of 2.')
+  
+  elseif length(vars)==2
+	% we've reached the object to delete
+	[obj2rm, ind] = getobj(s, vars{1}, vars{2});
 
-% update exp
-exp = update_exp(exp);
+	if isempty(obj2rm)
+	  % couldn't find it
+		fprintf('WARNING: no object "%s" found in field "%s"\n', vars{2}, vars{1});
+		return
+	end
+
+	% remove the object
+	s.(vars{1})(ind) = [];
+
+  elseif length(vars)>2
+	% get the next object
+	obj = getobj(s, vars{1}, vars{2});
+	
+	% call rmobj with this new object
+	obj = rmobj(obj, vars(3:end));
+	
+	% when we've finished climbing, unwind using setobj
+	s = setobj(s, vars{1}, obj);
+end
