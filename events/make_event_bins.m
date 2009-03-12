ce8dfb2e63caa0c15d96ca2fde2a5055dabf51c8
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2009-03-12 22:56:53 +0000

    More tweaks to make_event_bins.

--- events/make_event_bins.m
+++ events/make_event_bins.m
@@ -1,47 +1,46 @@
-function bin_labels = make_event_bins(events, bins)
+function labels = make_event_bins(events, bins)
 %MAKE_EVENT_BINS   Divide an events structure into bins.
 %
-%  bin_labels = make_event_bins(events, bins)
+%  labels = make_event_bins(events, bins)
 %
 %  This function is used to create regressors for statistics
 %  and pattern classification.
 %
 %  INPUTS:
-%      events:  an events structure.
+%   events:  an events structure.
 %
-%        bins:  input that specifies how to divide the events
-%               structure into bins. Form can be:
-%
-%               'field_name'
-%                 The name of one of the fields. Same as
-%                 getStructField(events, 'field_name').
-%
-%               {'field_name1','field_name2',...}
-%                 Cell array of fieldnames. Bins will be made
-%                 such that each conjuction of the fields will
-%                 be unique.
-%
-%               {'filter1','filter2',...}
-%                 Cell array of strings that will be input to
-%                 inStruct to form each bin. Note that with
-%                 this option, it is possible to make overlapping
-%                 bins.
-%
-%               number_of_bins
-%                 Integer specifying the number of bins to
-%                 randomly divide events into non-overlapping
-%                 bins that are of as equal length as possible.
-%
-%               'overall'
-%                 All events will be put into one bin.
-%
-%               'none'
-%                 Each event will have its own bin.
+%     bins:  input that specifies how to divide the events
+%            structure into bins (see below).
 %
 %  OUTPUTS:
-%  bin_labels:  a vector or cell array of strings the same length
-%               as events, where each unique value indicates one
-%               bin.
+%   labels:  a vector or cell array of strings the same length
+%            as events, where each unique value indicates one
+%            bin.
+%
+%  BINS FORMAT:
+%    'field_name'
+%      The name of one of the fields. Same as 
+%      getStructField(events, 'field_name').
+%
+%    {'field_name1','field_name2',...}
+%      Cell array of fieldnames. Bins will be made such that each 
+%      conjuction of the fields will be unique. (BROKEN)
+%
+%    {'filter1','filter2',...}
+%      Cell array of strings that will be input to inStruct to 
+%      form each bin. Note that with this option, it is possible 
+%      to make overlapping bins.
+%
+%    number_of_bins
+%      Integer specifying the number of bins to randomly divide 
+%      events into non-overlapping bins that are of as equal 
+%      length as possible.
+%
+%    'overall'
+%      All events will be put into one bin.
+%
+%    'none'
+%      Each event will have its own bin.
 
 % input checks
 if ~exist('events','var')
@@ -53,9 +52,8 @@ if ~exist('events','var')
 end
 
 if iscell(bins)
-	fnames = fieldnames(events);
-
 	% first check if each string in the cell array is a field
+	fnames = fieldnames(events);
 	if false %sum(~ismember(bins,fnames))==0
 		% make the new field a conjunction of multiple fields
 		for i=1:length(events)
@@ -63,35 +61,35 @@ if iscell(bins)
 		  for j=1:length(bins)
 		    val = [val num2str(events(i).(bins{j})) '.'];
 		  end
-		  field{i} = val;
+		  labels{i} = val;
 		end
 
 		else
 		% assume each cell contains an eventfilter
-		field = NaN(1,length(events));
+		labels = NaN(1,length(events));
 		for i=1:length(bins)
 			thisfield = inStruct(events, bins{i});
-			field(thisfield) = i;
+			labels(thisfield) = i;
 		end
 	end
 
 	elseif isfield(events, bins)
 	% each unique value of the field will be used
-	field = getStructField(events, bins);
+	labels = getStructField(events, bins);
 
 	elseif strcmp(bins, 'overall')
 	% lump all events together
-	field = ones(1, length(events));
+	labels = ones(1, length(events));
 
 	elseif isnumeric(bins)
 	% randomly divide up the events
-	eventsPerBin = fix(length(events)/bins);
-	inds = repmat(1:bins,1,eventsPerBin);
-	field = inds(randperm(length(inds)));
+	events_per_bin = fix(length(events)/bins);
+	inds = repmat(1:bins,1,events_per_bin);
+	labels = inds(randperm(length(inds)));
 
 	elseif strcmp(bins,'none')
 	% no binning will take place
-	field = 1:length(events);
+	labels = 1:length(events);
 	
 	else
 	error('Invalid input for bins.')
