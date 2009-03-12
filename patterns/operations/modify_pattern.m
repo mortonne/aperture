057c31ce2eedf501feff5893d795524ae8b06365
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2009-03-12 23:34:58 +0000

    Finally added documentation to modify_pattern.

--- patterns/operations/modify_pattern.m
+++ patterns/operations/modify_pattern.m
@@ -1,21 +1,86 @@
-function [pat,pattern,events] = modify_pattern(pat, params, patname, res_dir)
+function [pat,pattern,events] = modify_pattern(pat, params, pat_name, res_dir)
 %MODIFY_PATTERN   Modify an existing pattern.
-%   PAT = MODIFY_PATTERN(PAT,PARAMS,PATNAME,res_dir) modifies PAT using 
-%   options in the PARAMS struct.  New patterns are saved in res_dir/patterns.
 %
-%   See patFilt for options for filtering each dimension, and see patBins 
-%   for binning each dimension.
+%  [pat, pattern, events] = modify_pattern(pat, params, pat_name, res_dir)
 %
-%   See patPCA for options for getting principal components of patterns.
+%  Use this function to modify existing patterns. You can either save the
+%  new pattern under a different name and/or file, or overwrite the old
+%  one.
 %
-%   Also see applytosubj.
+%  Operations are run in this order: 1) filtering, 2) binning, 3) principle
+%  components analysis, 4) saving out in slices
+%
+%  INPUTS:
+%       pat:  a pattern object.
+%
+%    params:  a structure specifying options for modifying the pattern.
+%             See below.
+%
+%  pat_name:  string identifier for the new pattern. If this is the same
+%             as pat.name, or if pat_name is not specified, the old
+%             pattern will be replaced.
+%
+%   res_dir:  directory where the new pattern will be saved.
+%
+%  OUTPUTS:
+%       pat:  a modified pattern object, named pat_name.
+%
+%   pattern:  the modified pattern.
+%
+%    events:  modified events.
+%
+%  PARAMS:
+%  Filtering
+%   eventFilter - input to filterStruct; used to filter the events dimension
+%   chanFilter  - filter for the channels dimension
+%   chan_filter - filter that uses filter_struct instead of filterStruct
+%   timeFilter  - filter for the time dimension
+%   freqFilter  - filter for the frequency dimension
+%
+%  Binning
+%   field          - input to make_event_bins
+%   eventbinlabels - cell array of strings, with one cell per bin, giving
+%                    a label for each event bin
+%   chanbins       - 
+%   chanbinlabels  - cell array of strings
+%   MSbins         - [N bins X 2] array, where MSbins(Y,1) gives the start
+%                    of bin Y in milliseconds, and MSbins(Y,2) gives the
+%                    end of the bin
+%   MSbinlabels    - cell array of strings
+%   freqbins       - bins in Hz for the frequency dimension, specified in 
+%                    the same format as MS bins
+%   freqbinlabels  - cell array of string labels
+%
+%  PCA
+%   nComp - number of principal components to extract
+%
+%  Slicing
+%   splitDim - dimension along which to split the pattern into slices;
+%              the pattern will be saved with one file per slice
+%
+%  EXAMPLE:
+%   % calculate averages for recalled and not recalled events
+%   params.field = 'recalled';
+%   pat = getobj(subj, 'pat', 'voltage');
+%
+%   % get the average for each unique value of the 'recalled' field
+%   % of events, and overwrite the old pattern
+%   pat = modify_pattern(pat, params);
 
-if isempty(pat)
+% input checks
+if ~exist('pat','var')
+  error('You must pass a pattern object.')
+  elseif ~isstruct(pat)
+  error('pat must be a structure.')
+  elseif isempty(pat)
   error('The input pat object is empty.')
 end
-if ~exist('patname','var') | isempty(patname)
+if ~exist('params','var')
+  params = struct;
+end
+if ~exist('pat_name','var') | isempty(pat_name)
   % default to overwriting the existing pattern
-  patname = pat.name;
+  pat_name = pat.name;
 end
 if ~exist('res_dir','var')
   % get the path to the pattern's file
@@ -25,12 +90,10 @@ if ~exist('res_dir','var')
     temp = pat.file;
   end
   % set the default results directory
-  res_dir = fullfile(fileparts(fileparts(fileparts(temp))), patname);
-end
-if ~exist('params','var')
-  params = struct;
+  res_dir = fullfile(fileparts(fileparts(fileparts(temp))), pat_name);
 end
 
+% default parameters
 params = structDefaults(params, ...
                         'nComp',           [], ...
                         'excludeBadChans', 0,  ...
@@ -42,10 +105,10 @@ params = structDefaults(params, ...
 
 oldpat = pat;
 
-% initialize the new pat object
-if ~strcmp(oldpat.name, patname)
-  % if the patname is different, save the pattern to a new file
-  patfile = fullfile(res_dir, 'patterns', objfilename('pattern', patname, pat.source));
+% set the file in which to save the new pattern
+if ~strcmp(oldpat.name, pat_name)
+  % if the pat_name is different, save the pattern to a new file
+  patfile = fullfile(res_dir, 'patterns', objfilename('pattern', pat_name, pat.source));
   else
   patfile = oldpat.file;
 end
@@ -64,7 +127,8 @@ catch err
   end
 end
 
-pat = init_pat(patname,patfile,oldpat.source,combineStructs(params,oldpat.params),oldpat.dim);
+% initialize the new pat object
+pat = init_pat(pat_name,patfile,oldpat.source,combineStructs(params,oldpat.params),oldpat.dim);
 if isfield(oldpat,'stat')
   pat.stat = oldpat.stat;
 end
@@ -117,7 +181,7 @@ pattern = patMeans(pattern, patbins);
 if ~isempty(params.nComp)
   % run PCA on the pattern
   [pat, pattern, coeff] = patPCA(pat, params, pattern);
-  coeffFile = fullfile(res_dir, 'patterns', objfilename('coeff', patname, pat.source));
+  coeffFile = fullfile(res_dir, 'patterns', objfilename('coeff', pat_name, pat.source));
   pat.dim.coeff = coeffFile;
   save(pat.dim.coeff, 'coeff');
 end
@@ -131,7 +195,7 @@ if params.savePat
     end
 
     % we need to save a new events struct
-    pat.dim.ev.file = fullfile(res_dir, 'events', objfilename('events', patname, pat.source));  
+    pat.dim.ev.file = fullfile(res_dir, 'events', objfilename('events', pat_name, pat.source));  
     save(pat.dim.ev.file, 'events');
   end
 
@@ -144,6 +208,4 @@ if params.savePat
     else
     pat.dim.splitdim = [];
   end
-
-  %closeFile(pat.file);
 end
