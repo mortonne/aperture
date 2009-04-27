5eb145af797ea99ea7d1d7e1d11ea3d5ee63eaef
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2009-04-27 14:56:46 +0000

    pat_topoplot: many added features and tweaks.

--- plotting/figures/pat_topoplot.m
+++ plotting/figures/pat_topoplot.m
@@ -78,13 +78,18 @@ params = structDefaults(params, ...
                         'plot_input',       {},                         ...
                         'map_limits',       [],                         ...
                         'print_input',      {'-depsc'},                 ...
+                        'figure_prop',      {},                         ...
+                        'axis_prop',        {},                         ...
+                        'colorbar',         true,                       ...
+                        'plot_perimeter',   true,                      ...
                         'event_bins',       '',                         ...
                         'diff',             false,                      ...
                         'mult_fig_windows', 0,                          ...
                         'stat_name',        '',                         ...
                         'p_range',          [0.005 0.05],               ...
                         'cap_type',         'HCGSN128',                 ...
-                        'correctm',         'fdr');
+                        'correctm',         'fdr',                      ...
+                        'correctm_scale',   'all');
 
 % load the pattern
 pattern = load_pattern(pat);
@@ -103,28 +108,18 @@ if ~isempty(params.stat_name)
   load(stat.file, 'p');
   
   % HACK - remove any additional p-values
-  p = p(1,:,:,:);
+  pattern = p(1,:,:,:);
   % END HACK
-  
-  sig = params.p_range(2); % threshold for significance
-  max_sig = params.p_range(1); % color gradient will max out here
-  
-  if ~isempty(params.correctm)
-    % correct for multiple comparisons across all samples
-    sig = correct_mult_comp(abs(p(:)), sig, params.correctm);
-    max_sig = correct_mult_comp(abs(p(:)), max_sig, params.correctm);
-    fprintf('Corrected for multiple comparisons using %s:\n', params.correctm)
-    fprintf('min alpha: %.8f\n', sig)
-    fprintf('max alpha: %.8f\n', max_sig)
-  end
-  
-  % make the colormap, set the pattern to be plotted as the
-  % z-scores of the p-values
-  [pattern, map, map_limits] = sig_colormap(p, [max_sig sig], 'two_way_signed');
-  colormap(map)
 
-  else
-  if params.diff || size(pattern,1)~=1
+  if strcmp(params.correctm_scale, 'all')
+    % make the colormap, set the pattern to be plotted as the
+    % z-scores of the p-values
+    [pattern, map, map_limits] = prep_sig_map(pattern, params.p_range, params.correctm, true);
+    colormap(map)
+  end
+
+else
+  if params.diff
     if size(pattern,1)~=2
       error('Can only take difference if there are two event types.')
     end
@@ -144,21 +139,40 @@ if ~isempty(params.stat_name)
 end
 
 % set the peripheral channels to the neutral color
-to_blank = perimeter_chans(params.cap_type);
+if params.plot_perimeter
+  to_blank = [];
+else
+  to_blank = perimeter_chans(params.cap_type);
+end
 
-fprintf('making topoplots from pattern %s...\n', pat.name)
 files = cell(size(pattern,1), 1, size(pattern,3), size(pattern,4));
+num_figs = prod(size(files));
+fprintf('making %d topoplots from pattern %s...\n', num_figs, pat.name)
 
-colorbar
 % one plot for each event/time/frequency
+n = 1;
 for e=1:size(pattern,1)
   for t=1:size(pattern,3)
     for f=1:size(pattern,4)
+      fprintf('%d ', n);
+      
       % get the slice to plot
       x = squeeze(pattern(e,:,t,f));
       
+      if ~isempty(params.stat_name) && strcmp(params.correctm_scale, 'fig')
+        % make the colormap, set the pattern to be plotted as the
+        % z-scores of the p-values
+        [x, map, map_limits] = prep_sig_map(x, params.p_range, params.correctm, true);
+        colormap(map)
+      end
+      
       % remove perimeter channels
       x(to_blank) = mean(map_limits);
+      
+      clf
+      if params.colorbar
+        colorbar
+      end
       
       % make the plot
       switch params.plot_type
@@ -172,12 +186,46 @@ for e=1:size(pattern,1)
         error('Invalid plot type: %s', params.plot_type)
       end
       
+      % set properties
+      if ~isempty(params.axis_prop)
+        set(gca, params.axis_prop{:});
+      end
+      if ~isempty(params.figure_prop)
+        set(gcf, params.figure_prop{:});
+      end
+      
       % write the filename
-      filename = sprintf('%s_%s_%s_e%dt%df%d.eps', fig_name, pat.name, pat.source, e,t,f);
+      filename = sprintf('%s_%s_%s_e%dt%df%d', fig_name, pat.name, pat.source, e,t,f);
       files{e,1,t,f} = fullfile(res_dir, filename);
       
       % print to file
       print(gcf, params.print_input{:}, files{e,1,t,f});
+      n = n + 1;
     end
   end
 end
+fprintf('\n')
+
+function [z,map,map_limits] = prep_sig_map(p, p_range, correctm, verbose)
+  if ~exist('verbose','var')
+    verbose = false;
+  end
+  
+  sig = p_range(2); % threshold for significance
+  max_sig = p_range(1); % color gradient will max out here
+  
+  if ~isempty(correctm)
+    % correct for multiple comparisons across all samples
+    sig = correct_mult_comp(abs(p(:)), sig, correctm);
+    max_sig = correct_mult_comp(abs(p(:)), max_sig, correctm);
+    if verbose
+      fprintf('Corrected for multiple comparisons using %s:\n', correctm)
+      fprintf('min alpha: %.8f\n', sig)
+      fprintf('max alpha: %.8f\n', max_sig)
+    end
+  end
+  
+  % make the colormap, set the pattern to be plotted as the
+  % z-scores of the p-values
+  [z, map, map_limits] = sig_colormap(p, [max_sig sig], 'two_way_signed');
+%endfunction
