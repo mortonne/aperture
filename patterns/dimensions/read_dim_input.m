9f29b540af50abbd12eb763ac099d928f5d9bab1
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2009-04-27 14:48:18 +0000

    read_dim_input: now also outputs long names.

--- patterns/dimensions/read_dim_input.m
+++ patterns/dimensions/read_dim_input.m
@@ -1,7 +1,7 @@
-function [dim_name,dim_number] = read_dim_input(dim_input)
+function [dim_name,dim_number,dim_long_name] = read_dim_input(dim_input)
 %READ_DIM_INPUT   Parse user input indicating a dimension.
 %
-%  [dim_name,dim_number] = read_dim_input(dim_input)
+%  [dim_name,dim_number,dim_long_name] = read_dim_input(dim_input)
 %
 %  INPUTS:
 %   dim_input:  either a string specifying the name of the dimension 
@@ -12,6 +12,9 @@ function [dim_name,dim_number] = read_dim_input(dim_input)
 %    dim_name:  name of the dimension.
 %
 %  dim_number:  number of the dimension.
+%
+%  dim_long_name:  long name of the dimension:
+%                  'Events', 'Channel', 'Time', 'Frequency'
 
 % input checks
 if ~exist('dim_input','var')
@@ -19,24 +22,27 @@ if ~exist('dim_input','var')
 end
 
 % process input
-dim_names = {'ev', 'chan', 'time', 'freq'};
+DIM_NAMES = {'ev', 'chan', 'time', 'freq'};
+DIM_LONG_NAMES = {'Event', 'Channel', 'Time', 'Frequency'};
 if isnumeric(dim_input)
   % input was dim_input number
   dim_number = dim_input;
 
   % get dim_input name
-  dim_name = dim_names{dim_input};
+  dim_name = DIM_NAMES{dim_input};
 
-  elseif ischar(dim_input)
+elseif ischar(dim_input)
   dim_name = dim_input;
   % make sure the name is valid
-  if ~ismember(dim_name, dim_names)
+  if ~ismember(dim_name, DIM_NAMES)
     error('Invalid dim_input name: %s', dim_input)
   end
 
   % get dim_input number
-  dim_number = find(strcmp(dim_name, dim_names));
+  dim_number = find(strcmp(dim_name, DIM_NAMES));
 
-  else
+else
   error('dim_input must be an integer or a string.')
 end
+
+dim_long_name = DIM_LONG_NAMES{dim_number};
