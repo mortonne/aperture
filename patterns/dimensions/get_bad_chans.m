9cbc22e6f61580326453949928cf59aa687da125
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2009-04-27 14:49:55 +0000

    get_bad_chans: now can use multiple bad chans files, calls read_chans_file.

--- patterns/dimensions/get_bad_chans.m
+++ patterns/dimensions/get_bad_chans.m
@@ -1,11 +1,15 @@
-function [bad_chans,eeg_ind] = get_bad_chans(eeg_files)
+function [bad_chans,eeg_ind] = get_bad_chans(eeg_files,bad_chan_files)
 %GET_BAD_CHANS   Get a list of bad channels for given EEG files.
 %
-%  [bad_chans, eeg_ind] = get_bad_chans(eeg_files)
+%  [bad_chans, eeg_ind] = get_bad_chans(eeg_files, bad_chan_files)
 %
 %  INPUTS:
 %  eeg_files:  a cell array of roots for EEG filenames.
 %              e.g. {session_0/eeg/eeg.reref/LTP001_13Mar08_1417, ...}
+%
+%  bad_chan_files:  cell array of paths to bad channel files. Paths must
+%              be relative to the parent directory of each EEG file.
+%              Default: {'bad_chan.txt'}
 %
 %  OUTPUTS:
 %  bad_chans:  a cell array with one cell for each unique string in
@@ -23,6 +27,17 @@ function [bad_chans,eeg_ind] = get_bad_chans(eeg_files)
 %   % get a list of bad channels for event 23:
 %   bc = bad_chans{eeg_ind(23)};
 
+% input checks
+if ~exist('eeg_files','var') || ~iscell(eeg_files)
+  error('You must pass a cell array of EEG files.')
+end
+if ~exist('bad_chan_files','var')
+  bad_chan_files = {'bad_chan.txt'};
+end
+if ~iscell(bad_chan_files)
+  bad_chan_files = {bad_chan_files};
+end
+
 % get all unique EEG files
 uniq_eeg_files = unique(eeg_files);
 
@@ -32,14 +47,30 @@ eeg_ind = NaN(1,length(eeg_files));
 
 % get bad channels for each unique EEG file
 for i=1:length(uniq_eeg_files)
+  % write out the indices for this EEG file
+  eeg_ind(strcmp(uniq_eeg_files{i}, eeg_files)) = i;
+  
   % assume that we passed in something like the eegfile field in the
   % standard events structure
   eeg_dir = fileparts(uniq_eeg_files{i});
-  
+
   % read in the channels
-  temp = textscan(fopen(fullfile(eeg_dir, 'bad_chan.txt')), '%d');
-  bad_chans{i} = temp{1};
-  
-  % write out the indices for this EEG file
-  eeg_ind(strcmp(uniq_eeg_files{i}, eeg_files)) = i;
+  bad_chans{i} = read_bad_channels(eeg_dir, bad_chan_files);
 end
+
+
+function bad_channels = read_bad_channels(eeg_dir,bad_chan_files)
+  bad_channels = [];
+  for i=1:length(bad_chan_files)
+    fname = fullfile(eeg_dir, bad_chan_files{i});
+    if ~exist(fname,'file')
+      fprintf('Warning: bad channels file not found: %s\n', fname)
+      continue
+    end
+    
+    chans = read_chans_file(fname);
+    bad_channels = [bad_channels, chans];
+  end
+  
+  bad_channels = unique(bad_channels);
+%endfunction
