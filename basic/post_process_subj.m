0cef1f2267988236e0caaa1f9f18afcbdf089a4d
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2009-04-27 14:52:49 +0000

    post_process_subj: added error printing

--- basic/post_process_subj.m
+++ basic/post_process_subj.m
@@ -11,6 +11,12 @@ function subj = post_process_subj(subj,varargin)
 
 % do any necessary post-processing, save events file for each subj
 for sess=subj.sess
-  % split, sync, rereference, detect blink artifacts
-  prep_egi_data2(subj.id,sess.dir,varargin{:});
+  try
+    % split, sync, rereference, detect blink artifacts
+    prep_egi_data2(subj.id,sess.dir,varargin{:});
+  catch err
+    warning('eeg_ana:post_process_subj:SessError', ...
+            'prep_egi_data2 threw an error for %s session %d:\n %s', ...
+            subj.id, sess.number, getReport(err))
+  end
 end
