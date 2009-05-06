17d5443f00cadfe5a7cf72c65b2fcf24000664cc
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2009-05-06 22:28:00 +0000

    added functions for running a grid search.

---
+++ utils/grid2params.m
@@ -0,0 +1,50 @@
+function params = grid2params(search_params,grid)
+%GRID2PARAMS   Get optimum parameters from the results of a
+%              grid search.
+%
+%  params = grid2params(search_params, grid)
+%
+%  Find the winning cell in the grid, and return a structure
+%  with the corresponding parameters. It is assumed that the
+%  values in the grid matrix are values like RMSD where
+%  smaller values indicate a better fit. To select the
+%  largest value instead, change the sign of grid before 
+%  passing it.
+%
+%  INPUTS:
+%  search_params:  structure containing the ranges of
+%                  parameters used in the grid search. Each
+%                  field should be a cell array giving the
+%                  values that were searched over.
+%
+%           grid:  matrix of values corresponding to some
+%                  measure of fitness. Should have one
+%                  dimension for each field of search_params.
+%
+%  OUTPUTS:
+%         params:  structure with the optimum parameters. If 
+%                  there was a tie for the best set of 
+%                  parameters, params will have one element
+%                  for each winning set.
+%
+%  See also grid_search.
+
+% input checks
+if ~exist('search_params','var') || ~isstruct(search_params)
+  error('You must pass a structure with the range of parameters in the search.')
+elseif ~exist('grid','var') || ~isnumeric(grid)
+  error('You must pass a matrix of fitness values.')
+end
+
+values = struct2cell(search_params);
+names = fieldnames(search_params);
+
+% find the winning spot in the grid
+[ind{1:length(values)}] = ind2sub(size(grid), find(grid==min(grid(:))));
+
+% create the winning parameters
+for i=1:length(values)
+  for j=1:length(ind{i})
+    params(j).(names{i}) = values{i}{ind{i}(j)};
+  end
+end
---
+++ utils/grid_search.m
@@ -0,0 +1,108 @@
+function fit = grid_search(params, fit_fcn, fit_inputs)
+%GRID_SEARCH   Run a grid search of parameter space.
+%
+%  fit = grid_search(params, fit_fcn, fit_inputs)
+%
+%  INPUTS:
+%      params:  structure of parameters, where each field
+%               corresponds to one parameter, and gives a
+%               cell array of values to search over for
+%               that parameter.
+%
+%     fit_fcn:  function that takes a params structure as
+%               the first input, and returns one number.
+%
+%  fit_inputs:  cell array of additional inputs to fit_fcn.
+%
+%  OUTPUTS:
+%         fit:  matrix with one element for each combination
+%               of parameters in the params structure. Contains
+%               the output from fit_fcn for each cell of the
+%               grid.
+%
+%  EXAMPLE:
+%   % define ranges for the 'a', 'b', and 'c' parameters
+%   params = struct('a',{num2cell(1:3)}, 'b',{{2}}, 'c',{num2cell(1:4)});
+%
+%   % define a function that produces a value for each combination
+%   % of parameters (this function just gets the sum)
+%   fit_fcn = @(x)(sum(cell2mat(struct2cell(x))));
+%
+%   % evaluate fit_fcn for all combinations of the parameters
+%   fit = grid_search(params, fit_fcn);
+%
+%  See also grid2params.
+
+% input checks
+if ~exist('params','var')
+  error('You must pass a params structure.')
+elseif any(~cellfun(@iscell, struct2cell(params)))
+  error('Each search parameter field must be a cell array.')
+elseif ~exist('fit_fcn','var')
+  error('You must pass a function to evaluate on each cell of the grid.')
+end
+if ~exist('fit_inputs','var')
+  fit_inputs = {};
+end
+
+% flatten the variables structure
+values = struct2cell(params);
+names = fieldnames(params);
+
+% prepare the job
+sm = findResource;
+j.job = createJob;
+
+% initialize a cell array of grid indices
+i = repmat({1},1,length(values));
+
+% run for-loops over all dimensions of the grid, and create
+% a task for each cell
+j = grid(j, i, 1, values, fit_fcn, fit_inputs, cell(1,length(values)), names);
+
+% initialize the output grid
+% removing so fit can be an array or a struct array
+%fit = NaN(size(j.id));
+
+% run all cells
+submit(j.job);
+wait(j.job);
+
+% put the outputs into grid format
+out = getAllOutputArguments(j.job);
+for i=1:length(out)
+  fit(j.id==i) = out{i};
+end
+
+function j = grid(j, i, n, c, f, f_in, values, names)
+  % j - job
+  % i - indices in the grid
+  % n - variable number
+  % c - cell array of possible values
+  % f - function to be evaluated for each cell of the grid
+  % f_in - additional inputs
+  % values - cell array of valus for this iteration of
+  %          all the for loops
+  % names - cell array of variable names
+  
+  % loop over each value of this variable
+  for k=1:length(c{n})
+    % get this value
+    values{n} = c{n}{k};
+    i{n} = k;
+    
+    if n<length(c)
+      % continue down the var cell...
+      j = grid(j, i, n+1, c, f, f_in, values, names);
+    else
+      % this is the last variable
+      params = cell2struct(values, names, 2);
+
+      % create a task for this cell
+      t = createTask(j.job, f, 1, {params, f_in{:}});
+      j.id(i{:}) = t.ID;
+      
+      %j(i{:}) = f(params, f_in{:});
+    end
+  end
+%endfunction
