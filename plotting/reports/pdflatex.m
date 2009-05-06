6ce0ec1610bdfd738f6785826defa749504a183d
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2009-05-06 22:19:53 +0000

    pdflatex: added docstring, now can compile using pdflatex.

--- plotting/reports/pdflatex.m
+++ plotting/reports/pdflatex.m
@@ -1,11 +1,27 @@
-function pdf_file = pdflatex(latex_file)
+function pdf_file = pdflatex(latex_file,compile_method)
 %PDFLATEX   Compile a LaTeX file to make a PDF.
 %
-%  pdf_file = pdflatex(latex_file)
+%  pdf_file = pdflatex(latex_file, compile_method)
+%
+%  INPUTS:
+%      latex_file:  LaTeX file. A .tex extension is optional.
+%
+%  compile_method:  method to use when compiling the file:
+%                    'pdflatex' - use when you have non-eps
+%                    graphics files. 
+%                    'latexdvipdf' - use when you have .eps
+%                    files, and no other types of graphics.
+%                    (Default)
+%
+%  OUTPUTS:
+%        pdf_file:  path to the compiled PDF file.
 
 % input checks
 if ~exist(latex_file,'file')
   error('%s not found.', latex_file)
+end
+if ~exist('compile_method','var')
+  compile_method = 'latexdvipdf';
 end
 
 % strip away the file extension
@@ -22,27 +38,20 @@ end
 
 fprintf('compiling %s...', latex_file)
 
-% compile the LaTeX code
-max_tries = 10;
-tries = 0;
-run = true;
-while run && tries < max_tries
-  % run latex
-  command = sprintf('latex %s | grep "Rerun LaTeX"', latex_file);
-  [s,w] = unix(command);
+switch compile_method
+  case 'latexdvipdf'
+  compile(latex_file, 'latex');
   
-  if isempty(w) % success!
-    run = false;
-    else % latex says we need to run again
-    fprintf('recompiling...')
-    tries = tries + 1;
-  end
+  % make a PDF
+  command = sprintf('dvipdf %s', latex_file);
+  unix(command);
+  
+  case 'pdflatex'
+  compile(latex_file, 'pdflatex');
+  
+  otherwise
+  error('Unknown compiling method: %s', compile_method)
 end
-fprintf('\n')
-
-% make a PDF
-command = sprintf('dvipdf %s', latex_file);
-unix(command);
 
 % pass out the filename to the PDF
 pdf_file = sprintf('%s.pdf', latex_file);
@@ -51,3 +60,31 @@ if ~exist(pdf_file,'file')
 end
 
 cd(pd);
+
+function compile(latex_file, compile_command, max_tries)
+  % input checks
+  if ~exist('latex_file','var')
+    error('You must pass a LaTeX file to compile.')
+  elseif ~exist('compile_command','var')
+    error('You must pass a command to run.')
+  end
+  if ~exist('max_tries','var')
+    max_tries = 3;
+  end
+  
+  tries = 0;
+  run = true;
+  while run && tries < max_tries
+    % run the command
+    command = sprintf('%s %s | grep "Rerun LaTeX"', compile_command, latex_file);
+    [s,w] = unix(command);
+
+    if isempty(w) % success!
+      run = false;
+    else % latex says we need to run again
+      fprintf('recompiling...')
+      tries = tries + 1;
+    end
+  end
+  fprintf('\n')
+%endfunction
