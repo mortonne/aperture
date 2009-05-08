92ec3d1e474c90c46cc649f3e36f1006e7b5c9a7
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2009-05-08 03:54:09 +0000

    copy_exp: new docstring, input checks.

--- basic/copy_exp.m
+++ basic/copy_exp.m
@@ -1,13 +1,31 @@
-function exp = copy_exp(exp,resdir)
-%COPY_EXP   Make a copy of an exp structure.
-%   EXP = COPY_EXP(EXP,RESDIR) saves a copy of the exp structure EXP
-%   in resdir/exp.mat.  
+function exp = copy_exp(exp,res_dir)
+%COPY_EXP   Make a copy of an experiment object.
+%
+%  exp = copy_exp(exp, res_dir)
+%
+%  INPUTS:
+%      exp:  an experiment object.
+%
+%  res_dir:  path to the directory to save the copy of exp.
+%
+%  OUTPUTS:
+%      exp:  copied experiment object, with the 'file' and 'resDir'
+%            fields updated.
+
+% input checks
+if ~exist('exp','var') || ~isstruct(exp)
+  error('You must pass an experiment object.')
+elseif ~exist('res_dir','var') || ~ischar(res_dir)
+  error('You must give the path to a results directory.')
+elseif ~exist(res_dir,'dir')
+  error('Directory does not exist: %s', res_dir)
+end
 
 % change the resdir field
-exp.resDir = resdir;
+exp.resDir = res_dir;
 
 % change the file field that indicates the .mat file where exp is saved
-exp.file = fullfile(resdir,'exp.mat');
+exp.file = fullfile(res_dir, 'exp.mat');
 
-% update
+% save the new copy
 exp = update_exp(exp);
