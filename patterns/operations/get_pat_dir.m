be4c98394d7407e03fce8fee60fadd6b3707d2a4
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2009-05-08 03:58:39 +0000

    get_pat_dir

---
+++ patterns/operations/get_pat_dir.m
@@ -0,0 +1,47 @@
+function pat_dir = get_pat_dir(pat, subdir)
+%GET_PAT_DIR   Get the path to a pattern's standard directory.
+%
+%  pat_dir = get_pat_dir(pat, subdir)
+%
+%  It is assumed that the pattern's files are saved in
+%  [pat_dir]/patterns.
+%
+%  INPUTS:
+%      pat:  a pattern object.
+%
+%   subdir:  string name of a subdirectory of the pattern's
+%            main directory to return. If subdir does not
+%            exist, it will be made. Default: ''
+%
+%  OUTPUTS:
+%  pat_dir:  path to the requested pattern directory.
+%
+%  EXAMPLE:
+%   % get the path to the standard directory for a pattern's
+%   % reports
+%   report_dir = get_pat_dir(pat, 'reports');
+
+% input checks
+if ~exist('pat','var') || ~isstruct(pat)
+  error('You must pass a pattern object.')
+elseif ~isfield(pat,'file')
+  error('pat must have a "file" field.')
+end
+if ~exist('subdir','var')
+  subdir = '';
+end
+
+% get one of this pattern's files
+if iscell(pat.file)
+  pat_file = pat.file{1};
+else
+  pat_file = pat.file;
+end
+
+% get the requested directory
+pat_dir = fullfile(fileparts(fileparts(pat_file)), subdir);
+
+% make sure it exists
+if ~exist(pat_dir,'dir')
+  mkdir(pat_dir)
+end
