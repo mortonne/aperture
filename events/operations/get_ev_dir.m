7120e62cfd27f480386034ec12a27405b11a08ec
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2009-06-06 00:28:35 +0000

    added get_ev_dir

---
+++ events/operations/get_ev_dir.m
@@ -0,0 +1,35 @@
+function ev_dir = get_ev_dir(ev, subdir)
+%GET_EV_DIR   Get the path to an event structure's standard directory.
+%
+%  ev_dir = get_ev_dir(ev, subdir)
+%
+%  It is assumed that the events structure's files are saved in
+%  [ev_dir]/events.
+%
+%  INPUTS:
+%       ev:  an events object.
+%
+%   subdir:  string name of a subdirectory of the pattern's
+%            main directory to return. If subdir does not
+%            exist, it will be made. Default: ''
+%
+%  OUTPUTS:
+%   ev_dir:  path to the requested events directory.
+
+% input checks
+if ~exist('ev','var') || ~isstruct(ev)
+  error('You must pass a pattern object.')
+elseif ~isfield(ev,'file')
+  error('ev must have a "file" field.')
+end
+if ~exist('subdir','var')
+  subdir = '';
+end
+
+% get the requested directory
+ev_dir = fullfile(fileparts(fileparts(ev.file)), subdir);
+
+% make sure it exists
+if ~exist(ev_dir,'dir')
+  mkdir(ev_dir)
+end
