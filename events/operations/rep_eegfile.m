dbcbc8f30be0c8b66551855e455c03be20c01b9e
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2009-06-06 00:28:11 +0000

    added rep_eegfile.

---
+++ events/operations/rep_eegfile.m
@@ -0,0 +1,40 @@
+function events = rep_eegfile(events, varargin)
+%REP_EEGFILE   Run strrep on the eegfile field of an events structure.
+%
+%  events = rep_eegfile(events, X1, X2, Y1, Y2, ...)
+%
+%  INPUTS:
+%    events:  an events structure. Must have an "eegfile" field.
+%
+%  varargin:  any number of string pairs, where the first is the string
+%             to replace, and the second is the string to replace it
+%             with.
+%
+%  OUTPUTS:
+%    events:  events structure with a modified eegfile field.
+%
+%  EXAMPLE:
+%   % change the EEG file field to point to rereferenced data
+%   events = rep_eegfile(events, 'eeg.noreref', 'eeg.reref');
+
+% input checks
+if ~exist('events','var') || ~isstruct(events)
+  error('You must pass an events structure.')
+elseif ~isfield(events, 'eegfile')
+  error('events must have an "eegfile" field.')
+elseif mod(length(varargin),2)~=0
+  error('length of varargin must be a multiple of two.')
+end
+
+% get the EEG file field
+eegfile = {events.eegfile};
+
+% run strrep on each input pair
+for i=1:2:length(varargin)
+  to_replace = varargin{i};
+  replacement = varargin{i+1};
+  eegfile = strrep(eegfile, to_replace, replacement);
+end
+
+% add the modified field back in
+[events.eegfile] = eegfile{:};
