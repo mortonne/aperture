c5b780814a34637cc02a46f752e7d4784ecba24e
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2009-06-08 21:01:11 +0000

    patsize: added option of returning specified dimension.

--- patterns/dimensions/patsize.m
+++ patterns/dimensions/patsize.m
@@ -1,9 +1,38 @@
-function psize = patsize(dim)
-%PATSIZE   Get the size of a pattern from its dim struct.
-%   PSIZE = PATSIZE(DIM) returns the size of the pattern
-%   corresponding to DIM.
+function D = patsize(dim_info,dim)
+%PATSIZE   Get the size of a pattern from its dim structure.
+%
+%  D = patsize(dim_info, dim)
+%
+%  INPUTS:
+%  dim_info:  structure containing information about the
+%             dimensions of a pattern. Created by
+%             create_pattern.
+%
+%       dim:  optional; the dimension to return. If omitted,
+%             an array with the size of each dimension is
+%             returned. Can be either the number of a
+%             dimension in the pattern matrix, or the name
+%             of one the dimensions ('ev','chan','time','freq').
+%
+%  OUTPUTS:
+%         D:  an array with the size of the pattern.
 
-psize(1) = dim.ev.len;
-psize(2) = length(dim.chan);
-psize(3) = length(dim.time);
-psize(4) = length(dim.freq);
+% input checks
+if ~exist('dim_info','var') || ~isstruct(dim_info)
+  error('You must pass a dim structure.')
+end
+if ~exist('dim','var')
+  dim = [];
+end
+
+% mapping between fields and dimension numbers
+D(1) = dim_info.ev.len;
+D(2) = length(dim_info.chan);
+D(3) = length(dim_info.time);
+D(4) = length(dim_info.freq);
+
+% return the requested dimension size
+if ~isempty(dim)
+  [name,number] = read_dim_input(dim);
+  D = D(number);
+end
