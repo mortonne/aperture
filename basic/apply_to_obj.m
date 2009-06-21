682f46b7f98721b7bfce98948a15bf49e3a226bf
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2009-06-21 08:32:41 +0000

    apply_to_obj: updated implementation and docstring.

--- basic/apply_to_obj.m
+++ basic/apply_to_obj.m
@@ -4,9 +4,9 @@ function s_out = apply_to_obj(s,obj_type,obj_name,fcn_handle,fcn_inputs)
 %  s_out = apply_to_obj(s, obj_type, obj_name, fcn_handle, fcn_inputs)
 %
 %  In many cases you need to run a function on a child object, then
-%  immediately update the parent object with the modified child.  For example,
-%  you might want to modify a pat object, then update it's parent subj
-%  object.
+%  immediately update the parent object with the modified child.  For
+%  example, you might want to modify a pat object, then update its
+%  parent subj object.
 %
 %  INPUTS:
 %           s:  a structure
@@ -14,19 +14,19 @@ function s_out = apply_to_obj(s,obj_type,obj_name,fcn_handle,fcn_inputs)
 %    obj_type:  the "type" of object that you want to modify.  Must be a
 %               subfield of s.
 %
-%    obj_name:  the string identifier for the object that you want to modify;
-%               this should be the same as the "name" field of the object.
+%    obj_name:  the string identifier for the object that you want to
+%               modify.
 %
-%  fcn_handle:  handle to a function to input the object.  The first input
-%               should be the object, and the first output should be an object
-%               of the same type.
+%  fcn_handle:  handle to a function to input the object.  The first
+%               input should be the object, and the first output should
+%               be an object of the same type.
 %
-%  fcn_inputs:  a cell array of additional inputs to fcn_handle (the object
-%               is the first input).
+%  fcn_inputs:  a cell array of additional inputs to fcn_handle (the
+%               object is the first input).
 %
 %  OUTPUTS:
-%       s_out:  same a s, but the object specified by obj_type and obj_name
-%               is modified.
+%       s_out:  same a s, but the object specified by obj_type and
+%               obj_name is modified.
 %
 %  EXAMPLES:
 %   % we want to modify one subject's pattern named 'voltage' using the
@@ -35,45 +35,37 @@ function s_out = apply_to_obj(s,obj_type,obj_name,fcn_handle,fcn_inputs)
 %
 %  See also applytosubj, apply_to_pat.
 
+% input checks  
+if ~exist('s','var')
+  error('You must pass an object whose child object you want to modify.')
+elseif ~exist('obj_type','var')
+  error('You must specify an object type.')
+elseif ~exist('obj_name','var')
+  error('You must give the name of an object.')
+elseif ~exist('fcn_handle','var')
+  error('You must specify a function to run.')
+end
 if ~exist('fcn_inputs','var')
   fcn_inputs = {};
-end
-
-if ~exist('fcn_handle','var')
-  error('You must specify a function to run.')
-  elseif ~exist('obj_name','var')
-  error('You must give the name of an object.')
-  elseif ~exist('obj_type','var')
-  error('You must specify an object type.')
-  elseif ~exist('s','var')
-  error('You must pass an object whose child object you want to modify.')
 end
 
 % if we error out, return the unmodified structure
 s_out = s;
 
 % get the object we want to modify
-obj = getobj(s,obj_type,obj_name);
-
-if isempty(obj)
+try
+  obj = getobj(s, obj_type, obj_name);
+catch
   % couldn't find it
   warning('%s object %s not found.', obj_type, obj_name)
   return
 end
 
 % set the source field of the object
-if isfield(s,'name')
-  obj.source = s.name;
-  elseif isfield(s,'id')
-  obj.source = s.id;
-  elseif isfield(s,'experiment')
-  obj.source = s.experiment;
-  else
-  error('structure has no name field.')
-end
+obj.source = get_obj_name(s);
 
 % run the function
-obj = fcn_handle(obj,fcn_inputs{:});
+obj = fcn_handle(obj, fcn_inputs{:});
 
 % update the structure with the modified object
-s_out = setobj(s,obj_type,obj);
+s_out = setobj(s, obj_type, obj);
