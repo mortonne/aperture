4629e99fd49d4a6d822f8f055e351352d69b0700
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2009-06-21 06:14:26 +0000

    grid2params: updated style of docstring.

--- utils/grid2params.m
+++ utils/grid2params.m
@@ -1,31 +1,27 @@
 function params = grid2params(search_params,grid)
-%GRID2PARAMS   Get optimum parameters from the results of a
-%              grid search.
+%GRID2PARAMS   Get optimum parameters from the results of a grid search.
 %
 %  params = grid2params(search_params, grid)
 %
-%  Find the winning cell in the grid, and return a structure
-%  with the corresponding parameters. It is assumed that the
-%  values in the grid matrix are values like RMSD where
-%  smaller values indicate a better fit. To select the
-%  largest value instead, change the sign of grid before 
-%  passing it.
+%  Find the winning cell in the grid, and return a structure with the
+%  corresponding parameters. It is assumed that the values in the grid
+%  matrix are values like RMSD where smaller values indicate a better
+%  fit. To select the largest value instead, change the sign of grid
+%  before passing it.
 %
 %  INPUTS:
-%  search_params:  structure containing the ranges of
-%                  parameters used in the grid search. Each
-%                  field should be a cell array giving the
-%                  values that were searched over.
+%  search_params:  structure containing the ranges of parameters used
+%                  in the grid search. Each field should be a cell array
+%                  giving the values that were searched over.
 %
-%           grid:  matrix of values corresponding to some
-%                  measure of fitness. Should have one
-%                  dimension for each field of search_params.
+%           grid:  matrix of values corresponding to some measure of 
+%                  fitness. Should have one dimension for each field of
+%                  search_params.
 %
 %  OUTPUTS:
-%         params:  structure with the optimum parameters. If 
-%                  there was a tie for the best set of 
-%                  parameters, params will have one element
-%                  for each winning set.
+%         params:  structure with the optimum parameters. If there was
+%                  a tie for the best set of parameters, params will
+%                  have one element for each winning set.
 %
 %  See also grid_search.
 
