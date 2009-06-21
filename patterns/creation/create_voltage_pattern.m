c6bc5c95dad024312725c2a72bca602941515080
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2009-06-21 06:04:22 +0000

    minor change.

--- patterns/creation/create_voltage_pattern.m
+++ patterns/creation/create_voltage_pattern.m
@@ -8,10 +8,10 @@ function subj = create_voltage_pattern(subj, pat_name, params, res_dir)
 %
 %  pat_name:  string identifier for the pattern.
 %
-%    params:  structure that specifies options for pattern creation. See below 
+%    params:  structure that specifies options for pattern creation. See below
 %             for options.
 %
-%   res_dir:  path to the directory to save results. patterns will be saved in 
+%   res_dir:  path to the directory to save results. patterns will be saved in
 %             [res_dir]/patterns; if events are modified, new events will be 
 %             saved in [res_dir]/events.
 %
@@ -23,13 +23,14 @@ function subj = create_voltage_pattern(subj, pat_name, params, res_dir)
 %  All fields are optional. Defaults are shown in parentheses.
 %  Events
 %   evname          - name of the events object to use. ('events')
-%   replace_eegfile - [N X 2] cell array, where each row contains two strings to 
-%                     be passed into strrep, to change the eegfile field in 
+%   replace_eegfile - [N X 2] cell array, where each row contains two strings
+%                     to be passed into strrep, to change the eegfile field in
 %                     events ({})
 %   eventFilter     - input to filterStruct which designates which events to 
 %                     include in the pattern ('')
-%   kthresh         - kurtosis threshold; scalar indicating the maximum allowable 
-%                     kurtosis for an event before it is˜excluded (5)
+%   kthresh         - kurtosis threshold; scalar indicating the maximum 
+%                     allowable kurtosis for an event before it is excluded 
+%                     (5)
 %
 %  Channels
 %   chanFilter      - used to choose which channels to include in the pattern. 
@@ -41,8 +42,8 @@ function subj = create_voltage_pattern(subj, pat_name, params, res_dir)
 %   offsetMS        - time in milliseconds before each event to start the 
 %                     pattern (-200)
 %   durationMS      - duration in milliseconds of each epoch (2200)
-%   relativeMS      - range of times relative to the start of each event to use 
-%                     for baseline subtraction ([-200 0])
+%   relativeMS      - range of times relative to the start of each event to 
+%                     use for baseline subtraction ([-200 0])
 %
 %  Filtering
 %   filttype        - type of filter to use (see buttfilt) ('stop')
@@ -51,23 +52,27 @@ function subj = create_voltage_pattern(subj, pat_name, params, res_dir)
 %   bufferMS        - buffer in milliseconds to use during filtering (1000)
 %
 %  Z-transforming
-%   ztransform      - logical; if true, voltage will be z-transformed relative to 
-%                     the baseline for that session/channel (false)
-%   baseEventFilter - input to filterStruct; designates which events to include for 
-%                     calculating the baseline (params.eventFilter)
+%   ztransform      - logical; if true, voltage will be z-transformed relative
+%                     to the baseline for that session/channel (false)
+%   baseEventFilter - input to filterStruct; designates which events to
+%                     include for calculating the baseline 
+%                     (params.eventFilter)
 %   baseOffsetMS    - time before each baseline event to start the baseline 
 %                     period (-200)
 %   baseDurationMS  - duration of each baseline event epoch (200)
-%   baseRelativeMS  - range of times relative to the start of each event to use for 
-%                     baseline subtraction (params.relativeMS)
+%   baseRelativeMS  - range of times relative to the start of each event to 
+%                     use for baseline subtraction (params.relativeMS)
 %
 %  File Management
-%   lock            - if true, the pattern's file will be locked during pattern 
-%                     creation, and unlock when the program finishes. Useful for 
-%                     processing multiple subjects in parallel on a cluster. (false)
-%   overwrite       - if true, existing pattern files will be overwritten (false)
-%   updateOnly      - if true, the pattern will not be created, but a pattern object 
-%                     will be created and attached to the subject object (false)
+%   lock            - if true, the pattern's file will be locked during 
+%                     pattern creation, and unlock when the program finishes.
+%                     Useful for processing multiple subjects in parallel on a
+%                     cluster. (false)
+%   overwrite       - if true, existing pattern files will be overwritten 
+%                     (false)
+%   updateOnly      - if true, the pattern will not be created, but a pattern
+%                     object will be created and attached to the subject 
+%                     object (false)
 %
 %   See also create_power_pattern.
 
