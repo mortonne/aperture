ff8f2b42bab58c4b4d475304a4493a9685cc6fd1
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2009-09-25 23:56:21 +0000

    classify_pat: added support for classifying whole patterns, changed from pc to stat objects.

--- patclass/classify_pat.m
+++ patclass/classify_pat.m
@@ -1,20 +1,22 @@
-function pat = classify_pat(pat, params, pc_name, res_dir)
+function pat = classify_pat(pat, params, stat_name, res_dir)
 %CLASSIFY_PAT   Run a pattern classifier on a pattern.
 %
-%  pat = classify_pat(pat, params, pc_name, res_dir)
+%  pat = classify_pat(pat, params, stat_name, res_dir)
 %
 %  INPUTS:
-%      pat:  a pattern object.
+%        pat:  a pattern object.
 %
-%   params:  structure with options for the classifier.  See below for
-%            options.
+%     params:  structure with options for the classifier.  See below for
+%              options.
 %
-%  pc_name:  string identifier of the new pattern classification object.
+%  stat_name:  string identifier of the new stat object which will hold
+%              the classification results.  Default: 'patclass'
 %
-%  res_dir:  directory where results will be saved.
+%    res_dir:  directory where results will be saved.  If not specified,
+%              results will be saved in the pattern's stats directory.
 %
 %  OUTPUTS:
-%      pat:  modified pattern object with an added pc object.
+%        pat:  modified pattern object with an added stat object.
 %
 %  PARAMS:
 %   regressor  - REQUIRED - input to make_event_bins; used to create the
@@ -23,13 +25,13 @@ function pat = classify_pat(pat, params, pc_name, res_dir)
 %                indices for cross-validation.
 %   classifier - string indicating the type of classifier to use.  See
 %                run_classifier for available classifiers and options.
-%                Default: 'classify'
+%                ('classify')
 %   scramble   - boolean; if true, the regressor will be scrambled before
-%                classification.  Useful for debugging.  Default: false
-%   overwrite  - if true, existing pc files will be overwritten.
-%                Default: false
+%                classification.  Useful for debugging.  (false)
+%   overwrite  - if true, existing pc files will be overwritten. (true)
 %   iter_dims  - vector of which dimensions the classification
-%                should iterate over.  Default?
+%                should iterate over.  If empty, all features of the
+%                pattern will be used in one classification run. ([])
 %
 %  EXAMPLE:
 %   % classify based on subsequent memory
@@ -48,12 +50,12 @@ if ~exist('pat','var') || ~isstruct(pat)
 elseif ~exist('params','var') || ~isstruct(params)
   error('You must pass a params structure.')
 elseif ~isfield(params, 'regressor')
-	error('You must specify a regressor in params.')
+  error('You must specify a regressor in params.')
 elseif ~isfield(params, 'selector')
-	error('You must specify a selector in params.')
+  error('You must specify a selector in params.')
 end
-if ~exist('pc_name', 'var')
-	pc_name = 'patclass';
+if ~exist('stat_name', 'var')
+  stat_name = 'patclass';
 end
 if ~exist('res_dir', 'var')
   res_dir = get_pat_dir(pat, 'patclass');
@@ -64,20 +66,19 @@ params = structDefaults(params, ...
                         'iter_dims',  [],         ...
                         'scramble',   0,          ...
                         'lock',       0,          ...
-                        'overwrite',  1,          ...
-                        'select_test',1);
+                        'overwrite',  1);
 
 % set where the results will be saved
-filename = sprintf('%s_%s_%s.mat', pat.name, pc_name, pat.source);
-pc_file = fullfile(res_dir, filename);
+filename = sprintf('%s_%s_%s.mat', pat.name, stat_name, pat.source);
+stat_file = fullfile(res_dir, filename);
 
 % check the output file
-if ~params.overwrite && exist(pc_file, 'file')
+if ~params.overwrite && exist(stat_file, 'file')
   return
 end
 
 % initialize the stat object
-stat = init_stat(pc_name, pc_file, pat.name, params);
+stat = init_stat(stat_name, stat_file, pat.name, params);
 
 % load the pattern and corresponding events
 pattern = load_pattern(pat, params);
@@ -95,22 +96,31 @@ end
 % get the selector
 selector = make_event_bins(events, params.selector);
 
-% run pattern classification separately for each time and frequency bin
-res = apply_by_slice(@xval, {pattern}, params.iter_dims, ...
-		     {selector, targets, params}, ...
-		     'uniform_output', false);
+if isempty(params.iter_dims)
+  % use all features for classification
+  res = xval(pattern, selector, targets, params);
+  
+  % put the iterations on the first dimension
+  res.iterations = res.iterations';
+else
+  % run pattern classification separately for each value on the iter_dims
+  res = apply_by_slice(@xval, {pattern}, params.iter_dims, ...
+                       {selector, targets, params}, ...
+                       'uniform_output', false);
+  
+  % fix the res structure
+  res_size = size(res);
+  res_fixed_size = [length(res{1}.iterations) res_size(2:end)];
 
-% fix the res structure
-res_size = size(res);
-res_fixed_size = [length(res{1}.iterations) res_size(2:end)];
-
-cell_vec = [res{:}];
-struct_vec = [cell_vec.iterations];
-res_fixed.iterations = reshape(struct_vec, res_fixed_size);
-res = res_fixed;
+  cell_vec = [res{:}];
+  struct_vec = [cell_vec.iterations];
+  res_fixed.iterations = reshape(struct_vec, res_fixed_size);
+  res = res_fixed;
+end
 
 % save the results
 save(stat.file, 'res');
 
+% add the stat object to the output pat object
 pat = setobj(pat, 'stat', stat);
 
