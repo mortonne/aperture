9f244fc00333be1bd413cf6a29477bdeb62d690f
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2009-11-19 05:10:10 +0000

    many small tweaks to create_pattern.

--- patterns/creation/create_pattern.m
+++ patterns/creation/create_pattern.m
@@ -38,7 +38,7 @@ function subj = create_pattern(subj, fcn_handle, params, pat_name, res_dir)
 %   chanFilter      - used to choose which channels to include in the
 %                     pattern. Can a string to pass into filterStruct,
 %                     or an array of channel numbers to include ('')
-%   resampledRate   - rate to resample to (500)
+%   resampledRate   - rate to resample to. ([])
 %   downsample      - rate to downsample to (applies to power patterns)
 %                     ([])
 %   offsetMS        - time in milliseconds before each event to start
@@ -47,33 +47,28 @@ function subj = create_pattern(subj, fcn_handle, params, pat_name, res_dir)
 %   freqs           - for patterns with a frequency dimension, specifies
 %                     which frequencies (in Hz) the pattern should 
 %                     include ([])
-%   lock            - if true, the pattern's file will be locked during
-%                     pattern creation, and unlock when the program
-%                     finishes. Useful for processing multiple subjects
-%                     in parallel on a cluster. (false)
-%   overwrite       - if true, existing pattern files will be overwritten
+%   overwrite       - if true, existing pattern files will be
+%                     overwritten. (false)
 %   updateOnly      - if true, the pattern will not be created, but a
 %                     pattern object will be created and attached to the
-%                     subject object.
+%                     subject object. (false)
 %
 %  See also create_voltage_pattern, create_power_pattern.
 
 % input checks
-if ~exist('subj','var') || ~isstruct(subj)
+if ~exist('subj', 'var') || ~isstruct(subj)
   error('You must pass a subject object.')
-elseif length(subj)>1
+elseif length(subj) > 1
   error('You must pass only one subject.')
-elseif ~all(isfield(subj, {'id','chan','ev'}))
+elseif ~all(isfield(subj, {'id', 'chan', 'ev'}))
   error('The subject object must have "id", "chan", and "ev" fields.')
-elseif ~exist('fcn_handle','var') || ~isa(fcn_handle, 'function_handle')
+elseif ~exist('fcn_handle', 'var') || ~isa(fcn_handle, 'function_handle')
   error('You must pass a function handle.')
 end
-if ~exist('params','var')
+if ~exist('params', 'var')
   params = struct;
-elseif ~isstruct(params)
-  error('params must be a structure.')
 end
-if ~exist('pat_name','var')
+if ~exist('pat_name', 'var')
   pat_name = 'pattern';
 elseif ~ischar(pat_name)
   error('pat_name must be a string.')
@@ -90,7 +85,7 @@ params = structDefaults(params,  ...
                         'replace_eegfile',  {},         ... 
                         'eventFilter',      '',         ...
                         'chanFilter',       '',         ...
-                        'resampledRate',    500,        ...
+                        'resampledRate',    [],         ...
                         'downsample',       [],         ...
                         'offsetMS',         -200,       ...
                         'durationMS',       2200,       ...
@@ -98,7 +93,6 @@ params = structDefaults(params,  ...
                         'freqs',            [],         ...
                         'freqFilter',       '',         ...
                         'precision',        'double',   ...
-                        'lock',             false,      ...
                         'overwrite',        false,      ...
                         'updateOnly',       false);
 
@@ -106,12 +100,25 @@ if ~isfield(params,'baseEventFilter')
   params.baseEventFilter = params.eventFilter;
 end
 
-% set where the pattern will be saved
-pat_file = fullfile(res_dir, 'patterns', sprintf('pattern_%s_%s.mat', pat_name, subj.id));
-
-% print status and final parameters
+% print status
 if ~params.updateOnly
-  fprintf('creating pattern for %s named %s using %s.\n', subj.id, pat_name, func2str(fcn_handle))
+  fprintf('creating "%s" pattern from "%s" events using %s...\n', ...
+          pat_name, params.evname, func2str(fcn_handle))
+end
+
+% set where the pattern will be saved
+pat_dir = fullfile(res_dir, 'patterns');
+pat_file = fullfile(pat_dir, objfilename('pattern', pat_name, subj.id));
+
+if ~params.overwrite && exist(pat_file, 'file')
+  fprintf('pattern exists in %s.\nSkipping...\n', pat_file)
+  return
+end
+if ~exist(res_dir, 'dir')
+  mkdir(res_dir);
+end
+if ~exist(pat_dir, 'dir')
+  mkdir(pat_dir)
 end
 
 % events dimension
@@ -124,17 +131,31 @@ if ~isempty(params.replace_eegfile)
 end
 base_events = filterStruct(ev.mat, params.baseEventFilter);
 
-% channels dimension
-chan = subj.chan;
+% get channel info from the subject
+chan = get_dim(subj, 'chan');
 
 % time dimension
 if ~isempty(params.downsample)
-	stepSize = fix(1000/params.downsample);
+  step_size = fix(1000 / params.downsample);
 else
-	stepSize = fix(1000/params.resampledRate);
+  if isempty(params.resampledRate)
+    % if not resampling, we'll need to know the samplerate of the data
+    % so we can initialize the pattern.
+    eegfiles = unique({ev.mat.eegfile});
+    samplerates = cellfun(@(x)GetRateAndFormat(fileparts(x)), eegfiles);
+    if length(unique(samplerates)) > 1
+      params.resampledRate = min(samplerates);
+      fprintf(['Events contain multiple samplerates. ' ...
+               'Resampling to %d Hz...\n'], params.resampledRate)
+    else
+      params.resampledRate = unique(samplerates);
+    end
+  end
+  step_size = fix(1000 / params.resampledRate);
 end
 % millisecond values for the final pattern
-ms_values = params.offsetMS:stepSize:(params.offsetMS+params.durationMS-1);
+end_ms = params.offsetMS + params.durationMS - step_size;
+ms_values = params.offsetMS:step_size:end_ms;
 time = init_time(ms_values);
 
 % frequency dimension
@@ -149,45 +170,34 @@ try
 catch err
   id = get_error_id(err);
   if strcmp(id, 'EmptyPattern')
-    fprintf('Filtering will remove a dimension of the pattern. Aborting pattern creation...\n')
-    return
-  end
-end
-
-src_events = get_mat(pat.dim.ev);
-pat.params.channels = [pat.dim.chan.number];
-
-% get the information we'll need later to create bins, and update pat.dim.
-% to conserve memory, we'll do the actual binning as we accumulate the pattern.
-[pat, bins] = patBins(pat, params);
-
-% finalize events for the pattern
-if pat.dim.ev.modified
-  % save the modified events struct to a new file
-  events_dir = get_pat_dir(pat, 'events');  
-  pat.dim.ev.file = fullfile(events_dir, sprintf('events_%s_%s.mat', pat_name, subj.id));
-end
-pat.dim.ev = move_obj_to_hd(pat.dim.ev, true);
-
-% update subj with the new pat object
-subj = setobj(subj, 'pat', pat);
-
-% check the output file
-try
-  prepFiles({}, pat.file, params);
-catch err
-  if strfind(err.identifier, 'fileExists')
-    fprintf('file exists. Skipping...\n')
-    return
+    error('Filtering will remove a dimension of the pattern.')
   else
     rethrow(err)
   end
 end
 
+% get updated events and channel info before doing binning
+src_events = get_mat(pat.dim.ev);
+pat.params.channels = get_dim_vals(pat.dim, 'chan');
+
+% get the information we'll need later to create bins, and update
+% pat.dim. to conserve memory, we'll do the actual binning as we
+% accumulate the pattern.
+[pat, bins] = patBins(pat, params);
+
+% finalize events for the pattern
+if pat.dim.ev.modified
+  % save the modified events struct to a new file
+  pat.dim.ev.file = fullfile(get_pat_dir(pat, 'events'), ...
+                             objfilename('events', pat_name, subj.id));
+end
+pat.dim.ev = move_obj_to_hd(pat.dim.ev, true);
+
 % if we just want to update the subject object, we're done
 if params.updateOnly
-  closeFile(pat.file);
   fprintf('Pattern %s added to subj %s.\n', pat_name, subj.id)
+  pat.params = rmfield(pat.params, 'channels');
+  subj = setobj(subj, 'pat', pat);
   return
 end
 
@@ -205,7 +215,8 @@ for session=unique([src_events.session])
   sess_base_events = base_events([base_events.session]==session);
 
   % make the pattern for this session
-  pattern(sess_ind,:,:,:) = fcn_handle(pat, sess_events, sess_base_events, bins);
+  pattern(sess_ind,:,:,:) = fcn_handle(pat, sess_events, sess_base_events, ...
+                                       bins);
 end
 fprintf('\n')
 
@@ -214,8 +225,10 @@ fprintf('\n')
 pattern = patMeans(pattern, bins(1));
 
 % save the pattern
-save(pat.file, 'pattern');
+pat = set_mat(pat, pattern, 'hd');
 fprintf('Pattern saved in %s.\n', pat.file)
 
-% unlock the pattern file if needed
-closeFile(pat.file);
+% update subj with the new pat object
+pat.params = rmfield(pat.params, 'channels');
+subj = setobj(subj, 'pat', pat);
+
