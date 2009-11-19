fa13f2ee2609c5c8962202feb0a3d0bbd8312a40
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2009-11-19 05:05:20 +0000

    updated implementation of init_dim functions and added docstrings.

--- patterns/dimensions/init_freq.m
+++ patterns/dimensions/init_freq.m
@@ -1,20 +1,37 @@
 function freq = init_freq(freqs, labels)
+%INIT_FREQ   Initialize a frequency dimension.
+%
+%  freq = init_freq(freqs, labels)
+%
+%  INPUTS:
+%    freqs:  vector of frequency values in Hz.
+%
+%   labels:  (optional) cell array of strings giving labels for each
+%            frequency bin.
+%
+%  OUTPUTS:
+%     freq:  struct with information about a frequency dimension.
 
+% input checks
 if ~exist('freqs', 'var') || isempty(freqs)
   freq = struct('vals', [],  'avg', [],  'label', '');
   return
+elseif ~isnumeric(freqs)
+  error('freqs must be numeric.')
 end
-
 if ~exist('labels', 'var')
   labels = {};
+elseif ~iscellstr(labels)
+  error('labels must be a cell array of strings.')
+elseif length(labels) ~= length(freqs)
+  error('labels must be the same length as freqs.')
 end
 
-for f=1:length(freqs)
-  freq(f).vals = freqs(f);
-  freq(f).avg = freqs(f);
-  if ~isempty(labels)
-    freq(f).label = labels{f};
-  else
-    freq(f).label = sprintf('%d Hz', freqs(f));
-  end
+% make the time struct, with one element for each time bin
+freqs_cell = num2cell(freqs);
+if isempty(labels)
+  % if no user-specified labels, print default labels
+  labels = cellfun(@(x)sprintf('%d Hz', x), freqs_cell, 'UniformOutput', ...
+                   false);
 end
+freq = struct('vals', freqs_cell, 'avg', freqs_cell, 'label', labels);
--- patterns/dimensions/init_time.m
+++ patterns/dimensions/init_time.m
@@ -1,20 +1,38 @@
-function time = init_time(MSvals, labels)
+function time = init_time(ms_vals, labels)
+%INIT_TIME   Initialize a time dimension.
+%
+%  time = init_time(ms_vals, labels)
+%
+%  INPUTS:
+%  ms_vals:  vector of time values in milliseconds.
+%
+%   labels:  (optional) cell array of strings giving labels for each
+%            time bin.
+%
+%  OUTPUTS:
+%     time:  struct with information about a time dimension.
 
-if ~exist('MSvals', 'var')
+% input checks
+if ~exist('ms_vals', 'var') || isempty(ms_vals)
   time = struct('MSvals', [],  'avg', [],  'label', '');
   return
+elseif ~isnumeric(ms_vals)
+  error('ms_vals must be numeric.')
 end
-
 if ~exist('labels', 'var')
   labels = {};
+elseif ~iscellstr(labels)
+  error('labels must be a cell array of strings.')
+elseif length(labels) ~= length(ms_vals)
+  error('labels must be the same length as ms_vals.')
 end
 
-for t=1:length(MSvals)
-  time(t).MSvals = MSvals(t);
-  time(t).avg = MSvals(t);
-  if ~isempty(labels)
-    time(t).label = labels{t};
-  else
-    time(t).label = sprintf('%d ms', MSvals(t));
-  end
+% make the time struct, with one element for each time bin
+ms_vals_cell = num2cell(ms_vals);
+if isempty(labels)
+  % if no user-specified labels, print default labels
+  labels = cellfun(@(x)sprintf('%d ms', x), ms_vals_cell, 'UniformOutput', ...
+                   false);
 end
+time = struct('MSvals', ms_vals_cell, 'avg', ms_vals_cell, 'label', labels);
+
