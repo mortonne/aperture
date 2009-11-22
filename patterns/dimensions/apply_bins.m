7cbbd2291b2befa59d12b01bf920413b92ff6662
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2009-11-22 03:01:23 +0000

    added apply_bins and updated style of make_bins.

---
+++ patterns/dimensions/apply_bins.m
@@ -0,0 +1,55 @@
+function bin_inds = apply_bins(x, bin_defs)
+%APPLY_BINS   Apply bins to a vector.
+%
+%  bin_inds = apply_bins(x, bin_defs)
+%
+%  INPUTS:
+%         x:  vector to apply binning to.
+%
+%  bin_defs:  [bins X 2] matrix that defines the bins. Each row defines
+%             one bin. bin_defs(i,1) and bin_defs(i,2) define the upper
+%             and lower limits, respectively, of bin i. Limits are
+%             inclusive, with one exception: if the upper limit of a bin
+%             is equal to the lower limit of the next bin, the upper
+%             limit will not be inclusive.
+%
+%  OUTPUTS:
+%  bin_inds:  cell array of length [1 X bins] containing the indices of
+%             x that correspond to each bin.
+%
+%  EXAMPLE:
+%   >> x = [7 1 15 13 2 14];
+%   >> bin_defs = [1 7; 7 13; 13 15];
+%   >> bin_inds = apply_bins(x, bin_defs);
+%   x(bin_inds{1}) % [1 2]
+%   x(bin_inds{2}) % 7
+%   x(bin_inds{3}) % [15 13 14]
+
+% input checks
+if ~exist('x', 'var') || ~isnumeric(x)
+  error('You must pass a vector to bin.')
+elseif ~isvector(x)
+  error('x must be a vector.')
+elseif ~exist('bin_defs', 'var') || ~isnumeric(bin_defs)
+  error('You must pass bin definitions.')
+elseif size(bin_defs, 2)~=2
+  error('bin_defs must be a [bins X 2] matrix.')
+end
+
+n_bins = size(bin_defs, 1);
+bin_inds = cell(1, n_bins);
+for i=1:n_bins
+  % extract the limits for this bin
+  lower = bin_defs(i,1);
+  upper = bin_defs(i,2);
+  
+  % get the indices of the values that correspond to this bin
+  if i~=n_bins && upper==bin_defs(i+1, 1)
+    % upper limit is equal to next bin's lower limit; don't include it
+    x_bin = lower <= x & x < upper;
+  else
+    % include the whole range
+    x_bin = lower <= x & x <= upper;
+  end
+  bin_inds{i} = find(x_bin);
+end
--- patterns/dimensions/make_bins.m
+++ patterns/dimensions/make_bins.m
@@ -1,7 +1,7 @@
-function bins = make_bins(step_size,start,final)
+function bins = make_bins(step_size, start, final)
 %MAKE_BINS   Define the boundaries of a series of bins.
 %
-%  bins = make_bins(step_size,start,final)
+%  bins = make_bins(step_size, start, final)
 %
 %  INPUTS:
 %  step_size:  size of each bin.
@@ -11,8 +11,8 @@ function bins = make_bins(step_size,start,final)
 %      final:  end of the last bin.
 %
 %  OUTPUTS:
-%       bins:  [number of bins]X2 array, where bins(X,1) gives
-%              the start of bin X and bins(X,2) gives the end.
+%       bins:  [number of bins]X2 array, where bins(X,1) gives the start
+%              of bin X and bins(X,2) gives the end.
 
 % initialize
 nbins = ceil((final-start)/step_size);
@@ -24,7 +24,7 @@ for i=1:nbins
   bins(i,1) = s;
   
   % end of this bin
-  e = s+step_size;
+  e = s + step_size;
   bins(i,2) = e;
   
   s = e;
