fa2296598573cdd9723a44a7a2ecf57bef462951
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2009-11-23 03:48:20 +0000

    create_acts_pattern: added options of using acts or correct/incorrect.

--- patclass/create_acts_pattern.m
+++ patclass/create_acts_pattern.m
@@ -1,7 +1,8 @@
-function new_pat = create_acts_pattern(pat, stat_name, new_pat_name, res_dir)
+function new_pat = create_acts_pattern(pat, stat_name, new_pat_name, params, ...
+                                       res_dir)
 %CREATE_ACTS_PATTERN   Create a pattern from classifier outputs.
 %
-%  new_pat = create_acts_pattern(pat, stat_name, new_pat_name, res_dir)
+%  new_pat = create_acts_pattern(pat, stat_name, new_pat_name, params, res_dir)
 %
 %  From the results of pattern classification, get the classifier
 %  output for the target on each trial, and put it in a new pattern.
@@ -16,18 +17,35 @@ function new_pat = create_acts_pattern(pat, stat_name, new_pat_name, res_dir)
 %
 %  new_pat_name:  string name of the new pattern that will be created.
 %
+%        params:  structure specifying options for creating the new
+%                 pattern.  See below.
+%
 %       res_dir:  path to the directory where the pattern will be saved.
 %                 If not specified, a directory named new_pat_name
 %                 on the same level as pat's will be used.
 %
 %  OUTPUTS:
 %       new_pat:  the new pattern object.
+%
+%  PARAMS:
+%   stat_type - ['acts' {'correct'}]
+%   dim       - 2
+%   precision - 'single'
 
 % input checks
+if ~exist('params', 'var')
+  params = struct;
+end
 if ~exist('res_dir', 'var')
   pat_parent_dir = fileparts(get_pat_dir(pat));
   res_dir = fullfile(pat_parent_dir, new_pat_name);
 end
+
+defaults.stat_type = 'correct';
+defaults.dim = 2;
+defaults.precision = 'single';
+
+params = propval(params, defaults);
 
 % get the results of pattern classification
 stat = getobj(pat, 'stat', stat_name);
@@ -35,21 +53,18 @@ res = getfield(load(stat.file, 'res'), 'res');
 
 % size of the new pattern is events X [dimensions of res]
 res_size = size(res.iterations);
-new_pat_size = [patsize(pat.dim, 1) res_size(2:end)];
-pattern = NaN(new_pat_size, 'single');
+new_pat_size = [patsize(pat.dim, 1) 1 1 1];
+new_pat_size(2:length(res_size) - 1) = res_size(2:end);
+pattern = NaN(new_pat_size, params.precision);
 
 % create a pattern with classifier outputs
+fprintf('creating "%s" pattern from "%s" classification results...\n', ...
+        new_pat_name, stat_name)
+
 for c=1:new_pat_size(2)
   for t=1:new_pat_size(3)
     for f=1:new_pat_size(4)
-      
-      for i=1:res_size(1)
-        iter_res = res.iterations(i,c,t,f);
-        corr = iter_res.acts(logical(iter_res.targs));
-        pattern(iter_res.test_idx,c,t,f) = corr;
-        %pattern(iter_res.test_idx,c,t,f) = iter_res.perf;
-      end
-      
+      pattern(:,c,t,f) = get_acts(res.iterations(:,c,t,f), params);
     end
   end
 end
@@ -64,7 +79,8 @@ new_pat_file = fullfile(pat_dir, ...
                         objfilename('pattern', new_pat_name, pat.source));
 
 % create a new pat object (only name and file are different)
-new_pat = init_pat(new_pat_name, new_pat_file, pat.source, stat.params, pat.dim);
+new_pat = init_pat(new_pat_name, new_pat_file, pat.source, stat.params, ...
+                   pat.dim);
 
 % get the dimensions that we iterated over when doing classification
 iter_dims = stat.params.iter_dims;
@@ -92,3 +108,22 @@ end
 % add the classifier outputs as the pattern matrix
 new_pat = set_mat(new_pat, pattern);
 
+function acts = get_acts(res, params)
+  n_events = length(res(1).train_idx);
+  acts = NaN(n_events, 1);
+  
+  for i=1:length(res)
+    iter_res = res(i);
+    
+    if strcmp(params.stat_type, 'acts')
+      % get classifier activation for the correct unit
+      mat = iter_res.acts(logical(iter_res.targs));
+    elseif strcmp(params.stat_type, 'correct')
+      % for each event, get whether the classifier guessed correctly
+      perfmet = perfmet_maxclass(iter_res.acts, iter_res.targs);
+      mat = perfmet.corrects;
+    end
+
+    acts(iter_res.test_idx) = mat;
+  end
+%endfunction
