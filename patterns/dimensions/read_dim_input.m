1923ad090860d1ddc71809d5036f3f5e160e1c16
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2010-01-23 11:19:14 +0000

    read_dim_input: updated formatting, added dim number sanity check

--- patterns/dimensions/read_dim_input.m
+++ patterns/dimensions/read_dim_input.m
@@ -1,23 +1,23 @@
-function [dim_name,dim_number,dim_long_name] = read_dim_input(dim_input)
+function [name, number, long_name] = read_dim_input(dim_input)
 %READ_DIM_INPUT   Parse user input indicating a dimension.
 %
-%  [dim_name,dim_number,dim_long_name] = read_dim_input(dim_input)
+%  [name, number, long_name] = read_dim_input(dim_input)
 %
 %  INPUTS:
-%   dim_input:  either a string specifying the name of the dimension 
-%               (can be: 'ev', 'chan', 'time', 'freq'), or an integer
-%               corresponding to the dimension in the actual matrix.
+%  dim_input:  either a string specifying the name of the dimension
+%              (can be: 'ev', 'chan', 'time', 'freq'), or an integer
+%              corresponding to the dimension in the actual matrix.
 %
 %  OUTPUTS:
-%    dim_name:  name of the dimension.
+%       name:  name of the dimension.
 %
-%  dim_number:  number of the dimension.
+%     number:  number of the dimension.
 %
-%  dim_long_name:  long name of the dimension:
-%                  'Events', 'Channel', 'Time', 'Frequency'
+%  long_name:  long name of the dimension:
+%              'Events', 'Channel', 'Time', 'Frequency'
 
 % input checks
-if ~exist('dim_input','var')
+if ~exist('dim_input', 'var')
   error('You must pass dim_input.')
 end
 
@@ -26,23 +26,26 @@ DIM_NAMES = {'ev', 'chan', 'time', 'freq'};
 DIM_LONG_NAMES = {'Event', 'Channel', 'Time', 'Frequency'};
 if isnumeric(dim_input)
   % input was dim_input number
-  dim_number = dim_input;
-
+  number = dim_input;
+  if ~ismember(number, 1:length(DIM_NAMES))
+    error('Invalid dim_input number: %d', dim_input)
+  end
+  
   % get dim_input name
-  dim_name = DIM_NAMES{dim_input};
+  name = DIM_NAMES{dim_input};
 
 elseif ischar(dim_input)
-  dim_name = dim_input;
+  name = dim_input;
   % make sure the name is valid
-  if ~ismember(dim_name, DIM_NAMES)
-    error('Invalid dim_input name: %s', dim_input)
+  if ~ismember(name, DIM_NAMES)
+    error('Invalid dim_input name: "%s"', dim_input)
   end
 
   % get dim_input number
-  dim_number = find(strcmp(dim_name, DIM_NAMES));
+  number = find(strcmp(name, DIM_NAMES));
 
 else
   error('dim_input must be an integer or a string.')
 end
 
-dim_long_name = DIM_LONG_NAMES{dim_number};
+long_name = DIM_LONG_NAMES{number};
