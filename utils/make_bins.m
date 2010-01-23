00cfaf150a090c614fcb9ff89c2e625776fbc5f5
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2010-01-23 12:09:55 +0000

    make_bins: updated docstring, added sanity checks

--- utils/make_bins.m
+++ utils/make_bins.m
@@ -1,31 +1,54 @@
-function bins = make_bins(step_size, start, final)
-%MAKE_BINS   Define the boundaries of a series of bins.
+function bin_defs = make_bins(step_size, start, final)
+%MAKE_BINS   Define the boundaries of non-overlapping bins.
 %
-%  bins = make_bins(step_size, start, final)
+%  bin_defs = make_bins(step_size, start, final)
 %
 %  INPUTS:
 %  step_size:  size of each bin.
 %
 %      start:  start of the first bin.
 %
-%      final:  end of the last bin.
+%      final:  last value to be included.
 %
 %  OUTPUTS:
-%       bins:  [number of bins]X2 array, where bins(X,1) gives the start
-%              of bin X and bins(X,2) gives the end.
+%   bin_defs:  [number_of_bins X 2] array, where bin_defs(i,1) gives the
+%              start of bin i and bin_defs(i,2) gives the end.
+%
+%  EXAMPLE:
+%  >> bin_defs = make_bins(3, 0, 7)
+%  bin_defs =
+%     0     3
+%     3     6
+%     6     9
+%
+%  See also apply_bins.
+
+% input checks
+if ~exist('step_size', 'var')
+  error('You must specify a step size.')
+elseif step_size <= 0
+  error('Step size must be positive.')
+elseif ~exist('start', 'var') || ~isnumeric(start)
+  error('You must specify a start value.')
+elseif ~exist('final', 'var') || ~isnumeric(final)
+  error('You must specify a final value.')
+end
+if start >= final
+  error('Final value must be greater than start value.')
+end
 
 % initialize
-nbins = ceil((final-start)/step_size);
-bins = NaN(nbins,2);
+nbins = ceil((final - start) / step_size);
+bin_defs = NaN(nbins, 2);
 
 s = start;
 for i=1:nbins
   % start of this bin
-  bins(i,1) = s;
+  bin_defs(i,1) = s;
   
   % end of this bin
   e = s + step_size;
-  bins(i,2) = e;
+  bin_defs(i,2) = e;
   
   s = e;
 end
