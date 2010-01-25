394efe4b20edf90efade04ece67d0541e4872077
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2010-01-25 21:25:09 +0000

    create_targets: cleaned up docstring, added sanity checks

--- patclass/create_targets.m
+++ patclass/create_targets.m
@@ -1,30 +1,54 @@
-function targets = create_targets(events, reg_name)
-% CREATE_TARGETS
-%   Makes a logical matrix out of an events structure and an events
-%   field. 
+function targets = create_targets(events, bin_defs)
+%CREATE_TARGETS   Create a conditions matrix from an events structure.
 %
+%  targets = create_targets(events, bin_defs)
 %
-%   OUTPUT:
-%      targets - [nEv x nConds] where nEv is the number of events
-%      in the events structure, and nConds is the number of unique
-%      elements that appear in the specified reg_name field.  When
-%      a given event is of a particular condition, the
-%      corresponding row of targets contains a 1.
+%  INPUTS:
+%    events:  an events structure.
 %
-%   NOTES:  To make a version where one can specify a continuous
+%  bin_defs:  input to make_event_bins. Each unique label will
+%             correspond to a column of targets.
+%
+%  OUTPUTS:
+%   targets:  [events X conditions] logical array. Each event is assumed
+%             to only have one active condition. The active condition
+%             for event i is indicated by the true element of
+%             targets(i,:).
+%
+%  NOTES:  To make a version where one can specify a continuous
 %   valued field from events.  Requires that each event has a
 %   vector [1x nConds] with the values for each cond for that event.
+%
+%  EXAMPLE:
+%   % create an events structure with a field to use for creating a
+%   % targets matrix
+%   events = struct('x', num2cell([repmat(1,1,4) repmat(2,1,7)]));
+%
+%   % create a conditions matrix using the field
+%   targets = create_targets(events, 'x');
+%
+%  See also make_event_bins.
 
-
-% create the regressors
-targ_vec = make_event_bins(events, reg_name);
-conds = unique(targ_vec(~isnan(targ_vec)));
-targets = zeros(length(events), length(conds));
-for i=1:length(conds)
-  cond_match = targ_vec == conds(i);
-  targets(:, i) = cond_match;
+% input checks
+if ~exist('events', 'var') || ~isstruct(events)
+  error('You must pass an events structure.')
+elseif ~exist('bin_defs', 'var')
+  error('You must pass bin definitions.')
 end
 
+% create the regressors
+targ_vec = make_event_bins(events, bin_defs);
+if iscellstr(targ_vec)
+  targ_vec = make_index(targ_vec);
+end
 
+% unique condition labels
+conds = unique(targ_vec(~isnan(targ_vec)));
 
+% create logical conditions matrix
+targets = false(length(events), length(conds));
+for i=1:length(conds)
+  cond_match = targ_vec == conds(i);
+  targets(:,i) = cond_match;
+end
 
