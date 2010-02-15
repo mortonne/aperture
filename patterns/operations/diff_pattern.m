0c286e9c8a4944052479d0fb1ae3531cdf07abd7
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2010-02-15 20:34:02 +0000

    diff_pattern: now supports multiple channel pairs, channel labels

--- patterns/operations/diff_pattern.m
+++ patterns/operations/diff_pattern.m
@@ -3,9 +3,8 @@ function pat = diff_pattern(pat, varargin)
 %
 %  pat = diff_pattern(pat, ...)
 %
-%  Currently only supports taking difference between two channels.
-%  Later, can expand to take pairs of indices (or vals?) along any
-%  dimension.
+%  Currently, only takes differences between channels. Later, can expand
+%  to take pairs of indices (or vals?) along any dimension.
 %
 %  INPUTS:
 %      pat:  input pattern object.
@@ -16,20 +15,21 @@ function pat = diff_pattern(pat, varargin)
 %  PARAMS:
 %  These options may be specified using parameter, value pairs or by
 %  passing a structure. Defaults are shown in parentheses.
-%   chans     - [1 X 2] array of channel numbers. Difference will be
-%               chans(1) - chans(2).
-%   chanlabels - currently unsupported
-%   save_mats - if true, and input mats are saved on disk, modified mats
-%               will be saved to disk. If false, the modified mats will
-%               be stored in the workspace, and can subsequently be
-%               moved to disk using move_obj_to_hd. (true)
-%   overwrite - if true, existing patterns on disk will be overwritten.
-%               (false)
-%   save_as   - string identifier to name the modified pattern. If
-%               empty, the name will not change. ('')
-%   res_dir   - directory in which to save the modified pattern and
-%               events, if applicable. Default is a directory named
-%               pat_name on the same level as the input pat.
+%   chans      - cell array of [1 X 2] arrays of channel numbers.
+%                Difference will be chans(1) - chans(2).
+%   chanlabels - cell array of strings with labels for each pair of
+%                channels. ({})
+%   save_mats  - if true, and input mats are saved on disk, modified
+%                mats will be saved to disk. If false, the modified mats
+%                will be stored in the workspace, and can subsequently
+%                be moved to disk using move_obj_to_hd. (true)
+%   overwrite  - if true, existing patterns on disk will be overwritten.
+%                (false)
+%   save_as    - string identifier to name the modified pattern. If
+%                empty, the name will not change. ('')
+%   res_dir    - directory in which to save the modified pattern and
+%                events, if applicable. Default is a directory named
+%                pat_name on the same level as the input pat.
 
 % input checks
 if ~exist('pat', 'var') || ~isstruct(pat)
@@ -46,20 +46,33 @@ pat = mod_pattern(pat, @get_chandiffs, {params}, saveopts);
 
 function pat = get_chandiffs(pat, params)
   pat = move_obj_to_workspace(pat);
-
-  % find the channels
-  channels = get_dim_vals(pat.dim, 'chan');
-  chan_ind = [find(channels==params.chans(1)) find(channels==params.chans(2))];
-  if length(chan_ind)~=2
-    error('channels not found.')
-  end
-  
-  % take the difference
   pattern = get_mat(pat);
-  pattern = pattern(:,chan_ind(1),:,:) - pattern(:,chan_ind(2),:,:);
-  pat = set_mat(pat, pattern);
-
-  % fix the dimension info
-  pat.dim.chan = struct('number', params.chans, 'region', '', 'label', ...
-                        '');
+  
+  s = patsize(pat.dim);
+  new_pattern = nan(s(1), length(params.chans), s(3), s(4));
+  channels = get_dim_vals(pat.dim, 'chan');
+  
+  for i=1:length(params.chans)
+    diff_chans = params.chans{i};
+    
+    % find the channels
+    chan_ind = [find(channels==diff_chans(1)) find(channels==diff_chans(2))];
+    if length(chan_ind)~=2
+      error('channels not found.')
+    end
+    
+    % take the difference
+    new_pattern(:,i,:,:) = pattern(:,chan_ind(1),:,:) - ...
+                           pattern(:,chan_ind(2),:,:);
+    
+    % fix dimension info
+    if isempty(params.chanlabels)
+      label = sprintf('%d - %d', diff_chans(1), diff_chans(2));
+    else
+      label = params.chanlabels{i};
+    end
+    chan_info(i) = struct('number', diff_chans, 'region', '', 'label', label);
+  end
+  pat.dim.chan = chan_info;
+  pat = set_mat(pat, new_pattern);
 %endfunction
