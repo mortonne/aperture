c606ee10a6339eb2cedd9387b51f6c2cb2dd5a8e
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2010-02-17 22:33:05 +0000

    xval now calls traintest

--- patclass/xval.m
+++ patclass/xval.m
@@ -28,39 +28,32 @@ function res = xval(pattern, selector, targets, varargin)
 %   f_perfmet    - function handle for calculating performance
 %   perfmet_args - cell array of addition arguments for the perfmet
 %                  function
+%   verbose      - if true, more output will be printed. (false)
 
 % input checks
 if ~exist('pattern', 'var') || ~isnumeric(pattern)
   error('You must pass a pattern matrix.')
-elseif ~exist('selector', 'var') || ~isnumeric(selector)
-  error('You must pass a selector vector.')
-elseif ~isvector(selector)
-  error('Selector must be a vector.')
+elseif ~exist('selector', 'var')
+  error('You must pass a selector.')
+elseif ~isvector(selector) || ~isnumeric(selector)
+  error('Selector must be a numeric vector.')
 elseif length(selector) ~= size(pattern, 1)
   error('Different number of observations in pattern and selector.')
-elseif ~exist('targets', 'var') || ~isnumeric(targets)
-  error('You must pass a targets vector.')
-elseif length(targets) ~= size(pattern, 1)
-  error('Different number of observations in pattern and targets vector.')
+elseif ~exist('targets', 'var')
+  error('You must pass a targets matrix.')
+elseif size(targets, 1) ~= size(pattern, 1)
+  error('Different number of observations in pattern and targets matrix.')
 end
 
 % default params
-defaults.f_train = @train_logreg;
-defaults.train_args = struct('penalty', 10);
-defaults.f_test = @test_logreg;
-defaults.f_perfmet = {@perfmet_maxclass};
-[params, unused] = propval(varargin, defaults);
+defaults.verbose = false;
+[xval_params, params] = propval(varargin, defaults);
 
-if ~iscell(params.f_perfmet)
-  params.f_perfmet = {params.f_perfmet};
-end
-if ~isfield(params, 'perfmet_args')
-  params.perfmet_args = cell(1, length(params.f_perfmet));
-  params.perfmet_args{:} = deal(struct);
-end
-if isstruct(params.train_args)
-  params.train_args = {params.train_args};
-end
+% use propval since struct does weird things will cell array inputs
+params = propval(params, struct, 'strict', false);
+
+% override printing in traintest
+params.verbose = false;
 
 % get the selector value for each iteration
 sel_vals = unique(selector);
@@ -68,9 +61,6 @@ sel_vals = sel_vals(~isnan(sel_vals));
 if length(sel_vals) < 2
   error('Selector must have at least two unique non-NaN values.')
 end
-
-f_train = params.f_train;
-f_test = params.f_test;
 
 % flatten all dimensions > 2 into one vector
 patsize = size(pattern);
@@ -82,60 +72,41 @@ pattern = remove_nans(pattern);
 pattern = rescale(pattern);
 
 n_iter = length(sel_vals);
-n_perfs = length(params.f_perfmet);
-store_perfs = NaN(n_iter, n_perfs);
+%n_perfs = length(params.f_perfmet);
+%store_perfs = NaN(n_iter, n_perfs);
 for i=1:n_iter
-  
   % find the observations to train and test on
   unused_idx = isnan(selector);
   train_idx = ~unused_idx & (selector ~= sel_vals(i));
   test_idx = selector == sel_vals(i);
-  
-  % train
-  scratchpad = f_train(pattern(train_idx,:)', targets(train_idx,:)', ...
-                       params.train_args{:});
 
-  % test
-  test_targets = targets(test_idx,:)';
-  [acts, scratchpad] = f_test(pattern(test_idx,:)', test_targets, scratchpad);
-  scratchpad.cur_iteration = i;
+  % run classification and assess performance
+  iter_res = traintest(pattern(test_idx,:), pattern(train_idx,:), ...
+                       targets(test_idx,:), targets(train_idx,:), params);
   
-  % calculate performance
-  for p=1:n_perfs
-    pm_fh = params.f_perfmet{p};
-    args = params.perfmet_args{p};
-    if iscell(args)
-      pm = pm_fh(acts, test_targets, scratchpad, args{:});
-    else
-      pm = pm_fh(acts, test_targets, scratchpad, args);
-    end
-    pm.function_name = func2str(pm_fh);
-    
-    iter_res.perfmet{p} = pm;
-    [iter_res.perf(p), store_perfs(i,p)] = deal(pm.perf);
-  end
-
-  % save data from this iteration in MVPA style
+  % save the indices for this cross-validation in res
   iter_res.train_idx = train_idx;
   iter_res.test_idx = test_idx;
   iter_res.unused_idx = unused_idx;
   iter_res.unknown_idx = [];
-  iter_res.targs = test_targets;
-  iter_res.acts = acts;
-  iter_res.scratchpad = scratchpad;
-  iter_res.train_funct_name = func2str(f_train);
-  iter_res.test_funct_name = func2str(f_test);
-  iter_res.args = params;
+
   res.iterations(i) = iter_res;
 
-  fprintf('%.2f\t', res.iterations(i).perf)
-  if n_perfs > 1
-    fprintf('\n')
+  if xval_params.verbose
+    fprintf('%.2f\t', res.iterations(i).perf)
   end
+  %if n_perfs > 1
+  %  fprintf('\n')
+  %end
 end
-fprintf('TOTAL: %.2f', nanmean([res.iterations.perf]))
 
-if n_perfs==1
-  fprintf('\n')
+if xval_params.verbose
+  fprintf('TOTAL: %.2f', nanmean([res.iterations.perf]))
+else
+  fprintf('%.2f', nanmean([res.iterations.perf]))
 end
+
+%if n_perfs==1
+  fprintf('\n')
+%end
 
