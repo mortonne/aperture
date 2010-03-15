6c2f210c81ff274a3a921dacf8fbefb7af582e9f
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2010-03-15 01:29:52 +0000

    rec_baseline_events now does not use session index

--- events/operations/rec_baseline_events.m
+++ events/operations/rec_baseline_events.m
@@ -4,16 +4,16 @@ function events = rec_baseline_events(events, rec_duration, varargin)
 %  events = rec_baseline_events(events, rec_duration, ...)
 %
 %  INPUTS:
-%        events:  an events structure. Must have fields:
-%                  type      - all vocalization events have type
-%                              REC_WORD. Recall start is REC_START
-%                  session   - numeric session index
+%        events:  an events structure. Assumed to have fields:
+%                  type      - Start of recall periods have type
+%                              "REC_START"
 %                  mstime    - experiment time in milliseconds
 %                  rectime   - time since start of recall in ms
 %                  eegoffset - sample number in the EEG file
 %                  eegfile   - path to the EEG file
 %
-%  rec_duration:  duration of the recall period in ms.
+%  rec_duration:  duration of each recall period in ms. Must be the
+%                 same for all recall periods.
 %
 %  OUTPUTS:
 %        events:  events with added REC_BASE events.
@@ -29,83 +29,86 @@ defaults.post = 1000;
 defaults.duration = 200;
 params = propval(varargin, defaults);
 
-samplerate = GetRateAndFormat(events(1));
+% get the time of each event
+times = [events.mstime];
+
+% make sure the mstime field is valid across sessions
+if ~issorted(times)
+  error(['Event times are not in order. This could indicate overlap ' ...
+         'between session times.'])
+end
+
+% get recall period start times
+rec_start_events = events(strcmp({events.type}, 'REC_START'));
+
 base_events = [];
-sessions = unique([events.session]);
-for session = sessions
-  sess_events = events([events.session] == session);
+for i = 1:length(rec_start_events)
+  rec_start = rec_start_events(i).mstime;
+  rec_start_samp = rec_start_events(i).eegoffset;
   
-  n_rec_events = nnz(strcmp({sess_events.type}, 'REC_WORD'));
-  if n_rec_events == 0
-    % no recall events to get baseline for
-    fprintf('no recall events in session %d. skipping...\n', session)
+  % times of all events during this recall period (assuming they are
+  % disruptive events of some type, not suitable for baseline)
+  rec_ind = rec_start < times & times <= rec_start + rec_duration;
+  voc_times = times(rec_ind);
+  
+  % get EEG files for this recall period
+  eeg_files = unique({events(rec_ind).eegfile});
+  if length(eeg_files) > 1
+    warning('Recall period split over multiple EEG files. Skipping...')
     continue
   end
   
-  % get recall period start times
-  rec_start_events = sess_events(strcmp({sess_events.type}, 'REC_START'));
-  sess_times = [sess_events.mstime];
+  % assume same samplerate regardless
+  samplerate = GetRateAndFormat(rec_start_events(i));
   
-  for i = 1:length(rec_start_events)
-    rec_start = rec_start_events(i).mstime;
-    rec_start_samp = rec_start_events(i).eegoffset;
-    
-    % times of all events during this recall period (assuming they are
-    % disruptive events of some type, not suitable for baseline). If
-    % they are OK for baseline, why are you using this script? You're
-    % so stupid!
-    rec_ind = rec_start < sess_times & sess_times <= rec_start + rec_duration;
-    voc_times = sess_times(rec_ind);
-    
-    % find free time between events
-    p = rmfield(params, 'duration');
-    p.start = rec_start;
-    p.finish = rec_start + rec_duration;
-    free = free_epochs(voc_times, params.duration, p);
+  % find free time between events
+  p = rmfield(params, 'duration');
+  p.start = rec_start;
+  p.finish = rec_start + rec_duration;
+  free = free_epochs(voc_times, params.duration, p);
 
-    % for a proper baseline, we want the sample to be the same size as
-    % the number of recall events, so we can estimate the variance of the
-    % data we want to z-transform. If we have more than we need, take a
-    % random sample
-    n_trial_events = length(voc_times);
-    n_free = length(free);
-    if n_free < n_trial_events
-      fprintf('Warning: only found %d baseline epochs for %d recall events.\n', ...
-              n_free, n_trial_events)
-    end
-    
-    if n_free == 0
-      continue
-    elseif n_free <= n_trial_events
-      rand_free_times = free;
-    else
-      rand_free_times = sort(randsample(free, n_trial_events));
-    end
-    
-    % use apply_event_bins to get fields that are the same throughout
-    % the trial
-    free_events = apply_event_bins(sess_events(rec_ind), 1:nnz(rec_ind));
-    free_events = repmat(free_events, 1, length(rand_free_times));
-    
-    % set the type code
-    [free_events.type] = deal('REC_BASE');
-    
-    % experiment time
-    c = num2cell(rand_free_times);    
-    [free_events.mstime] = c{:};
-    
-    % recall period time
-    c = num2cell(rand_free_times - rec_start);
-    [free_events.rectime] = c{:};
-    
-    % EEG sample
-    rand_free_samp = rec_start_samp + ...
-                     ms2samp(rand_free_times, samplerate, rec_start);
-    c = num2cell(rand_free_samp);
-    [free_events.eegoffset] = c{:};
-    
-    base_events = cat_structs(base_events, free_events);
+  % for a proper baseline, we want the sample to be the same size as
+  % the number of recall events, so we can estimate the variance of the
+  % data we want to z-transform. If we have more than we need, take a
+  % random sample
+  n_trial_events = length(voc_times);
+  n_free = length(free);
+  if n_free < n_trial_events
+    fprintf('Warning: only found %d baseline epochs for %d recall events.\n', ...
+            n_free, n_trial_events)
   end
+  
+  if n_free == 0
+    continue
+  elseif n_free <= n_trial_events
+    rand_free_times = free;
+  else
+    rand_free_times = sort(randsample(free, n_trial_events));
+  end
+  
+  % use apply_event_bins to get fields that are the same throughout
+  % the trial
+  free_events = apply_event_bins(events(rec_ind), 1:nnz(rec_ind));
+  free_events = repmat(free_events, 1, length(rand_free_times));
+  
+  % set the type code
+  [free_events.type] = deal('REC_BASE');
+  
+  % experiment time
+  c = num2cell(rand_free_times);    
+  [free_events.mstime] = c{:};
+  
+  % recall period time
+  c = num2cell(rand_free_times - rec_start);
+  [free_events.rectime] = c{:};
+  
+  % EEG sample
+  rand_free_samp = rec_start_samp + ...
+      ms2samp(rand_free_times, samplerate, rec_start);
+  c = num2cell(rand_free_samp);
+  [free_events.eegoffset] = c{:};
+  
+  base_events = cat_structs(base_events, free_events);
 end
 
 % concatenate and sort
