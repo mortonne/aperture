7c089d90868198734b1fbd39f69ca142fca28bc1
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2010-05-31 23:08:13 +0000

    now calls prep_egi_data

--- basic/post_process_subj.m
+++ basic/post_process_subj.m
@@ -7,26 +7,27 @@ function subj = post_process_subj(subj,varargin)
 %      subj:  standard subj structure representing one subject.
 %
 %  varargin:  additional inputs (after subj.id and sess.dir)
-%             to prep_egi_data2.
+%             to prep_egi_data.
 
 % do any necessary post-processing, save events file for each subj
 for sess=subj.sess
   try
     % split, sync, rereference, detect blink artifacts
-    prep_egi_data2(subj.id,sess.dir,varargin{:});
+    prep_egi_data(subj.id, sess.dir, varargin{:});
   catch err
     switch get_error_id(err)
      case {'NoMatchStart', 'NoMatchEnd'}
-      fprintf('Warning: alignment failed for %s.\n', ...
-              sess.dir)
+      fprintf('Warning: alignment failed for %s.\n', sess.dir)
+      
      case 'PulseFileNotFound'
-      fprintf('Warning: pulse file not found for %s.\n', ...
-              sess.dir)
+      fprintf('Warning: pulse file not found for %s.\n', sess.dir)
+      
      case 'NoEEGFile'
-      fprintf('Warning: all events out of bounds for %s.\n', ...
-              sess.dir)
+      fprintf('Warning: all events out of bounds for %s.\n', sess.dir)
+      
      case 'CorruptedEEGFile'
       fprintf('Warning: EEG file for %s is corrupted.\n', sess.dir)
+      
      otherwise
       % just print the error output
       warning('eeg_ana:post_process_subj:SessError', ...
@@ -34,5 +35,5 @@ for sess=subj.sess
               sess.dir, getReport(err))
     end
   end
-  
 end
+
