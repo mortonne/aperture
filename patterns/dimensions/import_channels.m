02a69f8f48012b49bfff9e824ff4f1921d73a792
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2010-07-18 04:44:31 +0000

    import_channels: added ability to import labels from locs files

--- patterns/dimensions/import_channels.m
+++ patterns/dimensions/import_channels.m
@@ -1,12 +1,23 @@
-function subj = import_channels(subj, n_chans)
+function subj = import_channels(subj, locs_file)
 %IMPORT_CHANNELS   Import channel information for one subject.
 %
-%  subj = import_channels(subj, n_chans)
+%  subj = import_channels(subj, locs_file)
 %
-%  For now, this doesn't actually import anything; it just makes a basic
-%  chan structure with numbers and blank labels.  In the future, this
-%  will be able to read various types of channel locations files an add
-%  them in an eeg_ana-friendly manner.
+%  INPUTS:
+%       subj:  subject object.
+%
+%  locs_file:  path to an EEGLAB-compatible electrode locations file.
+%              See readlocs for supported formats. Alternatively,
+%              can be an integer indicating the number of channels.
+%
+%  OUTPUTS:
+%       subj:  subject object with an added "chan" structure holding
+%              channel information.
+%
+%  NOTES:
+%   Currently only imports the channel labels from the locs_file. Once
+%   functions are adapted to allow saving of chan structures to disk,
+%   will also import location information.
 
 % input checks
 if ~exist('subj', 'var') || ~isstruct(subj)
@@ -15,8 +26,27 @@ elseif ~isscalar(subj)
   error('Only one subject at a time.')
 end
 
-for i=1:n_chans
-  subj.chan(i).number = i;
-  subj.chan(i).label = sprintf('%d', i);
+% number of channels
+if isnumeric(locs_file)
+  n_chans = locs_file;
+  for i=1:n_chans
+    subj.chan(i).number = i;
+    subj.chan(i).label = sprintf('%d', i);
+  end
+  return
 end
+
+% input for readlocs
+elocs = readlocs(locs_file);
+numbers = num2cell(1:129);
+%[elocs.number] = number{:};
+%[elocs.label] = elocs.labels;
+
+% for now, just include the number (index) and label from the
+% locs file. Need to implement saving channel structures to disk
+% so we can have the location information without taking up too
+% much memory.
+labels = {elocs.labels};
+labels = cellfun(@(x) strrep(x, 'E', ''), labels, 'UniformOutput', false);
+subj.chan = struct('number', numbers, 'label', labels);
 
