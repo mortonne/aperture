41ac35224c819e52a5f4d867b84fa831eb9cf3bf
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2010-07-20 17:06:26 +0000

    extended gamma band def

--- patterns/dimensions/freq_bands1.m
+++ patterns/dimensions/freq_bands1.m
@@ -1,5 +1,5 @@
 function [freqbins,freqbinlabels] = freq_bands1
 %[freqbins,freqbinlabels] = freq_bands1
 
-freqbins = [2 4; 4 8.1; 10 14; 16 26; 28 42; 44 100];
+freqbins = [2 4; 4 8.1; 10 14; 16 26; 28 42; 44 120];
 freqbinlabels = {'Delta', 'Theta', 'Alpha', 'Beta', 'Low Gamma', 'High Gamma'};
