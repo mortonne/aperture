9cc6adb8c4e268397647a4eb6945bda7658385ea
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2010-07-20 17:11:29 +0000

    correct_mult_comp: clarified terminology

--- stats/correct_mult_comp.m
+++ stats/correct_mult_comp.m
@@ -1,44 +1,46 @@
-function [alpha_fw] = correct_mult_comp(p,alpha,method)
+function alpha_pc = correct_mult_comp(p, alpha_fw, method)
 %CORRECT_MULT_COMP   Correct p-values for multiple comparisons.
 %
-%  alpha_fw = correct_mult_comp(p,alpha,method)
+%  alpha_pc = correct_mult_comp(p, alpha_fw, method)
 %
 %  INPUTS:
 %         p:  array of p-values.
 %
-%     alpha:  scalar indicating the desired false alarm rate. Default is 0.05.
+%  alpha_fw:  desired familywise Type I error rate. Default is 0.05.
 %
-%    method:  string indicating which method to use for correcting multiple
-%             comparisons. Choices are 'bonferroni', 'holms', and 'fdr'.
+%    method:  string indicating which method to use for correcting
+%             for multiple comparisons. Choices are 'bonferroni',
+%             'holms', and 'fdr'.
 %
 %  OUTPUTS:
-%  alpha_fw:  adjusted alpha that gives a family-wise false alarm rate.
+%  alpha_pc:  per-comparison alpha to use to give the desired familywise
+%             Type I error rate.
 
 % input checks
-if ~exist('p','var')
+if ~exist('p', 'var')
   error('You must pass an array of p-values')
 end
-if ~exist('alpha','var')
-  alpha = 0.05;
+if ~exist('alpha_fw', 'var')
+  alpha_fw = 0.05;
 end
-if ~exist('method','var')
+if ~exist('method', 'var')
   method = '';
 end
 
 % find the threshold we need to set family-wise false alarm rate
 % to alpha
 switch lower(method)
-  case {'bonferoni', 'bonferroni'}
-  alpha_fw = alpha/numel(p);
+ case {'bonferoni', 'bonferroni'}
+  alpha_pc = alpha_fw / numel(p);
 
-  case 'fdr'
-  alpha_fw = fdr(p, alpha);
-  if isempty(alpha_fw)
-    alpha_fw = 0;
-  elseif alpha_fw<0
+ case 'fdr'
+  alpha_pc = fdr(p, alpha_fw);
+  if isempty(alpha_pc)
+    alpha_pc = 0;
+  elseif alpha_pc < 0
     keyboard
   end
   
-  otherwise
-  alpha_fw = alpha;
+ otherwise
+  alpha_pc = alpha_fw;
 end
