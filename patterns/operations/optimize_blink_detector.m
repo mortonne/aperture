2339b67471dfcb1df497cc8a6e2691f4c8c88534
zcohen <zcohen@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2010-09-06 16:11:00 +0000

    added optimize_blink_detector

---
+++ patterns/operations/optimize_blink_detector.m
@@ -0,0 +1,25 @@
+function [best_thresh blink_thresh] = optimize_blink_detector(pat)
+
+params = [];
+params.verbose = true;
+params.reject_full = true;
+params.veog_chans = [8 126];
+
+blink_thresh = [];
+
+for i = 1:600
+  params.blink_thresh = i;
+  [blink_thresh(i).d, blink_thresh(i).pHit, blink_thresh(i).pFA, stats] = ...
+      blink_detector_performance(pat,params);
+end
+
+best_d = find([blink_thresh.d]==max([blink_thresh.d]));
+
+%this sometimes returns more than one index so choose the first
+if length(best_d) > 1
+  best_d = best_d(1)
+end
+
+best_thresh = best_d;
+
+
