6902fdcc4832e21d06f0819a2a670c3812a0ec1e
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2010-09-14 22:33:43 +0000

    standardized baseline_pattern, added mod_pattern support

--- patterns/operations/baseline_pattern.m
+++ patterns/operations/baseline_pattern.m
@@ -1,26 +1,43 @@
-function pat = baseline_pattern(pat, baselineMS);
-%BASELINE_PATTERN   Re-baseline a pattern.
+function pat = baseline_pattern(pat, baselineMS, varargin);
+%BASELINE_PATTERN   Apply baseline correction to a pattern.
 %
-%  pat = baseline_pattern(pat, baselineMS)
+%  Subtracts the mean of the baseline period from each event in a
+%  pattern. Used to remove the effect of slow signal drifts.
+%
+%  pat = baseline_pattern(pat, baselineMS, ...)
 %
 %  INPUTS:
-%      pat:         input pattern object.
-%      baselineMS:  [2 X 1] array of MS values for baseline
-%                   period.
+%         pat:  input pattern object.
+%
+%  baselineMS:  [2 X 1] array of millisecond values indicating the range
+%               of times to include when calculating the baseline for
+%               each event.
 %
 %  OUTPUTS:
-%      pat:         pattern object that has been re-baselined.
+%         pat:  modified pattern object.
 
+% inputs
+if ~exist('pat', 'var')
+  error('You must pass a pattern object.')
+elseif ~exist('baselineMS', 'var')
+  error('You must define the baseline time.')
+end
 
-%note - this function should be rewritten to use mod_pattern
-%identify baseline period
+pat = mod_pattern(pat, @apply_baseline, {baselineMS}, varargin{:});
+
+function pat = apply_baseline(pat, baselineMS)
+
+% identify baseline period
 times = get_dim_vals(pat.dim, 'time');
 base = baselineMS(1) <= times & times < baselineMS(2);
 
-mat = get_mat(pat);
+pattern = get_mat(pat);
 
+% within each event, average over times within the baseline
 % events X channels X 1 X freq
-base_mean = nanmean(mat(:,:,base,:), 3);
-mat = mat - repmat(base_mean, [1 1 patsize(pat.dim, 3) 1]);
+base_mean = nanmean(pattern(:,:,base,:), 3);
 
-pat = set_mat(pat, mat, 'ws');
+% subtract the baseline
+pattern = pattern - repmat(base_mean, [1 1 patsize(pat.dim, 3) 1]);
+pat = set_mat(pat, pattern, 'ws');
+
