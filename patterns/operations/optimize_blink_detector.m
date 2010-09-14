e48b3f880a2a84cd8c9c9cb26426e1aba5a9e5f0
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2010-09-14 23:45:52 +0000

    optimize_blink_thresh: documentation, min_hits and thresh params

--- patterns/operations/optimize_blink_detector.m
+++ patterns/operations/optimize_blink_detector.m
@@ -1,23 +1,83 @@
-function [best_thresh blink_thresh] = optimize_blink_detector(pat, params)
+function [best_thresh, res] = optimize_blink_detector(pat, varargin)
+%OPTIMIZE_BLINK_DETECTOR   Use trackball data to optimize a blink detector.
+%
+%  Optimize blink detector parameters using trackball data. The
+%  threshold with the greatest d-prime (ability to distinguish between
+%  blinks and eye movements) will be output, along with stats for the
+%  other thresholds.
+%
+%  [best_thresh, res] = optimize_blink_detector(pat, ...)
+%
+%  INPUTS:
+%          pat:  a pattern object containing blink, up, down, left, and
+%                right eye movements.
+%
+%  OUTPUTS:
+%  best_thresh:  the optimal threshold for detecting blinks but not
+%                eye movements.
+%
+%  PARAMS:
+%  These options may be specified using parameter, value pairs or by
+%  passing a structure. Defaults are shown in parentheses.
+%   search_thresh - array of threshold values to search over.
+%                   (100:10:300)
+%   min_hits      - minimum hit rate that the winning detector must
+%                   have. (0.8)
+%  May also specify params for reject_blinks.
+%
+%  See also remove_eog_glm.
 
-params.verbose = true;
-params.reject_full = true;
+% options
+defaults.search_thresh = 100:10:300;
+defaults.min_hits = 0.8;
+[params, blink_params] = propval(varargin, defaults);
 
-blink_thresh = [];
+res = [];
 
-for i = 1:600
-  params.blink_thresh = i;
-  [blink_thresh(i).d, blink_thresh(i).pHit, blink_thresh(i).pFA, stats] = ...
-      blink_detector_performance(pat,params);
+% print the performance header
+header = {'Thresh', 'Hits', 'FA (up)', 'FA (down)', 'FA (left)', ...
+          'FA (right)', 'D-prime'};
+for i=1:length(header)
+  fprintf('%-11s', header{i})
+end
+fprintf('\n')
+
+blink_params.verbose = false;
+for i=1:length(params.search_thresh)
+  fprintf('%-11d', params.search_thresh(i))
+  blink_params.blink_thresh = params.search_thresh(i);
+
+  % run the detector and get performance
+  res(i).thresh = params.search_thresh(i);
+  [res(i).d, res(i).pHit, res(i).pFA, res(i).stats] = ...
+                                 blink_detector_performance(pat, blink_params);
+
+  % print performance
+  s = res(i).stats;
+  fprintf('%-11.2f%-11.2f%-11.2f%-11.2f%-11.2f%-11.2f\n', ...
+          s.pHit, s.pFA_up, s.pFA_down, s.pFA_left, ...
+          s.pFA_right, s.dprime)
 end
 
-best_d = find([blink_thresh.d]==max([blink_thresh.d]));
+% try the highest d-prime
+[y, best_d] = max([res.d]);
+if res(best_d).pHit < params.min_hits
+  % if hits are less than the minimum for the best d-prime,
+  % conditionalize on that and find lowest FA
+  min_fa = min([res([res.pHit] >= params.min_hits).pFA]);
+  best_fa = find([res.pFA] == min_fa);
+  if ~isempty(best_fa)
+    best_d = best_fa;
+  else
+    best_d = find(params.search_thresh == min(params.search_thresh));
+  end
+end
 
-%this sometimes returns more than one index so choose the first
+% this sometimes returns more than one index so choose the first
 if length(best_d) > 1
-  best_d = best_d(1)
+  best_d = best_d(1);
 end
 
-best_thresh = best_d;
+best_thresh = params.search_thresh(best_d);
 
 
