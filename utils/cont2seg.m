decca10596ab302905a48a905c640aa73f9179d2
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2010-09-16 20:32:24 +0000

    notes on cont, seg functions

--- utils/cont2seg.m
+++ utils/cont2seg.m
@@ -3,6 +3,9 @@ function new_pattern = cont2seg(pattern, pat_size)
 %reravel pattern back to its almost original size (eventsXtimeXchan)
 new_pattern = reshape(pattern, pat_size(1), pat_size(3), pat_size(2));
 
+%new_pattern = reshape(pattern, pat_size(3), pat_size(1), pat_size(2));
+
 %make pattern eventsXchanXtime
 new_pattern = permute(new_pattern, [1 3 2]);
+%new_pattern = permute(new_pattern, [2 3 1]);
 
--- utils/seg2cont.m
+++ utils/seg2cont.m
@@ -1,12 +1,18 @@
-function [new_pattern, pat_size] = seg2cont(pattern, pat_size)
+function [new_pattern, pat_size] = seg2cont(pattern)
+
+[n_events, n_chans, n_samps] = size(pattern);
 
 %make pattern eventsXtimeXchan
 pattern = permute(pattern, [1 3 2]);
+%pattern = permute(pattern, [3 1 2]);
 
 %unravel pattern so columns are channels and rows go from E1T1 to
 %E2T1 to E(end-1)Tend to EendTend
-new_pattern = reshape(pattern, size(pattern,1)*size(pattern,2), ...
-                        size(pattern,3));
+new_pattern = reshape(pattern, n_events * n_samps, n_chans);
+
+% now if there are n events and m samples:
+%% E1T1 E1T2 ... E1Tm E2T1 E2T2 ... E2Tm ... EnTm
+%new_pattern = reshape(pattern, n_events * n_samps, n_chans);
 
 
 
