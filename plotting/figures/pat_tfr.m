a47130dfc084a0fc4e530a861b3a4d33e44361cd
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2010-09-21 05:09:50 +0000

    pat_tfr: updated implementation, added events label-related features

--- plotting/figures/pat_tfr.m
+++ plotting/figures/pat_tfr.m
@@ -1,97 +1,98 @@
-function pat = pat_tfr(pat,fig_name,params,res_dir)
-%PAT_TFR   Make time-frequency representation plots and print them to disk.
+function pat = pat_tfr(pat, fig_name, varargin)
+%PAT_TFR   Make time-frequency representation plots.
 %
-%  pat = pat_tfr(pat, fig_name, params, res_dir)
+%  pat = pat_tfr(pat, fig_name, ...)
 %
 %  INPUTS:
-%           pat:  a pat object containing the pattern to be plotted.
+%       pat:  a pat object containing the pattern to be plotted.
 %
-%      fig_name:  string identifier for this set of figures.
-%
-%        params:  structure with options for plotting. See below.
-%
-%       res_dir:  path to the directory to save figures in. If not
-%                 specified, files will be saved in the pattern's
-%                 reports/figs directory.
+%  fig_name:  string identifier for this set of figures.
 %
 %  OUTPUTS:
-%           pat:  pat object with an added fig object.
+%       pat:  pat object with an added fig object.
 %
 %  PARAMS:
+%  These options may be specified using parameter, value pairs or by
+%  passing a structure. Defaults are shown in parentheses.
 %  Values to plot
 %   event_bins       - input to make_event_bins; can be used to average
-%                      over events before plotting
+%                      over events before plotting. ([])
 %   diff             - boolean; if true, before plotting, take the 
-%                      difference between event 1 and event 2. Default: false
+%                      difference between event 1 and event 2. (false)
 %   stat_name        - name of a stat object attached to pat. If
 %                      specified, p will be loaded from stat.file, and
-%                      only significant samples will be colored. Positive
-%                      p-values will be plotted red, while negative values
-%                      will be blue.
-%   alpha_range      - if plotting p-values, this gives the range of values
-%                      to color in. alpha_range(1) gives the alpha corresponding
-%                      to the darkest color in the colormap, while alpha_range(2)
-%                      gives the alpha value at which to begin shading.
-%                      Default: [0.005 0.05]
-%   correctm         - method to use to correct for multiple comparisions:
-%                      [ {none} | fdr | bonferroni ]
+%                      only significant samples will be colored.
+%                      Positive p-values will be plotted red, while
+%                      negative values will be blue. ('')
+%   alpha_range      - if plotting p-values, this gives the range of
+%                      values to color in. alpha_range(1) gives the
+%                      alpha corresponding to the darkest color in the
+%                      colormap, while alpha_range(2) gives the alpha
+%                      value at which to begin shading. ([0.005 0.05])
+%   correctm         - method to use to correct for multiple
+%                      comparisions: [ {none} | fdr | bonferroni ]
 %  Plotting options
 %   plot_mult_events - applies only if either time or frequency
 %                      dimension is singleton. If true, all events will
 %                      be plotted on one axis. Otherwise, each event
 %                      will be plotted on a separate figure. (true)
-%   map_limits       - limits for the z-axis of each plot: [z-min,z-max].
-%                      Default: -(absolute maximum) to (absolute maximum)
+%   map_limits       - limits for the z-axis of each plot:
+%                      [z-min z-max]. Default: -(absolute maximum) to
+%                      (absolute maximum)
 %   print_input      - input to print to use when printing figures.
-%                      Default: {'-depsc'}
-%   mult_fig_windows - if true, each figure will be plotted in a separate
-%                      window. Default: false
-%   Also see plot_tfr for more plotting options.
+%                      ({'-depsc'})
+%   mult_fig_windows - if true, each figure will be plotted in a
+%                      separate window. (false)
+%   res_dir          - path to the directory to save figures in. Default
+%                      is the pattern's standard reports directory.
+%                      Figures will be saved in [res_dir]/figs.
+%  Also see plot_tfr for more plotting options.
 
 % input checks
-if ~exist('pat','var') || ~isstruct(pat)
+if ~exist('pat', 'var') || ~isstruct(pat)
   error('You must pass in a pat object.')
 end
-if ~exist('fig_name','var')
+if ~exist('fig_name', 'var')
   fig_name = 'tfr';
 end
-if ~exist('params','var')
-  params = struct;
-elseif ~isstruct(params)
-  error('params must be a structure.')
-end
-if ~exist('res_dir','var') || isempty(res_dir)
-  report_dir = get_pat_dir(pat, 'reports');
-  cd(report_dir)
-  res_dir = './figs';
-elseif ~ismember(res_dir(1), {'/','.'})
-  res_dir = ['./' res_dir];
-end
-if ~exist(res_dir,'dir')
-  mkdir(res_dir)
+
+% backwards compatibility
+if length(varargin) == 2 && isstruct(varargin{1})
+  params = varargin{1};
+  params.res_dir = varargin{2};
 end
 
-% set default parameters
-params = structDefaults(params, ...
-                        'map_limits',       [],       ...
-                        'print_input',      {'-depsc'}, ...
-                        'event_bins',       '',       ...
-                        'plot_mult_events', true,     ...                        
-                        'diff',             false,    ...
-                        'mult_fig_windows', false,    ...
-                        'stat_name',        '',       ...
-                        'alpha_range',          [0.005 0.05], ...
-                        'correctm',         '');
+% options
+defaults.event_bins = [];
+defaults.diff = false;
+defaults.stat_name = '';
+defaults.alpha_range = [0.005 0.05];
+defaults.correctm = '';
+defaults.plot_mult_events = true;
+defaults.map_limits = [];
+defaults.legend = {};
+defaults.print_input = {'-depsc'};
+defaults.mult_fig_windows = false;
+defaults.res_dir = get_pat_dir(pat, 'reports');
+[params, plot_params] = propval(varargin, defaults);
+
+% prep the output directory
+params.res_dir = check_dir(params.res_dir);
+cd(params.res_dir)
+fig_dir = check_dir('./figs');
 
 if ~isempty(params.event_bins)
   % create bins using inputs accepted by make_event_bins
-  p = [];
-  p.eventbins = params.event_bins;
-  p.save_mats = false;
-  pattern = get_mat(modify_pattern(pat, p));
+  temp = bin_pattern(pat, 'eventbins', params.event_bins, ...
+                     'save_mats', false, 'verbose', false);
+  pattern = get_mat(temp);
+  event_labels = get_dim_labels(temp.dim, 'ev');
+  clear temp
 else
   pattern = get_mat(pat);
+  event_labels = get_dim_labels(pat.dim, 'ev');
 end
+chan_labels = get_dim_labels(pat.dim, 'chan');
 
 if ~isempty(params.stat_name)
   % get the stat object
@@ -116,7 +117,8 @@ if ~isempty(params.stat_name)
   
   % make the colormap, set the pattern to be plotted as the
   % z-scores of the p-values
-  [pattern, map, params.map_limits] = sig_colormap(p, [max_sig sig], 'two_way_signed');
+  [pattern, map, params.map_limits] = sig_colormap(p, [max_sig sig], ...
+                                                   'two_way_signed');
   colormap(map)
 else
   if params.diff
@@ -151,14 +153,18 @@ files = cell(num_events, size(pattern,2));
 
 % make one figure per channel
 n_figs = prod(size(files));
-fprintf('making %d TFR plots from pattern %s...\n', n_figs, pat.name);
+fprintf('making %d TFR plots from pattern "%s"...\n', n_figs, pat.name);
 
 n = 1;
 start_fig = gcf;
 files = cell(num_events, size(pattern,2));
-for i=1:num_events
-  for c=1:size(pattern,2)
-    fprintf('%d ', n)
+for i = 1:num_events
+  for c = 1:size(pattern, 2)
+    if num_events == 1
+      fprintf('%s ', chan_labels{c})
+    else
+      fprintf('%d ', n)
+    end
 
     if (t_sing || f_sing) && params.plot_mult_events
       e = ':';
@@ -177,23 +183,39 @@ for i=1:num_events
     if ~(t_sing || f_sing)
       % make a spectrogram
       data = permute(data, [4 3 1 2]);
-      h = plot_tfr(data, freq, time, params);
+      h = plot_tfr(data, freq, time, plot_params);
     elseif t_sing && f_sing
       error(['Cannot plot if both time and freqeuncy dimensions are ' ...
              'singleton.'])
     elseif f_sing
       % power vs. time
       data = permute(data, [1 3 4 2]);
-      h = plot_erp(data, time, params);
+      h = plot_erp(data, time, plot_params);
     elseif t_sing
       % power vs. frequency
       data = permute(data, [1 4 2 3]);
-      h = plot_freq(data, freq, params);
+      h = plot_freq(data, freq, plot_params);
     end
 
+    % legend
+    if num_events == 1
+      if ~isempty(params.legend)
+        l = legend(h, params.legend);
+      else
+        l = legend(h, event_labels);
+      end
+      set(l, 'Location', 'NorthEast')
+    end
+    
     % generate the filename
-    file_name = sprintf('%s_%s_%s_e%dc%d', pat.name, fig_name, pat.source, i, c);
-    files{i,c} = fullfile(res_dir, file_name);
+    if num_events > 1
+      file_name = sprintf('%s_%s_%s_%s_%s', pat.name, fig_name, pat.source, ...
+                          event_labels{i}, chan_labels{c});
+    else
+      file_name = sprintf('%s_%s_%s_%s', pat.name, fig_name, pat.source, ...
+                          chan_labels{c});
+    end
+    files{i,c} = fullfile(fig_dir, file_name);
 
     % print this figure
     print(gcf, params.print_input{:}, files{i,c})
