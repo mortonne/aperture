764cdb5a327a663b60b3a0886428309ece7b2f35
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2010-10-05 22:51:36 +0000

    documentation for sample index functions

--- patterns/dimensions/make_sample_matrix.m
+++ patterns/dimensions/make_sample_matrix.m
@@ -1,11 +1,32 @@
 function samples = make_sample_matrix(events, start, finish, ...
                                       samplerate_data, samplerate_pat)
+%MAKE_SAMPLE_MATRIX   Create unique labels for each sample in events.
+%
+%  samples = make_sample_matrix(events, start, finish, samplerate_data,
+%                                                        samplerate_pat)
+%
+%  INPUTS:
+%           events:  an events structure.
+%
+%            start:  start of each event in milliseconds.
+%
+%           finish:  end of each event in milliseconds.
+%
+%  samplerate_data:  samplerate of the raw data referenced by
+%                    events.eegfile.
+%
+%   samplerate_pat:  samplerate of the pattern; will be used for
+%                    creation of the samples matrix.
+%
+%  OUTPUTS:
+%          samples:  [events X samples] matrix with a label that
+%                    identifies unique samples. That is, if
+%                    samples[i,j] == samples[n,m], those positions in
+%                    the pattern matrix are taken from the same offset
+%                    in the EEG file.
 
 % assuming that finish time is inclusive
-
 index = make_sample_index([events.eegoffset], {events.eegfile});
-
-n_events = length(events);
 
 % time in samples relative to the onset sample
 start_samp = ms2samp(start, samplerate_data);
@@ -13,6 +34,7 @@ finish_samp = ms2samp(finish, samplerate_data);
 
 % add to make the samples matrix
 step = ms2samp(1000 / samplerate_pat, samplerate_data);
+n_events = length(events);
 event_samples = repmat(start_samp:step:finish_samp, n_events, 1);
 
 duration = finish - start + 1000 / samplerate_pat;
