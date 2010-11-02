55aac67c68bb79284dc9e06311774f0fa5460389
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2010-11-02 22:40:14 +0000

    remove_broadband: now actually saves the new pattern!

--- patterns/operations/remove_broadband.m
+++ patterns/operations/remove_broadband.m
@@ -33,59 +33,69 @@ function pat = remove_broadband(pat, varargin)
 % broadband is power at mean freq index (using the index really
 %   seems strange)
 
+% input checks
+if ~exist('pat', 'var') || ~isstruct(pat)
+  error('You must input a pattern object.')
+end
+
 % options
 defaults.plot = false;
 [params, saveopts] = propval(varargin, defaults);
 
-% load the pattern
-pattern = get_mat(pat);
+% run broadband subtraction
+pat = mod_pattern(pat, @run_remove_broadband, {params}, saveopts);
 
-% frequency information
-[n_events, n_chans, n_time, n_freq] = size(pattern);
-freqs = get_dim_vals(pat.dim, 'freq');
+function pat = run_remove_broadband(pat, params)
+  % load the pattern
+  pattern = get_mat(pat);
 
-iter_warn = 'stats:statrobustfit:IterationLimit';
-state = warning('query', iter_warn);
-warning('off', iter_warn);
-n_step = 100;
-step = floor((n_events * n_chans * n_time) / n_step);
-n = 0;
-b = NaN(1, 2);
-pow = NaN(1, n_freq);
-log_freqs = log2(freqs);
-for i = 1:n_events
-  for j = 1:n_chans
-    for k = 1:n_time
-      n = n + 1;
-      if mod(n, step) == 0
-        fprintf('.')
-      end
-      
-      pow = permute(pattern(i,j,k,:), [1 4 2 3]);
-      if all(isnan(pow))
-        continue
-      end
-      
-      % fit power on frequency
-      b = robustfit(log_freqs, pow);
-      
-      % if params.plot
-      %   clf
-      %   subplot(2,1,1)
-      %   plot_freq(b(1) + log_freqs * b(2), freqs);
-      %   hold on
-      %   plot_freq(pow, freqs, struct('y_label', 'Power'));
+  % frequency information
+  [n_events, n_chans, n_time, n_freq] = size(pattern);
+  freqs = get_dim_vals(pat.dim, 'freq');
+
+  iter_warn = 'stats:statrobustfit:IterationLimit';
+  state = warning('query', iter_warn);
+  warning('off', iter_warn);
+  n_step = 100;
+  step = floor((n_events * n_chans * n_time) / n_step);
+  n = 0;
+  b = NaN(1, 2);
+  pow = NaN(1, n_freq);
+  log_freqs = log2(freqs);
+  for i = 1:n_events
+    for j = 1:n_chans
+      for k = 1:n_time
+        n = n + 1;
+        if mod(n, step) == 0
+          fprintf('.')
+        end
         
-      %   subplot(2,1,2)
-      %   plot_freq(pow - (b(1) + log_freqs * b(2)), freqs, struct('y_label', 'Power'));
-      % end
+        pow = permute(pattern(i,j,k,:), [1 4 2 3]);
+        if all(isnan(pow))
+          continue
+        end
+        
+        % fit power on frequency
+        b = robustfit(log_freqs, pow);
       
-      % get the residuals from the robust fit
-      pattern(i,j,k,:) = pow - (b(1) + log_freqs * b(2));
+        % if params.plot
+        %   clf
+        %   subplot(2,1,1)
+        %   plot_freq(b(1) + log_freqs * b(2), freqs);
+        %   hold on
+        %   plot_freq(pow, freqs, struct('y_label', 'Power'));
+        
+        %   subplot(2,1,2)
+        %   plot_freq(pow - (b(1) + log_freqs * b(2)), freqs, struct('y_label', 'Power'));
+        % end
+      
+        % get the residuals from the robust fit
+        pattern(i,j,k,:) = pow - (b(1) + log_freqs * b(2));
+      end
     end
   end
-end
-fprintf('\n');
-
-warning(state.state, iter_warn);
+  fprintf('\n');
+  warning(state.state, iter_warn);
+  
+  pat = set_mat(pat, pattern, 'ws');
 
