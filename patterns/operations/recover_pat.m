0dd9c1d5faf1cb772f79566d73839c24c0ada1d9
zcohen <zcohen@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2010-12-09 18:10:29 +0000

    fixed small bug in recover_pat

--- patterns/operations/recover_pat.m
+++ patterns/operations/recover_pat.m
@@ -1,9 +1,10 @@
 function exp = recover_pat(exp, directory)
 
-d = dir(directory);
+search_directory = fullfile(directory, '*.mat');
+d = dir(search_directory);
 
 for i = 1:length(d)
   filename = fullfile(directory, d(i).name);
-  pat = getfield(load(filename,'obj'));
+  pat = getfield(load(filename),'obj');
   exp = setobj(exp, 'subj', pat.source, 'pat', pat);
 end
