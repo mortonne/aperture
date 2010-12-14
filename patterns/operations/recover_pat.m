e9576447ba79884d4d8993021b15942ff5795a49
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2010-12-14 16:54:24 +0000

    documented recover_pat

--- patterns/operations/recover_pat.m
+++ patterns/operations/recover_pat.m
@@ -1,10 +1,25 @@
-function exp = recover_pat(exp, directory)
+function exp = recover_pat(exp, pat_dir)
+%RECOVER_PAT   Load a pattern object from disk.
+%
+%  exp = recover_pat(exp, pat_dir)
+%
+%  INPUTS:
+%      exp:  an experiment object.
+%
+%  pat_dir:  directory containing subject pattern objects. The function
+%            will attempt to add "obj" variables from all MAT-files in
+%            the directory.
+%
+%  OUTPUTS:
+%      exp:  experiment object with the pattern objects added to the
+%            appropriate subjects.
 
-search_directory = fullfile(directory, '*.mat');
-d = dir(search_directory);
+search_dir = fullfile(pat_dir, '*.mat');
+d = dir(search_dir);
 
 for i = 1:length(d)
-  filename = fullfile(directory, d(i).name);
-  pat = getfield(load(filename),'obj');
+  filename = fullfile(pat_dir, d(i).name);
+  pat = getfield(load(filename, 'obj'), 'obj');
   exp = setobj(exp, 'subj', pat.source, 'pat', pat);
 end
+
