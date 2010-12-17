28ee6feb8c27feacf2ae460759858caf14752b57
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2010-12-17 22:40:56 +0000

    perf_perm_test: updated calling signature, deals with size mismatch

--- patclass/perf_perm_test.m
+++ patclass/perf_perm_test.m
@@ -1,7 +1,7 @@
-function stat = perf_perm_test(subj, stat_path, stat_name, res_dir)
+function exp = perf_perm_test(exp, stat_path, stat_name, varargin)
 %PERF_PERM_TEST   Test significance of a permutated performance metric.
 %
-%  stat = perf_perm_test(subj, stat_path, stat_name, res_dir)
+%  stat = perf_perm_test(subj, stat_path, stat_name, ...)
 %
 %  INPUTS:
 %       subj:  vector of subject objects.
@@ -13,11 +13,19 @@ function stat = perf_perm_test(subj, stat_path, stat_name, res_dir)
 %  stat_name:  name of the new stat object that will hold results of the
 %              significance test. Default is [stat_name]_perm.
 %
-%    res_dir:  directory where the results of the test will be saved.
-%              Default is the parent directory of the first stat object.
-%
 %  OUTPUTS:
 %       stat:  stat object with results.
+%
+%  PARAMS:
+%  These options may be specified using parameter, value pairs or by
+%  passing a structure. Defaults are shown in parentheses.
+%   oversample - if true, if pattern classification results have a size
+%                mismatch, the smaller results will be oversampled to
+%                match. (false)
+%   res_dir    - directory in which to save the results. Default is the
+%                parent directory of the first subject's stat file.
+
+subj = exp.subj;
 
 % input checks
 if ~exist('subj', 'var') || ~isstruct(subj)
@@ -28,38 +36,62 @@ end
 if ~exist('stat_name', 'var')
   stat_name = sprintf('%s_perm', stat_path{end});
 end
-if ~exist('params', 'var')
-  params = struct;
-end
-if ~exist('res_dir', 'var') || isempty(res_dir)
-  stat = getobj(subj(1), stat_path{:});
-  res_dir = fileparts(stat.file);
-end
+
+% options
+stat = getobj(subj(1), stat_path{:});
+defaults.oversample = false;
+defaults.res_dir = fileparts(stat.file);
+[params, perm_params] = propval(varargin, defaults);
+perm_params = propval(perm_params, struct, 'strict', false);
 
 % get stat objects from all subjects
-stats = getobjallsubj(subj, stat_path);
+stats = getobjallsubj(subj, stat_path{:});
 
 % load and concatenate the iterations substruct from res
 res = [];
 for i=1:length(stats)
-  subj_res = getfield(load(stats(i).file), 'res');
+  subj_res = getfield(load(stats(i).file, 'res'), 'res');
+  n_iter = size(subj_res.iterations, 1);
+  
+  % deal with possible mismatch in number of iterations
+  if n_iter < size(res, 1)
+    if ~params.oversample
+      error('Size mismatch. May try using oversample=true')
+    end
+    
+    n = size(res, 1) - n_iter;
+    filler = randsample(1:n_iter, n, true);
+    subj_res.iterations = cat(1, subj_res.iterations, ...
+                              subj_res.iterations(filler,:,:,:));
+  end
   res = cat(5, res, subj_res.iterations);
 end
 
 % run the permutation test
-p = apply_by_slice(@perm_test, {res}, 2:4, {});
+p = apply_by_slice(@perm_test, {res}, 2:4, {perm_params});
 
 % make a stat object to hold the results
 source = sprintf('%s-%s', subj(1).id, subj(end).id);
-stat_file = fullfile(res_dir, objfilename('stat', stat_name, source));
+stat_file = fullfile(params.res_dir, objfilename('stat', stat_name, source));
 stat = init_stat(stat_name, stat_file, source, params);
+exp = setobj(exp, 'stat', stat);
 
 save(stat.file, 'p')
 
-function p = perm_test(res)
+function p = perm_test(res, perm_params)
   % res: [iter X chan X time X freq X subj]
   % chan, time, and freq dimensions are singleton
   % c:   [subj X iter X field]
+  
+  if ~isfield(res(1).perfmet{1}, 'perm_perf')
+    fprintf('running permutations...')
+    parfor i = 1:numel(res)
+      res(i).perfmet{1} = perfmet_perm(res(i).acts, res(i).targs, ...
+                                       res(i).scratchpad, perm_params);
+    end
+    fprintf('done.\n')
+  end
+  
   c = permute(struct2cell(res), [6 2 1 3 4 5]);
   f = fieldnames(res);
   
@@ -94,8 +126,8 @@ function p = perm_test(res)
   % get the fraction of random performance values greater than or
   % equal to the observed value
   p = nnz(rand_perf >= obs_perf) / n_perm;
-  if p==0
+  if p == 0
     p = 1 / n_perm;
   end
-%endfunction
+
 
