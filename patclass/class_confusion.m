f9de02584d03a5c3d3af04079807775e9ea8e645
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2011-01-06 19:41:09 +0000

    class_confusion: now works if maxclass not the orig. perfmet

--- patclass/class_confusion.m
+++ patclass/class_confusion.m
@@ -8,8 +8,8 @@ function confmat = class_confusion(res, varargin)
 %  Additional inputs are passed to confusion.
 %
 %  OUTPUTS:
-%  confmat:  [class X class X chans X time X freq] matrix of
-%            correlations between classifier output and class labels.
+%  confmat:  [actual-class X guessed-class X chans X time X freq] matrix
+%            of rates for each (actual class, guessed class) condition.
 
 [n_iter, n_chans, n_time, n_freq] = size(res.iterations);
 n_class = size(res.iterations(1).acts, 1);
@@ -19,16 +19,12 @@ confmat = NaN(n_class, n_class, n_chans, n_time, n_freq);
 for j = 1:n_chans
   for k = 1:n_time
     for l = 1:n_freq
-      guesses = [];
-      desireds = [];
+      % get guesses and desireds across all cross-validation iterations
+      [acts, targs] = get_class_stats(res.iterations(:,j,k,l));
+      perf = perfmet_maxclass(acts, targs, struct);
       
-      for i = 1:n_iter
-        perfmet = res.iterations(i,j,k,l).perfmet{1};
-        guesses = [guesses perfmet.guesses];
-        desireds = [desireds perfmet.desireds];
-      end
-      
-      this_confmat = confusion(guesses, desireds, varargin{:});
+      % calculate the confusion matrix
+      this_confmat = confusion(perf.guesses, perf.desireds, varargin{:});
       confmat(:,:,j,k,l) = this_confmat;
     end
   end
