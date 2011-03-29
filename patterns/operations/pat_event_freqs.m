c7f4693fd81bc210b8b11b0f2f9d56e1772e25f1
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2011-03-29 15:48:32 +0000

    pat_event_freqs: default N is 0, added filtering support

--- patterns/operations/pat_event_freqs.m
+++ patterns/operations/pat_event_freqs.m
@@ -1,18 +1,24 @@
-function [n, labels] = pat_event_freqs(subj, pat_name, event_bins)
+function [n, labels] = pat_event_freqs(subj, pat_name, event_bins, ...
+                                       event_filter)
 %PAT_EVENT_FREQS   Frequency of different event types in patterns.
 %
 %  Use this to see how frequent different types of events are for
 %  a set of subjects.
 %
-%  [n, labels] = pat_event_freqs(subj, pat_name, event_bins)
+%  [n, labels] = pat_event_freqs(subj, pat_name, event_bins, event_filter)
+
+if ~exist('event_filter', 'var')
+  event_filter = '';
+end
 
 % get Ns and labels for each subject
 n_subj = length(subj);
 all_n = cell(1, n_subj);
 all_labels = cell(1, n_subj);
-for i = 1:n_subj
+parfor i = 1:n_subj
+  fprintf('%s\n', subj(i).id)
   pat = getobj(subj(i), 'pat', pat_name);
-  [subj_n, subj_labels] = get_subj_freq(pat, event_bins);
+  [subj_n, subj_labels] = get_subj_freq(pat, event_bins, event_filter);
   all_n{i} = subj_n;
   all_labels{i} = subj_labels;
 end
@@ -20,13 +26,18 @@ end
 % get them in the same order, set missing values
 labels = unique([all_labels{:}]);
 n_labels = length(labels);
-n = NaN(n_subj, n_labels);
+n = zeros(n_subj, n_labels);
 for i = 1:n_subj
   [tf, loc] = ismember(all_labels{i}, labels);
   n(i,loc) = all_n{i};
 end
 
-function [n, labels] = get_subj_freq(pat, event_bins)
+function [n, labels] = get_subj_freq(pat, event_bins, event_filter)
+
+  if ~isempty(event_filter)
+    % filter events but not the pattern matrix
+    pat = patFilt(pat, 'eventFilter', event_filter);
+  end
 
   % get bins from the event bins definitions
   [temp, bins] = patBins(pat, 'eventbins', event_bins);
