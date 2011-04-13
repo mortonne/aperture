8953affafc1bcd47751e10fd99da0b735f57e88c
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2011-04-13 22:10:12 +0000

    stat_paired_ttest: updated to new specs

--- stats/stat_paired_ttest.m
+++ stats/stat_paired_ttest.m
@@ -1,7 +1,7 @@
-function [p, statistic] = stat_paired_ttest(data, group, varargin)
+function [p, statistic, res] = stat_paired_ttest(data, group, tail)
 %Run a paired-samples ttest.
 %
-%  [p, statistic] = stat_paired_ttest(data, group)
+%  [p, statistic, res] = stat_paired_ttest(data, group, tail)
 %
 %  INPUTS:
 %     data:  vector of numeric data.
@@ -16,8 +16,11 @@ function [p, statistic] = stat_paired_ttest(data, group, varargin)
 %             label2 = sorted(2);
 %            t is calculated based on label1 - label2.
 %
-%  NOTES:
-%  Currently only runs two-tailed tests.
+%     tail:  direction hypothesis based on group{2} labels (assumed to
+%            be labeled 1 and 2). May be:
+%             'both'  1 - 2 is not zero
+%             'right' 1 - 2 is greater than zero
+%             'left'  1 - 2 is less than zero
 
 % Copyright 2007-2011 Neal Morton, Sean Polyn, Zachary Cohen, Matthew Mollison.
 %
@@ -41,7 +44,7 @@ if length(group) ~= 2
 end
 
 % unpack
-v1 = make_index(group{2});
+v1 = group{2};
 subj = group{1};
 
 % split data
@@ -50,11 +53,14 @@ n_subj = length(usubj);
 data1 = NaN(n_subj, 1);
 data2 = NaN(n_subj, 1);
 for i = 1:n_subj
-  data1(i) = data(strcmp(subj, usubj{i}) & v1 == 1);
-  data2(i) = data(strcmp(subj, usubj{i}) & v1 == 2);
+  data1(i) = data(subj == usubj(i) & v1 == 1);
+  data2(i) = data(subj == usubj(i) & v1 == 2);
 end
 
 % run a two-tailed paired ttest
-[h, p, ci, stats] = ttest(data1, data2, 0.05, 'both');
+[h, p, ci, stats] = ttest(data1, data2, 0.05, tail);
 statistic = stats.tstat;
+res = stats;
+res.output = sprintf('Paired ttest (%s tail):\np = %g, t(%d) = %g', ...
+                     tail, p, stats.df, statistic);
 
