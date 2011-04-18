8a1029ecae4d2d6ffc0cca0d1bd9dabf9309373c
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2011-04-18 22:40:21 +0000

    rmaov1: pairwise output now easier to read

--- stats/rmaov1.R
+++ stats/rmaov1.R
@@ -10,6 +10,10 @@ data <- read.table(args[1], colClasses=c('numeric', rep('factor', 2)),
 wide <- reshape(data, v.names='dep', idvar='subject', timevar='V1',
                 direction='wide')
 
+# reorder columns to have dep in ascending order
+ind <- c(1, order(names(wide)[-1]) + 1)
+wide <- wide[,ind]
+
 # use the car library
 # to install:
 # install.packages('car')
@@ -37,14 +41,19 @@ mc.aov <- Anova(mult.dv, idata=data.frame(rm.factor),
 summary(mc.aov)
 
 if (length(rm.factor) == 3) {
+  # calculate linear combinations
   pw.comp <- cbind(c(1,-1,0), c(1,0,-1), c(0,1,-1))
   pw.scores <- data.frame(as.matrix(wide[,-1]) %*% pw.comp)
   names(pw.scores) <- c('1vs2', '1vs3', '2vs3')
   n <- length(pw.scores[,1])
+
+  # corresponding t and significance (two-sided)
   obst.pw <- (sqrt(n) * mean(pw.scores)) / sd(pw.scores)
   pval.pw <- 2 * (1 - pt(abs(obst.pw), n - 1))
+
+  # print
+  m = mean(pw.scores)
+  se = sd(pw.scores) / sqrt(n)
+  round(data.frame(mean=m, SE=se, t=obst.pw, df=rep(n - 1, 3), p=pval.pw), 4)
 }
 
-obst.pw
-pval.pw
-n
