286577ae887d1e5944f15d9fc8c0401619a79b50
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2011-05-16 20:56:42 +0000

    added disp_job

---
+++ utils/distcomp/disp_job.m
@@ -0,0 +1,10 @@
+function disp_job(job)
+%DISP_JOB   Display the command window output of all tasks of a job.
+%
+%  disp_job(job)
+
+for i = 1:length(job.tasks)
+  fprintf('%s:\n', job.tasks(i).Name);
+  disp(job.tasks(i).commandWindowOutput)
+end
+
