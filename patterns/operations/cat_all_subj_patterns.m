83f41287f2ca639e90c1db637c59474bf3d3fa13
jmccluey <jmccluey@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2011-05-25 21:39:58 +0000

    added bin labels and levels to cat_all_subj_patterns

--- patterns/operations/cat_all_subj_patterns.m
+++ patterns/operations/cat_all_subj_patterns.m
@@ -16,23 +16,29 @@ function pat = cat_all_subj_patterns(subj, pat_name, dimension, varargin)
 %  PARAMS:
 %  These options may be specified using parameter, value pairs or by
 %  passing a structure. Defaults are shown in parentheses.
-%   event_filter - input to filterStruct to get a subset of events
-%                before concatenating. ('')
-%   event_bins - input to make_event_bins to average over events before
-%                concatenating across subjects. ({})
-%   dist       - flag for applying event bins in parallel (0=serial,
-%                1=distributed, 2=parallel). (0)
-%   memory     - memory to allocate each task if dist=1. ('2g')
-%   save_mats  - if true, mats associated with the new pattern will
-%                be saved to disk. If false, modified mats will be stored
-%                in the workspace, and can subsequently be moved to disk
-%                using move_obj_to_hd. (true)
-%   save_as    - name of the concatenated pattern. If all patterns have
-%                the same name, defaults to that name; otherwise, the
-%                default name is 'cat_pattern'.
-%   res_dir    - path to the directory in which to save the new pattern.
-%                Default is the same directory as the first pattern in
-%                pats.
+%   event_filter     - input to filterStruct to get a subset of events
+%                      before concatenating. ('')
+%   event_bins       - input to make_event_bins to average over events before
+%                      concatenating across subjects. ({})
+%   event_bin_labels - cell array of strings, with one cell per bin. Gives
+%                      a label for each event bin. ({})
+%   event_bin_levels - cell array of cell arrays of strings. Used only if
+%                      specifying bins as a conjunction of multiple
+%                      factors, e.g. two events fields. The label for
+%                      factor i, level j goes in eventbinlevels{i}{j}. ({})
+%   dist             - flag for applying event bins in parallel (0=serial,
+%                      1=distributed, 2=parallel). (0)
+%   memory           - memory to allocate each task if dist=1. ('2g')
+%   save_mats        - if true, mats associated with the new pattern will
+%                      be saved to disk. If false, modified mats will be stored
+%                      in the workspace, and can subsequently be moved to disk
+%                      using move_obj_to_hd. (true)
+%   save_as          - name of the concatenated pattern. If all patterns have
+%                      the same name, defaults to that name; otherwise, the
+%                      default name is 'cat_pattern'.
+%   res_dir          - path to the directory in which to save the new pattern.
+%                      Default is the same directory as the first pattern in
+%                      pats.
 
 % Copyright 2007-2011 Neal Morton, Sean Polyn, Zachary Cohen, Matthew Mollison.
 %
@@ -54,6 +60,8 @@ function pat = cat_all_subj_patterns(subj, pat_name, dimension, varargin)
 % options
 defaults.event_filter = '';
 defaults.event_bins = {};
+defaults.event_bin_labels = {};
+defaults.event_bin_levels = {};
 defaults.dist = 0;
 defaults.memory = '2g';
 [params, save_opts] = propval(varargin, defaults);
@@ -61,7 +69,8 @@ defaults.memory = '2g';
 % apply filtering and binning
 if ~isempty(params.event_filter) || ~isempty(params.event_bins)
   subj = apply_to_pat(subj, pat_name, @prep_pattern, ...
-                      {params.event_filter, params.event_bins}, ...
+                      {params.event_filter, params.event_bins, ...
+                      params.event_bin_labels, params.event_bin_levels}, ...
                       params.dist, 'memory', params.memory);
 end
 
@@ -72,7 +81,8 @@ pats = getobjallsubj(subj, {'pat', pat_name});
 pat = cat_patterns(pats, dimension, save_opts);
 
 
-function pat = prep_pattern(pat, event_filter, event_bins)
+function pat = prep_pattern(pat, event_filter, event_bins, ...
+                            event_bin_labels, event_bin_levels)
 
   if ~isempty(event_filter)
     pat = filter_pattern(pat, 'event_filter', event_filter, ...
@@ -80,6 +90,8 @@ function pat = prep_pattern(pat, event_filter, event_bins)
   end
   if ~isempty(event_bins)
     pat = bin_pattern(pat, 'eventbins', event_bins, ...
+                         'eventbinlabels', event_bin_labels, ...
+                         'eventbinlevels', event_bin_levels, ...
                          'save_mats', false, 'verbose', false);
   end
   
