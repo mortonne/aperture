653492e00d084e711fb02bc670bc843e7e165939
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2011-06-17 16:10:31 +0000

    tweak_fig: now prints immediately to steamline the process

--- utils/tweak_fig.m
+++ utils/tweak_fig.m
@@ -1,7 +1,22 @@
 function tweak_fig(fig_handle, file, printopt)
 %TWEAK_FIG   Make adustments to a figure before printing a final copy.
 %
+%  Often, the default settings of Matlab's figures are not quite what
+%  you want and need some tweaking. To complicate matters, printed
+%  figures generally look different than they are in Matlab. This
+%  function allows one to iteratively examine printed figures and make
+%  changes until the figure is perfect.
+%
 %  tweak_fig(fig_handle, file, printopt)
+%
+%  INPUTS:
+%  fig_handle:  handle to the figure to print.
+%
+%        file:  file to print to.
+%
+%    printopt:  cell array of additional inputs to print to specify
+%               options. Default is: {'-depsc'}. This prints the figure
+%               as a color EPS file.
 
 if ~exist('printopt', 'var')
   printopt = {'-depsc'};
@@ -15,6 +30,10 @@ end
 figure(fig_handle)
 
 while true
+  % print so they can look at it
+  print(fig_handle, printopt{:}, file);
+  fprintf('printed in: %s\n', file)
+  
   % what do you want to do next?
   r = input('print/change/quit (p/c/q): ', 's');
   switch r
