08da35801d9b6e627ad6f3998c3bc7da64027007
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2011-06-17 16:11:12 +0000

    disp_job: now displays one page at a time

--- utils/distcomp/disp_job.m
+++ utils/distcomp/disp_job.m
@@ -3,8 +3,12 @@ function disp_job(job)
 %
 %  disp_job(job)
 
+more on
+
 for i = 1:length(job.tasks)
   fprintf('%s:\n', job.tasks(i).Name);
   disp(job.tasks(i).commandWindowOutput)
 end
+
+more off
 
