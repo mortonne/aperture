41486d0148293f7498487b0a3bfe656cb2cab13c
jmccluey <jmccluey@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2011-06-20 15:09:40 +0000

    added ZDC's doc string for post_timebin

--- patterns/operations/post_timebin.m
+++ patterns/operations/post_timebin.m
@@ -1,8 +1,56 @@
 function pat = post_timebin(pat)
 
-step_size = unique(diff([pat.dim.time.avg]));
+%POST_TIMEBIN   fix the final timebin of a pattern after running
+%               bin_pattern (in preparation for fieldtrip)
+%
+%
+%  pat = post_timebin(pat)
+%
+%  INPUTS:
+%        pat:  pat object 
+%%
+%  OUTPUTS:
+%        pat:  pat object with the final timebin equal in size to
+%              other timebins
+%
+% NOTE: the issue this deals with is that bin_pattern handles time
+% binning in a way that leaves the final bin inclusive (or
+% exclusive, I'm not positive); this leaves the final bin average
+% or size as different than the previous bins (if you're binning
+% with even time bins). fieldtrip can't handle this, or more
+% specifically, the script that gets the unique time bins to get
+% the sample rate (get_pat_sample_rate.m), can't handle different
+% sized time bins, and so it throws an error. this script replaces
+% the last time bin's value with a value equal in step size to the
+% previous ones
+%
+% Copyright 2007-2011 Neal Morton, Sean Polyn, Zachary Cohen, Matthew Mollison.
+%
+% This file is part of EEG Analysis Toolbox.
+%
+% EEG Analysis Toolbox is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Lesser General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% EEG Analysis Toolbox is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU Lesser General Public License for more details.
+%
+% You should have received a copy of the GNU Lesser General Public License
+% along with EEG Analysis Toolbox.  If not, see <http://www.gnu.org/licenses/>.
+
+% input checks
+if ~exist('pat', 'var')
+  error('You must input a pattern object.')
+end
+
+times = get_dim_vals(pat.dim,'time');
+step_size = unique(diff([times]));
 if length(step_size)>1
-  stepper = (pat.dim.time(end-1).avg)- ...
-            (pat.dim.time(end-2).avg);
-  pat.dim.time(end).avg = pat.dim.time(end-1).avg+stepper;
+  stepper = (times(end-1))- ...
+            (times(end-2));
+  times(end) = times(end-1)+stepper;
+  warning('time bins not equal in size')
 end
