ceaab5b0a6e5708dbc9891003d70923840212967
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2011-07-06 16:37:24 +0000

    fragment_variable: cleaned up formatting and docstring

--- basic/fragment_variable.m
+++ basic/fragment_variable.m
@@ -1,26 +1,41 @@
 function frag_cell = fragment_variable(file_path, var_name, frag_path, ...
                                        frag_dim, unique_id)
-% FRAGMENT_VARIABLE take a variable saved in a file, and carve it up
-%      along some dimension, saving the carved pieces to a temporary
-%      location and passing back the path to these files.
+%FRAGMENT_VARIABLE   Resave a variable in slices.
 %
+%  Take a variable saved in a file, and carve it up along some
+%  dimension, saving the carved pieces to a temporary location and
+%  passing back the path to these files.
 %
+%  frag_cell = fragment_variable(file_path, var_name, frag_path, 
+%                                frag_dim, unique_id)
 %
+%  INPUTS:
+%  file_path:  path to a MAT file containing the variable to fragment.
 %
+%   var_name:  name of the variable to fragment.
+%
+%  frag_path:  path to save the new variables in.
+%
+%   frag_dim:  dimension along which to fragment the variable.
+%
+%  unique_id:  identifier for this variable.
+%
+%  OUTPUTS:
+%  frag_cell:  cell array with paths to MAT files containing each slice;
+%              the slice will be a variable named "frag".
 
+if ~exist(frag_path, 'dir')
+  mkdir(frag_path)
+end
 
 % load the variable
 var = load(file_path, var_name);
 sz = size(var.(var_name));
 
-grab = cell(1,length(sz));
-for i=1:length(grab)
-  grab{i} = ':';
-end
+grab = repmat({':'}, 1, length(sz));
 
 % step over the specified dimension
-for i=1:sz(frag_dim)
-  
+for i = 1:sz(frag_dim)
   grab{frag_dim} = i;
 
   % grab the specified slice
@@ -30,11 +45,9 @@ for i=1:sz(frag_dim)
   fullpath = fullfile(frag_path, ...
                       strcat('temp_',unique_id,'_',var_name, ...
                              '_d',num2str(frag_dim),'_ind',num2str(i)));
-  save(fullpath,'frag');
+  save(fullpath, 'frag');
   
   % save path to a cell array
   frag_cell{i} = fullpath;
-  % return the cell array
-
 end
-% endfunction
+
