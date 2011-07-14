dbc80f5b6222780224cc1a632a3f395813ef6ba8
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2011-07-14 04:06:56 +0000

    init_exp: now uses list2propval, removed useLock field, now does not create resDir

--- basic/init_exp.m
+++ basic/init_exp.m
@@ -1,4 +1,4 @@
-function exp = init_exp(experiment, varargin)
+function exp = init_exp(varargin)
 %INIT_EXP   Initialize an exp structure.
 %
 %  exp = init_exp(experiment, ...)
@@ -13,17 +13,13 @@ function exp = init_exp(experiment, varargin)
 %      exp:  an experiment object with the following optional fields,
 %            which can be set by passing in parameter, value pairs
 %            as additional arguments:
-%
-%             subj          - vector of subject objects. See get_sessdirs.
+%             subj          - vector of subject objects
 %             recordingType - the type of brain data collected
 %             resDir        - directory where results will be saved
 %             file          - path to the MAT-file where this exp
 %                             structure will be saved.  If not
 %                             specified, this will default to
 %                             resDir/exp.mat.
-%             useLock       - if true, exp.file will be locked during
-%                             loading and saving. Useful for running
-%                             distributed jobs that modify exp
 %
 %  EXAMPLE:
 %   % import information about subjects
@@ -31,37 +27,26 @@ function exp = init_exp(experiment, varargin)
 %
 %   % create a new exp structure
 %   exp = init_exp('catFR', 'subj', subj, 'recordingType', 'scalp');
+%
+%  See also init_subj, get_sessdirs.
 
-% input checks
-if ~exist('experiment','var')
-  error('You must specify an experiment name.')
-end
+% options
+defaults.experiment = '';
+defaults.recordingType = '';
+defaults.subj = struct;
+defaults.resDir = '';
+defaults.file = '';
+defaults.useLock = false;
+defaults.lastUpdate = '';
+old_args = {'subj', 'resDir', 'experiment', 'recordingType', 'useLock'};
+old_classes = {'struct' 'char' 'char' 'char' {'logical' 'numeric'}};
+init_fields = {'experiment'};
+init_classes = {'char'};
+exp = list2propval(varargin, defaults, ...
+                   'fields', old_args, 'classes', old_classes, ...
+                   'init_fields', init_fields, 'init_classes', init_classes);
 
-% set defaults
-def = struct('experiment',    experiment, ...
-             'recordingType', '',         ...
-             'subj',          struct,     ...
-             'resDir',        '',         ...
-             'file',          '',         ...
-             'useLock',       false,      ...
-             'lastUpdate',    '');
-
-try
-  in = struct(varargin{:});
-catch err
-  % not parameter, value pairs--must be using the old calling signature
-  old_args = {'subj', 'resDir', 'experiment', 'recordingType', 'useLock'};
-  pairs = {};
-  varargin = {experiment, varargin{:}};
-  for i=1:length(varargin)
-    pairs{end+1} = old_args{i};
-    pairs{end+1} = varargin{i};
-  end
-  in = struct(pairs{:});
-end
-
-exp = combineStructs(in, def);
-exp = orderfields(exp, def);
+exp = rmfield(exp, 'useLock');
 
 % sanity checks
 if ~ischar(exp.experiment)
@@ -72,14 +57,9 @@ elseif ~isstruct(exp.subj)
   error('subj must be a structure.')
 elseif ~ischar(exp.resDir)
   error('resDir must be a string.')
-elseif ~islogical(exp.useLock)
-  error('useLock must be a logical.')
 end
 
-% make sure the results directory exists and define exp.file
+% define exp.file
 if ~isempty(exp.resDir)
-  if ~exist(exp.resDir, 'dir')
-    mkdir(exp.resDir);
-  end
   exp.file = fullfile(exp.resDir, 'exp.mat');
 end
