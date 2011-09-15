c551c67a7e0d7357d36de69713a983e385a894b1
jmccluey <jmccluey@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2011-09-15 21:33:44 +0000

    import_channels: reverted to previous version, removed readlocs

--- patterns/dimensions/import_channels.m
+++ patterns/dimensions/import_channels.m
@@ -13,6 +13,11 @@ function subj = import_channels(subj, locs_file)
 %  OUTPUTS:
 %       subj:  subject object with an added "chan" structure holding
 %              channel information.
+%
+%  NOTES:
+%   Currently only imports the channel labels from the locs_file. Once
+%   functions are adapted to allow saving of chan structures to disk,
+%   will also import location information.
 
 % Copyright 2007-2011 Neal Morton, Sean Polyn, Zachary Cohen, Matthew Mollison.
 %
@@ -49,12 +54,16 @@ if isnumeric(locs_file)
 end
 
 % input for readlocs
-chan = readlocs(locs_file);
-numbers = num2cell(uint32(1:length(chan)));
-labels = {chan.labels};
-labels = cellfun(@(x) strrep(x, 'E', ''), labels, 'UniformOutput', false);
-[chan.numbers] = numbers{:};
-[chan.labels] = labels{:};
+elocs = readlocs(locs_file);
+numbers = num2cell(uint32(1:129));
+%[elocs.number] = number{:};
+%[elocs.label] = elocs.labels;
 
-subj.chan = chan;
+% for now, just include the number (index) and label from the
+% locs file. Need to implement saving channel structures to disk
+% so we can have the location information without taking up too
+% much memory.
+labels = {elocs.labels};
+labels = cellfun(@(x) strrep(x, 'E', ''), labels, 'UniformOutput', false);
+subj.chan = struct('number', numbers, 'label', labels);
 
