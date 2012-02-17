6fc2ea909d972eddd89fc28c7a885bb1a5e21b00
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2012-02-17 16:16:10 +0000

    load_exp_job: support for loading multiple jobs

--- basic/load_exp_job.m
+++ basic/load_exp_job.m
@@ -7,10 +7,11 @@ function exp = load_exp_job(exp, job, obj_path)
 %       exp:  experiment object.
 %
 %       job:  job (or vector of jobs) created by running apply_to_exp with
-%             async=true. Might not actually work with multiple jobs!
+%             async=true.
 %
 %  obj_path:  cell array of strings giving the path to an object that
-%             was modified by the job.
+%             was modified by the job. If multiple jobs, must pass a
+%             cell array, where obj_path{i} contains the path for job i.
 %
 %  OUTPUTS:
 %      exp:  experiment object with the new object from the experiment
@@ -18,16 +19,21 @@ function exp = load_exp_job(exp, job, obj_path)
 %
 %  See also load_job, apply_to_exp.
 
+if iscellstr(obj_path)
+  obj_path = {obj_path};
+end
+
 for i = 1:length(job)
-  temp = getAllOutputArguments(job);
+  temp = getAllOutputArguments(job(i));
   %if ~exist_obj(temp{1}, obj_path{:})
   %  fprintf('Warning: object missing for %s\n', temp{1}
   
-  if length(obj_path) > 2
-    obj_path = obj_path(1:2);
+  this_obj_path = obj_path{i};
+  if length(this_obj_path) > 2
+    this_obj_path = this_obj_path(1:2);
   end
   
-  obj = getobj(temp{1}, obj_path{:});
-  exp = setobj(exp, obj_path{1:end-1}, obj);
+  obj = getobj(temp{1}, this_obj_path{:});
+  exp = setobj(exp, this_obj_path{1:end-1}, obj);
 end
 
