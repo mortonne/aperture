f289e983d3a50bfa41729f33d428c6a6811a467c
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2013-04-01 16:39:05 +0000

    epoch_properties: style fixes, now calls epoch_amp_diff

--- patterns/eeglab/faster/epoch_properties.m
+++ patterns/eeglab/faster/epoch_properties.m
@@ -1,18 +1,23 @@
-function list_properties = epoch_properties(EEG,eeg_chans)
+function list_properties = epoch_properties(EEG, eeg_chans)
+%LIST_PROPERTIES   Stats on epochs for rejection.
+%
+%  list_properties = epoch_properties(EEG, eeg_chans)
+
 list_properties = [];
 
 if length(size(EEG.data)) < 3
-	fprintf('Not epoched.\n');
-	return;
+  fprintf('Not epoched.\n');
+  return
 end
 
 measure = 1;
 
-means = mean(EEG.data(eeg_chans,:),2);
+% mean over time and all epochs, for each channel
+means = mean(EEG.data(eeg_chans,:), 2);
 
-% 1 Epoch's mean deviation from channel means.
-for u = 1:size(EEG.data,3)
-	list_properties(u,measure) = mean(abs(squeeze(mean(EEG.data(eeg_chans,:,u),2)) - means));
+% 1 Epoch's mean deviation from channel means
+for u = 1:size(EEG.data, 3)
+  list_properties(u,measure) = mean(abs(squeeze(mean(EEG.data(eeg_chans,:,u),2)) - means));
 end
 measure = measure + 1;
 
@@ -21,13 +26,9 @@ list_properties(:,measure) = mean(squeeze(var(EEG.data(eeg_chans,:,:),0,2)));
 measure = measure + 1;
 
 % 3 Max amplitude difference
-for t = eeg_chans
-  for u = 1:size(EEG.data,3)
-    ampdiffs(t,u) = max(EEG.data(t,:,u)) - min(EEG.data(t,:,u));
-  end
-end
-%list_properties(:,measure) = mean(ampdiffs,1);
-list_properties(:,measure) = median(ampdiffs,1);
+ampdiffs = epoch_amp_diff(EEG, eeg_chans);
+list_properties(:,measure) = median(ampdiffs, 1);
+
 measure = measure + 1;
 
 % % 4 Median gradient
@@ -43,6 +44,6 @@ measure = measure + 1;
 % list_properties(:,measure) = mean(ampdiffs,1);
 % measure = measure + 1;
 
-for v = 1:size(list_properties,2)
-	list_properties(:,v) = list_properties(:,v) - median(list_properties(:,v));
+for v = 1:size(list_properties, 2)
+  list_properties(:,v) = list_properties(:,v) - median(list_properties(:,v));
 end
