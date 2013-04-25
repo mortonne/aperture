21169546ff63fa34eed665c13ce53c5584c3ed81
jmccluey <jmccluey@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2013-04-25 16:01:04 +0000

    get_sm: moved independentSubmitFcn back out of get_sm

--- utils/distcomp/get_sm.m
+++ utils/distcomp/get_sm.m
@@ -15,18 +15,3 @@ if ~exist(data_loc, 'dir')
   mkdir(data_loc);
 end
 set(sm, 'JobStorageLocation', data_loc)
-
-% determine which resource manager is being used
-jm = which_resource_manager;
-if strcmp('none', jm)
-  error('Job manager not found.')
-end
-
-% set SubmitFcn according to resource manager
-if strcmp('SGE', jm)
-  set(sm, 'IndependentSubmitFcn', {@independentSubmitFcn, params.memory});
-  
-elseif strcmp('TORQUE', jm)
-  params.memory = torque_mem_format(params.memory);
-  set(sm, 'IndependentSubmitFcn', {@independentSubmitFcn, params});
-end
