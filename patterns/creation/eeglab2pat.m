45bd5343a24ac18b47a77ea8cd657998fb84a575
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2013-06-24 14:43:05 +0000

    added eeglab2pat.m

---
+++ patterns/creation/eeglab2pat.m
@@ -0,0 +1,48 @@
+function pat = eeglab2pat(eeg)
+%EEGLAB2PAT   Import data from EEGLAB.
+%
+%  pat = eeglab2pat(eeg)
+
+% metadata
+pat_name = eeg.setname;
+pat_file = '';
+subj_id = eeg.subject;
+
+% dimensions
+events = convert_events(eeg.epoch);
+chan = convert_chans(eeg.chanlocs);
+time = init_time(eeg.times);
+freq = init_freq();
+
+% initialize the information struct
+pat = init_pat(pat_name, pat_file, eeg.subject, struct, ...
+               events, chan, time, freq);
+
+% set the data matrix. Need to convert from [channels X time X events]
+% to [events X channels X time]
+pat = set_mat(pat, permute(eeg.data, [3 1 2]), 'ws');
+
+
+function events = convert_events(events)
+
+  % find auto-generated event fields, which correspond to events
+  % within an epoch (these are not represented in the pattern
+  % format)
+  f = fieldnames(events);
+  strip = false(1, length(f));
+  for i = 1:length(f)
+    if length(f{i}) >= 5 && strcmp(f{i}(1:5), 'event')
+      strip = true;
+    end
+  end
+  events = rmfield(events, f(strip));
+  
+function chans = convert_chans(chanlocs)
+
+  % rename the labels field, add default channel numbers
+  labels = {chanlocs.labels};
+  number = num2cell(1:length(chanlocs));
+  chans = rmfield(chanlocs, 'labels');
+  [chans.number] = number{:};
+  [chans.label] = labels{:};
+  
