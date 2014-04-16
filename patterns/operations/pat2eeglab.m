03b0f9aefc7bf6f6c8c2a58c4378514360cd7557
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2014-04-16 18:50:25 +0000

    pat2eeglab.m: updated docstring, changed to make more general by removing references to experiment-specific fields

--- patterns/operations/pat2eeglab.m
+++ patterns/operations/pat2eeglab.m
@@ -1,43 +1,19 @@
 function EEG = pat2eeglab(pat, chanlocs_file)
 %PAT2EEGLAB   Convert a pattern object to EEGLAB format.
 %
-%  Prepare a pattern object for interactive visualization/analysis in
-%  EEGLAB. EEGLAB's GUI accesses standard variables that are expected to
-%  be defined in the base workspace. Hence, you must capture all the
-%  ouput variables, and give them the names used in this docstring.
-%  Otherwise, EEGLAB won't be able to find them.
+%  If you are using the EEGLAB GUI, you must use eeg_store to update
+%  variables used by the GUI after calling this function.
 %
-%  EEGLAB is expected to already be started when this function is run.
-%  ALLEEG should automatically be defined in the base workspace. After
-%  the function is run, ALLEEG, EEG, and CURRENTSET will be updated.
-%  Then you must run "eeglab redraw" to update the EEGLAB GUI with the
-%  new data. Multiple patterns may be imported, and they will all become
-%  available under the "Datasets" menu.
-%
-%  [ALLEEG, EEG, CURRENTSET] = pat2eeglab(ALLEEG, pat, chanlocs_file)
+%  EEG = pat2eeglab(pat, chanlocs_file)
 %
 %  INPUTS:
-%         ALLEEG:  EEGLAB standard variable that tracks all current
-%                  datasets.
-%
 %            pat:  an eeg_ana pattern object.
 %
 %  chanlocs_file:  path to a file containing channel locations. Default:
 %                  '~/eeg/HCGSN128.loc'
 %
 %  OUTPUTS:
-%      ALLEEG:  EEGLAB array of datasets, with the new EEG structure
-%               added.
-%
-%         EEG:  EEGLAB EEG structure, containing dataset information.
-%
-%  CURRENTSET:  index of the currrent EEGLAB dataset.
-%
-%  NOTES:
-%   For convenience, currently assumes that events will contain "rt"
-%   and "artifactMS" fields with ms values, and EEGLAB will create
-%   events corresponding to these times. Later, will generalize this
-%   feature.
+%      EEG:  EEGLAB EEG structure, containing dataset information.
 
 % Copyright 2007-2011 Neal Morton, Sean Polyn, Zachary Cohen, Matthew Mollison.
 %
@@ -66,13 +42,9 @@ samplerate = get_pat_samplerate(pat);
 
 time = get_dim(pat.dim, 'time');
 start_time = time(1).range(1);
-end_time = time(end).range(end);
-
-events = get_dim(pat.dim, 'ev');
 
 % import the pattern as segmented data
 set_name = sprintf('%s-%s', pat.name, pat.source);
-%cond_name = pat.dim.ev.name;
 cond_name = pat.name;
 comment_str = sprintf('created by pat2eeglab on %s', datestr(now));
 EEG = pop_importdata('data', pattern, ...
@@ -86,27 +58,10 @@ EEG = pop_importdata('data', pattern, ...
                      'chanlocs', chanlocs_file, ...
                      'comments', comment_str);
 
-%figure
-%pop_plottopo(EEG, [1:129] , 'words', 0, 'ylim', [-10 10]);
-
-% NaN out latency fields that are past the end of the epoch
-[events([events.rt] > end_time).rt] = deal(NaN);
-art = [events.artifactMS];
-[events(art > end_time | art <=1 ).artifactMS] = deal(NaN);
-
 % import events information
+events = get_dim(pat.dim, 'ev');
 events_cell = struct2cell(events')';
 EEG = pop_importepoch(EEG, events_cell, fieldnames(events), ...
                       'typefield', 'type', ...
-                      'latencyfields', {'rt', 'artifactMS'}, ...
                       'timeunit', 0.001);
-
-% add latency fields back in, since pop_importepoch strips them
-word_events = strcmp({EEG.event.type}, 'WORD');
-[EEG.event.rt] = deal(NaN);
-[EEG.event(word_events).rt] = deal(events.rt);
-
-%art_events = strcmp({EEG.event.type}, 'artifactMS');
-[EEG.event.artifactMS] = deal(NaN);
-[EEG.event(word_events).artifactMS] = deal(events.artifactMS);
 
