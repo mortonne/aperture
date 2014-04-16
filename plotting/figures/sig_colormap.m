5ebd9b41834ab2056d1e2b5a49f3529695de53ea
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2014-04-16 19:34:57 +0000

    sig_colormap.m: revamped implementation, now also returns information about critical values

--- plotting/figures/sig_colormap.m
+++ plotting/figures/sig_colormap.m
@@ -1,4 +1,4 @@
-function [z, map, map_limits] = sig_colormap(p, alpha_range, map_size, dir)
+function [z, map, limits, crit, p_crit] = sig_colormap(p, varargin)
 %SIG_COLORMAP   Create a colormap for plotting significance.
 %
 %  Create a colormap that colors only significant values, and plots
@@ -52,172 +52,40 @@ function [z, map, map_limits] = sig_colormap(p, alpha_range, map_size, dir)
 % You should have received a copy of the GNU Lesser General Public License
 % along with EEG Analysis Toolbox.  If not, see <http://www.gnu.org/licenses/>.
 
-% input checks
-if ~exist('p', 'var') || ~isnumeric(p)
-  error('You must supply an array of p-values.')
-end
-if ~exist('alpha_range', 'var')
-  alpha_range = [0.05 0.005];
-elseif ~isnumeric(alpha_range) || diff(alpha_range) > 0
-  error('alpha_range must be an increasing numeric array.')
-end
-if ~exist('dir', 'var')
-  dir = repmat(-1, size(p));
-  unsigned = true;
-end
-if ~exist('map_size', 'var')
-  map_size = 512;
-end
-if any(p < 0)
-  error('p-values cannot be negative.')
+% options
+def.alpha_range = [.05 .001];
+def.map_size = 512;
+def.dir = [];
+def.map_type = 'signed';
+opt = propval(varargin, def);
+
+if isempty(opt.dir)
+  opt.dir = repmat(-1, size(p));
 end
 
-% RGB values for the colors we will use
-WHITE = [1 1 1];
-LT_BLUE = [.8 .8 1];
-DK_BLUE = [0 0 1];
-LT_RED = [1 .8 .8];
-DK_RED = [1 0 0];
+% if either of our alphas are 0, make it very small
+% so norminv will be defined
+opt.alpha_range(opt.alpha_range == 0) = 1e-10;
+
+switch opt.map_type
+  case 'signed'
+    limits = [norminv(opt.alpha_range(2)) norminv(1 - opt.alpha_range(2))];
+    p_crit = [fliplr(opt.alpha_range) opt.alpha_range];
+  case 'unsigned'
+    limits = [norminv(opt.alpha_range(2)) 0];
+    p_crit = fliplr(opt.alpha_range);
+  otherwise
+    error('Unknown map type: %s', opt.map_type)
+end
+thresh = norminv(1 - opt.alpha_range(1));
+
+% create the map
+[map, crit] = thresh_colormap(limits, thresh, opt.map_size);
 
 % get corresponding z-values
 z = NaN(size(p));
-z(dir == 1) = norminv(1 - p(dir == 1));
-z(dir == -1) = norminv(p(dir == -1));
+z(opt.dir == 1) = norminv(1 - p(opt.dir == 1));
+z(opt.dir == -1) = norminv(p(opt.dir == -1));
 
-% if either of our alphas are 0, make it eps
-% so norminv will be defined
-alpha_range(alpha_range == 0) = eps(0);
-
-if unsigned
-  % treat the p-values as negative
-  z_neg = norminv(alpha_range(1));
-  z_neg_max = norminv(alpha_range(2));
-  
-  % define the colormap
-  windows{1} = [z_neg_max z_neg];
-  window_colors = { {DK_BLUE LT_BLUE} };
-  [map, map_limits] = make_sig_map(windows, window_colors, ...
-                                   [z_neg_max 0], map_size);
-else
-  error('plotting with direction currently not supported.')
-  
-  % get all the points of inflection for the map
-  neg_sig = norminv(alpha_range(1));
-  neg_max_sig = norminv(alpha_range(2));
-  pos_sig = norminv(1 - alpha_range(1));
-  pos_max_sig = norminv(1 - alpha_range(2));
-
-  % write values for each area of the map
-  windows{1} = [neg_max_sig  neg_sig    ];
-  windows{2} = [pos_sig      pos_max_sig];
-
-  % set the corresponding colors
-  window_colors = { { DK_BLUE, LT_BLUE}, ...
-                    { LT_RED,  DK_RED } };
-
-  % make the colormap
-  [map, map_limits] = make_sig_map(windows, window_colors, [], map_size);
-end
-
-z(z < map_limits(1)) = map_limits(1);
-z(z > map_limits(2)) = map_limits(2);
-
-
-function [map, map_limits] = make_sig_map(windows, window_colors, ...
-                                          val_range, map_size)
-  %MAKE_SIG_MAP   Make a colormap to use for plotting significance.
-  %
-  %  [map, map_limits] = make_sig_map(windows, window_colors,
-  %                                   val_range, map_size)
-  %
-  %  INPUTS:
-  %        windows:  cell array of 1X2 vectors, where windows{i}(1)
-  %                  gives the starting value of window i, and
-  %                  windows{i}(2) gives the end value of window i.
-  %
-  %  window_colors:  cell array of colors (in RGB format) corresponding
-  %                  to each window. Each cell can either contain a
-  %                  single color or a 1X2 cell array of colors,
-  %                  indicating start and end colors for a gradient.
-  %
-  %      val_range:  range of values that will correspond to the full
-  %                  color map. Default is
-  %                  [windows{1}(1) windows{end}(2)].
-  %
-  %       map_size:  integer indicating the size of the color map. Use
-  %                  larger values for higher resolution. Default: 128.
-  %
-  %  OUTPUTS:
-  %            map:  a color map. Areas that were not specified in the
-  %                  windows but are within val_range are colored white.
-  %
-  %     map_limits:  limits of the color map.
-
-  % input checks
-  if ~exist('windows', 'var')
-    error('You must specify values to map the colors onto.')
-  elseif ~exist('window_colors', 'var')
-    error('You must specify colors for the endpoints of each window.')
-  end
-  if ~exist('val_range', 'var') || isempty(val_range)
-    % use the first value in the first window
-    % to the last value in the last window
-    val_range = [windows{1}(1) windows{end}(2)];
-  end
-  if ~exist('map_size', 'var')
-    map_size = 128;
-  end
-
-  % initialize as a white map
-  map = ones(map_size,3);
-  for i=1:length(windows)
-    % sanity checks
-    if diff(windows{i}) < 0
-      error('window %d is not increasing.', i)
-    elseif windows{i}(1) < val_range(1) || windows{i}(2) > val_range(2)
-      error('window %d falls outside of val_range.', i)
-    end
-
-    % get the map indices for this window
-    map_start = val2color(windows{i}(1), val_range, map_size);
-    map_end = val2color(windows{i}(2), val_range, map_size);
-    map_window = map_start:map_end;
-
-    if isnumeric(window_colors{i})
-      % if only one color, expand to start and end colors
-      window_colors{i} = repmat(window_colors(i),1,2);
-    end
-
-    % create the map for this window
-    map(map_window,:) = makecolormap(window_colors{i}{:}, length(map_window));
-  end
-  
-  map_limits = val_range;
-
-
-function color_index = val2color(val, val_range, map_size)
-  %VAL2COLOR   Get the index in a colormap for a given value.
-  %
-  %  color_coord = val2color(val, val_range, map_size)
-  %
-  %  INPUTS:
-  %          val:  scalar indicating a value.
-  %  
-  %    val_range:  range of values corresponding to the color
-  %                map
-  %
-  %     map_size:  size of the color map.
-  %
-  %  OUTPUTS:
-  %  color_index:  index of val in the color map.
-  
-  % get the proportional value of val
-  x = (val - val_range(1)) / diff(val_range);
-  
-  % translate to color space
-  if x == 0
-    color_index = 1;
-  else
-    color_index = ceil(x * map_size);
-  end
-
+z(z < limits(1)) = limits(1) + eps(limits(1));
+z(z > limits(2)) = limits(2) - eps(limits(2));
