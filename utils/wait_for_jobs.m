4d8c96fcc7a81883cbeb2eb86b4b5a5d8ba8c4e8
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2014-06-15 03:35:57 +0000

    wait_for_jobs.m added

---
+++ utils/wait_for_jobs.m
@@ -0,0 +1,33 @@
+function wait_for_jobs(jobs, refresh)
+%WAIT_FOR_JOB   Wait for a set of jobs to finish.
+%
+%  wait_for_jobs(jobs, refresh)
+%
+%  INPUTS:
+%     jobs:  vector of job objects.
+%
+%  refresh:  seconds to pause between checking job status. Setting to
+%            check less often will reduce the load on the scheduler.
+%            Default is 2.
+
+if nargin < 2
+  refresh = 2;
+end
+
+finished = false;
+first = false;
+while ~finished
+  if ~first
+    pause(refresh)
+  else
+    first = false;
+  end
+  
+  job_finished = false(1, length(jobs));
+  for i = 1:length(jobs)
+    job_finished(i) = strcmp(jobs.State, 'finished');
+  end
+  finished = all(job_finished);
+end
+
+
