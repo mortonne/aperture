485073d16bbadadbcbbbccae73e964cce51e53cb
mortonne <mortonne@c12e3afc-dd6c-419a-8e81-404338cd32d5>
2014-06-15 03:44:43 +0000

    added gen_pat_dir.m, modified get_pat_dir.m to call it

--- patterns/operations/gen_pat_dir.m
+++ patterns/operations/gen_pat_dir.m
@@ -1,7 +1,9 @@
-function pat_dir = get_pat_dir(pat, varargin)
-%GET_PAT_DIR   Get the path to a pattern's standard directory.
+function pat_dir = gen_pat_dir(pat, varargin)
+%GEN_PAT_DIR   Get the path to a pattern's standard directory.
 %
-%  pat_dir = get_pat_dir(pat, s1, s2, ... sN)
+%  Same as get_pat_dir, but does not create any directories.
+%
+%  pat_dir = gen_pat_dir(pat, s1, s2, ... sN)
 %
 %  It is assumed that the pattern's files are saved in
 %  [pat_dir]/patterns.
@@ -37,10 +39,30 @@ function pat_dir = get_pat_dir(pat, varargin)
 % You should have received a copy of the GNU Lesser General Public License
 % along with EEG Analysis Toolbox.  If not, see <http://www.gnu.org/licenses/>.
 
-main_dir = check_dir(gen_pat_dir(pat));
-if ~isempty(varargin)
-  pat_dir = check_dir(gen_pat_dir(pat, varargin{:}));
-else
-  pat_dir = main_dir;
+% input checks
+if ~isfield(pat, 'file')
+  error('pat must have a "file" field.')
 end
+
+% get one of this pattern's files
+if iscell(pat.file)
+  pat_file = pat.file{1};
+else
+  pat_file = pat.file;
+end
+
+% get the standard main directory for the pattern
+main_dir = fileparts(fileparts(pat_file));
+
+% fix the path if it is relative; assuming that we don't want to use
+% filepaths relative to the search path
+if ~ismember(filesep, main_dir)
+  main_dir = fullfile('.', main_dir);
+end
+
+% get the requested directory
+pat_dir = fullfile(main_dir, varargin{:});
+
+% standardize the path
+pat_dir = check_dir(pat_dir, false);
 
