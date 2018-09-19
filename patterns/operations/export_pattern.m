6d3c4c7a6c64a9e1ea2004a86dcbae7225a24590
Neal Morton <mortonne@gmail.com>
2018-09-19 17:37:34 -0500

    add copyright notice

--- patterns/operations/export_pattern.m
+++ patterns/operations/export_pattern.m
@@ -26,6 +26,23 @@ function subj = export_pattern(subj, pat_name, out_dir)
 %  % export the 'volt' pattern for all subjects
 %  apply_to_subj(exp.subj, @export_pattern, {'volt', '~/results'})
 
+% Copyright 2007-2011 Neal Morton, Sean Polyn, Zachary Cohen, Matthew Mollison.
+%
+% This file is part of EEG Analysis Toolbox.
+%
+% EEG Analysis Toolbox is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Lesser General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% EEG Analysis Toolbox is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU Lesser General Public License for more details.
+%
+% You should have received a copy of the GNU Lesser General Public License
+% along with EEG Analysis Toolbox.  If not, see <http://www.gnu.org/licenses/>.
+
 pat = getobj(subj, 'pat', pat_name);
 pat = move_obj_to_workspace(pat);
 dims = {'ev' 'chan' 'time' 'freq'};
