04b0b9d8bfcdad4bc9fbdb8c2c5e62866400b47f
jeon11 <jin-jeon@hotmail.com>
2019-04-08 13:28:48 -0500

    added commas to seperate parameters so the script takes in filt_type

--- patterns/operations/freq_filter_pattern.m
+++ patterns/operations/freq_filter_pattern.m
@@ -9,7 +9,7 @@ def.buffer = [];
 def.precision = '';
 [opt, save_opt] = propval(varargin, def);
 
-pat = mod_pattern(pat, @filter_pat, {freq_range filttype opt}, save_opt);
+pat = mod_pattern(pat, @filter_pat, {freq_range, filt_type, opt}, save_opt);
 
 
 function pat = filter_pat(pat, freq_range, filt_type, opt)
@@ -35,10 +35,10 @@ function pat = filter_pat(pat, freq_range, filt_type, opt)
       for k = 1:n_freq
         % apply filter
         x = permute(pattern(i,j,:,k), [3 1 2 4]);
-        y = buttfilt(x, freq_range, samplerate, filt_type, opt.order);
+        y = buttfilt(double(x), freq_range, samplerate, filt_type, opt.order);
 
         % place in new pattern, without the buffer
-        new_pattern(i,j,:,k) = y(buffer_samp+1:end-buffer);
+        new_pattern(i,j,:,k) = y(buffer_samp+1:end-buffer_samp);
       end
     end
   end
